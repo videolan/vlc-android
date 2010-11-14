68e353ba6eef50aab87b98f1fc6d704495f6639d
Adrien Maglo <magsoft@videolan.org>
2010-11-14 14:49:02 +0000

    Add the vlc-android Java project.

---
+++ vlc-android/src/vlc/android/vlc.java
@@ -0,0 +1,31 @@
+package vlc.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.lang.*;
+
+public class vlc extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView tv = new TextView(this);
+
+        try
+        {
+        	System.loadLibrary("vlccore");
+    		System.loadLibrary("vlc");
+    		
+            tv.setText("Yes, we have successfully loaded libVLC!");
+        }
+        catch (UnsatisfiedLinkError e)
+        {
+        	tv.setText("Couldn't load libVLC. :-(");
+        }
+
+        setContentView(tv);
+    }
+}
