4b87a880e5729bb2f0601f575c062f77fca0e346
Rémi Duraffort <ivoire@videolan.org>
2010-11-15 23:44:29 +0100

    Create the right directory for the libs in compileLibvlcjni too (and update the HOWTO).

--- compileLibvlcjni.sh
+++ compileLibvlcjni.sh
@@ -10,5 +10,9 @@ ANDROID_NDK=$1
 cd libvlcjni
 $ANDROID_NDK/ndk-build
 
+if [  ! -d "../vlc-android/libs/armeabi" ]; then
+    mkdir -p ../vlc-android/libs/armeabi/
+fi
+
 cp -v libs/armeabi/libvlcjni.so ../vlc-android/libs/armeabi/
 
