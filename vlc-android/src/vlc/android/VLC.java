2bfc930e0fa9ea1e662d19a9bd33f83da575d091
Adrien Maglo <magsoft@videolan.org>
2011-01-28 00:18:03 +0000

    Rename the "vlc" class to "VLC" as names of Java classes always begin with an upper case letter.

--- vlc-android/src/vlc/android/VLC.java
+++ vlc-android/src/vlc/android/VLC.java
@@ -2,14 +2,14 @@ package vlc.android;
 
 import android.app.Activity;
 
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.widget.Button;
 
-import android.view.SurfaceView;
 import android.view.View;
 
 
-public class vlc extends Activity {
+public class VLC extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -17,11 +17,15 @@ public class vlc extends Activity {
         setContentView(R.layout.main);
 
         final Button button = (Button)findViewById(R.id.button);
-        final SurfaceView surfaceView = (SurfaceView)findViewById(R.id.surface_view);
+        final GLSurfaceView surfaceView = (GLSurfaceView)findViewById(R.id.surface_view);
         
-        libvlc = new LibVLC();
+        vout = new Vout(this);
         
-        surfaceView.getHolder().addCallback(new SurfaceCallback(libvlc));
+        surfaceView.setDebugFlags(GLSurfaceView.DEBUG_CHECK_GL_ERROR | GLSurfaceView.DEBUG_LOG_GL_CALLS);
+        surfaceView.setRenderer(vout);
+        surfaceView.setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+        
+        libvlc = new LibVLC(surfaceView, vout);
 
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -32,4 +36,5 @@ public class vlc extends Activity {
     }
 
     LibVLC libvlc;
+    Vout vout;
 }
