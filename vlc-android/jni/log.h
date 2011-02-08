3c0c9acf42451ac06eab3481726b0bdecc81b97c
Jean-Philippe André <jpeg@videolan.org>
2011-02-08 19:17:05 +0100

    Log: add LOGV & LOGW macros

--- vlc-android/jni/log.h
+++ vlc-android/jni/log.h
@@ -6,6 +6,8 @@
 #define  LOG_TAG    "libvlcjni"
 #define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
+#define  LOGW(...)  __android_log_print(ANDROID_LOG_WARNING,LOG_TAG,__VA_ARGS__)
 #define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
+#define  LOGV(...)  __android_log_print(ANDROID_LOG_VERBOSE,LOG_TAG,__VA_ARGS__)
 
 #endif // LIBVLCJNI_LOG_H
