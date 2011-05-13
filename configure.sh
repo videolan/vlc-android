5c77d09714354a12d85bcde191f1017132c03466
Dominique Martinet <asmadeus@codewreck.org>
2011-05-13 14:52:02 +0900

    build system minor changes for compatibility with MacOS. yay.

--- configure.sh
+++ configure.sh
@@ -7,7 +7,7 @@ fi
 
 ANDROID_API=android-9
 
-ANDROID_BIN=$ANDROID_NDK/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin/
+ANDROID_BIN=$ANDROID_NDK/toolchains/arm-linux-androideabi-4.4.3/prebuilt/*-x86/bin/
 ANDROID_INCLUDE=$ANDROID_NDK/platforms/$ANDROID_API/arch-arm/usr/include
 ANDROID_LIB=$ANDROID_NDK/platforms/$ANDROID_API/arch-arm/usr/lib
 GCC_PREFIX=${ANDROID_BIN}/arm-linux-androideabi-
@@ -26,7 +26,7 @@ else
 	EXTRA_CFLAGS+=" -march=armv6j -mtune=arm1136j-s -msoft-float"
 fi
 
-PATH="$ANDROID_BIN":$PATH \
+PATH="$ANDROID_BIN:$PATH" \
 CPPFLAGS="-I$ANDROID_INCLUDE" \
 LDFLAGS="-Wl,-rpath-link=$ANDROID_LIB,-Bdynamic,-dynamic-linker=/system/bin/linker -Wl,--no-undefined -Wl,-shared -L$ANDROID_LIB $EXTRA_LDFLAGS" \
 CFLAGS="-nostdlib $EXTRA_CFLAGS -O2" \
@@ -36,6 +36,8 @@ CC="${GCC_PREFIX}gcc" \
 CXX="${GCC_PREFIX}g++" \
 NM="${GCC_PREFIX}nm" \
 STRIP="${GCC_PREFIX}strip" \
+RANLIB="${GCC_PREFIX}ranlib" \
+AR="${GCC_PREFIX}ar" \
 PKG_CONFIG_LIBDIR="$VLC_SOURCEDIR/extras/contrib/hosts/arm-eabi/lib/pkgconfig" \
 sh ../configure --host=arm-eabi-linux --build=x86_64-unknown-linux \
                 --enable-static-modules \
