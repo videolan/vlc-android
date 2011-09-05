d27247da039f25faf828d65db1e998250f530876
Rafaël Carré <funman@videolan.org>
2011-09-05 18:44:04 +0200

    Don't autogenerate Android.mk

---
+++ vlc-android/jni/Android.mk
@@ -0,0 +1,24 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE    := libvlcjni
+LOCAL_SRC_FILES := libvlcjni.c aout.c thumbnailer.c
+LOCAL_C_INCLUDES := $(VLC_SRC_DIR)/include
+
+ifeq ($(NO_NEON),)
+ARCH=armeabi-v7a
+else
+ARCH=armeabi
+endif
+
+LOCAL_LDLIBS := -L$(VLC_CONTRIB)/lib \
+	$(VLC_MODULES) \
+	$(VLC_BUILD_DIR)/compat/.libs/libcompat.a \
+	$(VLC_BUILD_DIR)/lib/.libs/libvlc.a \
+	$(VLC_BUILD_DIR)/src/.libs/libvlccore.a \
+	-ldl -lz -lm -logg -lvorbisenc -lvorbis -lFLAC -lspeex -ltheora \
+	-lavformat -lavcodec -lavcore -lavutil -lpostproc -lswscale \
+	-lmpeg2 -lpng -ldca -ldvbpsi -ltwolame -lkate -llog -la52 \
+	-lebml -lmatroska -ltag \
+	$(ANDROID_NDK)/sources/cxx-stl/gnu-libstdc++/libs/$(ARCH)/libstdc++.a
+
+include $(BUILD_SHARED_LIBRARY)
