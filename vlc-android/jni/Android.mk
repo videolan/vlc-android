52a1ef3d04239b09dd782bf168376fc2a327145b
Asad Mehmood <asad78611@googlemail.com>
2011-09-05 18:47:11 +0200

    Update ffmpeg linking order
    libswscale depends on libavutil in the latest ffmpeg git master
    
    Signed-off-by: Rafaël Carré <funman@videolan.org>

--- vlc-android/jni/Android.mk
+++ vlc-android/jni/Android.mk
@@ -16,7 +16,7 @@ LOCAL_LDLIBS := -L$(VLC_CONTRIB)/lib \
 	$(VLC_BUILD_DIR)/lib/.libs/libvlc.a \
 	$(VLC_BUILD_DIR)/src/.libs/libvlccore.a \
 	-ldl -lz -lm -logg -lvorbisenc -lvorbis -lFLAC -lspeex -ltheora \
-	-lavformat -lavcodec -lavcore -lavutil -lpostproc -lswscale \
+	-lavformat -lavcodec -lavcore -lswscale -lavutil -lpostproc \
 	-lmpeg2 -lpng -ldca -ldvbpsi -ltwolame -lkate -llog -la52 \
 	-lebml -lmatroska -ltag \
 	$(ANDROID_NDK)/sources/cxx-stl/gnu-libstdc++/libs/$(ARCH)/libstdc++.a
