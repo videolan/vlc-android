82bbefaab1f2dfa4efc6b06edfa77b6ddb287159
Martin Storsjö <martin@martin.st>
2011-09-29 12:05:24 +0300

    Fetch android system headers from cyanogen on github and device libraries for linking
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- install.sh
+++ install.sh
@@ -7,6 +7,25 @@ if [ -z "$ANDROID_NDK" -o -z "$ANDROID_SDK" ]; then
    exit 1;
 fi;
 
+echo "Fetching Android system headers"
+# Using CyanogenMod headers instead of AOSP, since CyanogenMod
+# has commit 1563f4aca88d354c502dba056d173cefc7c2ea7f,
+# "Stagefright: Memcpy optimization on output port." (available
+# upstream at https://www.codeaurora.org/gitweb/quic/la/?p=platform/frameworks/base.git;a=commit;h=052368f194c9fc180b9b0335b60114a2f1fb88d8),
+# which adds some vtable entries needed on newer qualcomm devices.
+$GIT clone --depth=1 git://github.com/CyanogenMod/android_frameworks_base.git android-headers/frameworks/base
+$GIT clone --depth=1 git://github.com/CyanogenMod/android_system_core.git android-headers/system/core
+export ANDROID_SYS_HEADERS=${PWD}/android-headers
+
+echo "Fetching Android libraries for linking"
+# Libraries from any froyo/gingerbread device/emulator should work
+# fine, since the symbols used should be available on most of them.
+wget http://download.cyanogenmod.com/get/update-cm-7.0.3-N1-signed.zip
+unzip update-cm-7.0.3-N1-signed.zip system/lib/*
+mv system/lib android-libs
+rmdir system
+export ANDROID_LIBS=${PWD}/android-libs
+
 echo "Cloning and updating VLC"
 $GIT clone git://git.videolan.org/vlc.git
 
