4571f2631baf61134ded19993a17801a1b422864
Walter Heck <walterheck@gmail.com>
2012-01-02 18:22:18 +0100

    Improve the code+headers checkout
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- install.sh
+++ install.sh
@@ -7,29 +7,51 @@ if [ -z "$ANDROID_NDK" -o -z "$ANDROID_SDK" ]; then
    exit 1;
 fi;
 
-echo "Fetching Android system headers"
 # Using CyanogenMod headers instead of AOSP, since CyanogenMod
 # has commit 1563f4aca88d354c502dba056d173cefc7c2ea7f,
 # "Stagefright: Memcpy optimization on output port." (available
 # upstream at https://www.codeaurora.org/gitweb/quic/la/?p=platform/frameworks/base.git;a=commit;h=052368f194c9fc180b9b0335b60114a2f1fb88d8),
 # which adds some vtable entries needed on newer qualcomm devices.
-$GIT clone -b gingerbread --depth=1 git://github.com/CyanogenMod/android_frameworks_base.git android-headers/frameworks/base
-$GIT clone -b gingerbread --depth=1 git://github.com/CyanogenMod/android_system_core.git android-headers/system/core
+if [ ! -d "android-headers/frameworks/base" ]; then
+	echo "Fetching Android system headers (1/2)"
+	$GIT clone -b gingerbread --depth=1 git://github.com/CyanogenMod/android_frameworks_base.git android-headers/frameworks/base
+else
+	echo "Updating Android system headers (1/2)"
+	pushd android-headers/frameworks/base > /dev/null
+	$GIT pull origin gingerbread
+	popd > /dev/null
+fi
+if [ ! -d "android-headers/system/core" ]; then
+	echo "Fetching Android system headers (2/2)"
+	$GIT clone -b gingerbread --depth=1 git://github.com/CyanogenMod/android_system_core.git android-headers/system/core
+else
+	echo "Updating Android system headers (2/2)"
+	pushd android-headers/system/core > /dev/null
+	$GIT pull origin gingerbread
+	popd > /dev/null
+fi
 export ANDROID_SYS_HEADERS=${PWD}/android-headers
 
 echo "Fetching Android libraries for linking"
 # Libraries from any froyo/gingerbread device/emulator should work
 # fine, since the symbols used should be available on most of them.
-if [ ! -f "update-cm-7.0.3-N1-signed.zip" ]; then
-    wget http://download.cyanogenmod.com/get/update-cm-7.0.3-N1-signed.zip
-    unzip update-cm-7.0.3-N1-signed.zip system/lib/*
+if [ ! -f "update-cm-7.1.0.1-NS-signed.zip" ]; then
+    curl -O http://mirror.sea.tdrevolution.net/cm/stable/gingerbread/update-cm-7.1.0.1-NS-signed.zip
+    unzip update-cm-7.1.0.1-NS-signed.zip system/lib/*
     mv system/lib android-libs
     rmdir system
 fi
 export ANDROID_LIBS=${PWD}/android-libs
 
-echo "Cloning and updating VLC"
-$GIT clone git://git.videolan.org/vlc.git
+if [ ! -d "vlc" ]; then
+	echo "VLC source not found, cloning"
+	$GIT clone git://git.videolan.org/vlc.git
+else
+	echo "VLC source found, pulling from remote master"
+	pushd vlc > /dev/null
+	$GIT pull origin master
+	popd > /dev/null
+fi
 
 echo "Applying the patches"
 cd vlc
