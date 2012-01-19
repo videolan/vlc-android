42264ba77108dabdf256f33215f792c3d97b6db9
Rafaël Carré <funman@videolan.org>
2012-01-19 18:48:18 -0500

    Fix compat/vlccore linking order
    Link with live555

--- vlc-android/jni/Android.mk
+++ vlc-android/jni/Android.mk
@@ -23,14 +23,14 @@ endif
 
 LOCAL_LDLIBS := -L$(VLC_CONTRIB)/lib \
 	$(VLC_MODULES) \
-	$(VLC_BUILD_DIR)/compat/.libs/libcompat.a \
 	$(VLC_BUILD_DIR)/lib/.libs/libvlc.a \
 	$(VLC_BUILD_DIR)/src/.libs/libvlccore.a \
+	$(VLC_BUILD_DIR)/compat/.libs/libcompat.a \
 	-ldl -lz -lm -logg -lvorbisenc -lvorbis -lFLAC -lspeex -lspeexdsp -ltheora \
 	-lavformat -lavcodec -lswscale -lavutil -lpostproc \
-	-lmpeg2 -lpng -ldvbpsi -llog -la52 \
-	-lebml -lmatroska -ltag -lgsm -lopenjpeg -lgme -lupnp -lixml -lthreadutil -lxml2 \
-	-lsamplerate \
+	-lmpeg2 -lpng -ldvbpsi -llog -la52 -lebml -lmatroska -ltag \
+	-lgsm -lopenjpeg -lgme -lupnp -lixml -lthreadutil -lxml2 -lsamplerate \
+	-lliveMedia -lUsageEnvironment -lBasicUsageEnvironment -lgroupsock \
 	$(CPP_STATIC)
 
 include $(BUILD_SHARED_LIBRARY)
