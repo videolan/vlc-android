a72e09e24a832f9694a4194bb193252c32ca772a
Rafaël Carré <funman@videolan.org>
2012-01-21 00:40:50 -0500

    aout: cosmetics

--- vlc-android/jni/aout.c
+++ vlc-android/jni/aout.c
@@ -27,30 +27,22 @@ typedef struct
 /** Unique Java VM instance, as defined in libvlcjni.c */
 extern JavaVM *myVm;
 
-//void aout_open (void **opaque, unsigned int *rate, unsigned int *nb_channels, unsigned int *fourCCFormat, unsigned int *nb_samples)
 int aout_open(void **opaque, char *format, unsigned *rate, unsigned *nb_channels)
 {
     LOGI("Opening the JNI audio output");
-    aout_sys_t *p_sys;
-    JNIEnv *p_env;
 
-    // Replace opaque by p_sys and keep the libvlc pointe
-    p_sys = calloc (1, sizeof (*p_sys));
+    aout_sys_t *p_sys = calloc (1, sizeof (*p_sys));
     if (!p_sys)
-    {
-        *opaque = NULL;
-        return -1; // OOM
-    }
+        return -1;
 
-    // Keep a reference to our Java object and return aout_sys_t
-    p_sys->j_libVlc = (jobject) *opaque;
-    *opaque         = (void*) p_sys;
+    p_sys->j_libVlc = *opaque;       // Keep a reference to our Java object
+    *opaque         = (void*) p_sys; // The callback will need aout_sys_t
 
-    LOGI ("Parameters: %u channels, FOURCC '%4.4s', "
-          "sample rate: %uHz",
+    LOGI ("Parameters: %u channels, FOURCC '%4.4s',  sample rate: %uHz",
           *nb_channels, format, *rate);
 
     // Attach the thread to the VM. Keep this JNIEnv for aout_close()
+    JNIEnv *p_env;
     if ((*myVm)->AttachCurrentThread (myVm, &p_env, NULL) != 0)
     {
         LOGE("Couldn't attach the display thread to the JVM !");
@@ -126,20 +118,20 @@ int aout_open(void **opaque, char *format, unsigned *rate, unsigned *nb_channels
 /**
  * Play an audio sample
  **/
-//void aout_play (void *opaque, unsigned char *buffer, size_t bufferSize, unsigned int nb_samples)
 void aout_play(void *opaque, const void *samples, unsigned count, int64_t pts)
 {
-    aout_sys_t *p_sys = (aout_sys_t*) opaque;
+    aout_sys_t *p_sys = opaque;
     JNIEnv *p_env;
 
     /* How ugly: we constantly attach/detach this thread to/from the JVM
-         * because it will be killed before aout_close is called.
-         * aout_close will actually be called in an different thread!
-         */
+     * because it will be killed before aout_close is called.
+     * aout_close will actually be called in an different thread!
+     */
     (*myVm)->AttachCurrentThread (myVm, &p_env, NULL);
 
     (*p_env)->SetByteArrayRegion (p_env, p_sys->byteArray, 0,
-                                  2 /*nb_channels*/ * count * sizeof (uint16_t), (jbyte*) samples);
+                                  2 /*nb_channels*/ * count * sizeof (uint16_t),
+                                  (jbyte*) samples);
     if ((*p_env)->ExceptionCheck (p_env))
     {
         // This can happen if for some reason the size of the input buffer
@@ -151,7 +143,9 @@ void aout_play(void *opaque, const void *samples, unsigned count, int64_t pts)
     }
 
     (*p_env)->CallVoidMethod (p_env, p_sys->j_libVlc, p_sys->play,
-                              p_sys->byteArray, 2 /*nb_channels*/ * count * sizeof (uint16_t), FRAME_SIZE);
+                              p_sys->byteArray,
+                              2 /*nb_channels*/ * count * sizeof (uint16_t),
+                              FRAME_SIZE);
     // FIXME: check for errors
 
     (*myVm)->DetachCurrentThread (myVm);
@@ -160,7 +154,7 @@ void aout_play(void *opaque, const void *samples, unsigned count, int64_t pts)
 void aout_close(void *opaque)
 {
     LOGI ("Closing audio output");
-    aout_sys_t *p_sys = (aout_sys_t*) opaque;
+    aout_sys_t *p_sys = opaque;
     if (!p_sys)
         return;
 
