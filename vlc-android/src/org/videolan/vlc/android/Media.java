f939ce73560e3339b0bd13fcf848df29f8af102e
Sébastien Toque <xilasz@gmail.com>
2012-01-25 12:02:47 +0100

    ignore .bin files

--- vlc-android/src/org/videolan/vlc/android/Media.java
+++ vlc-android/src/org/videolan/vlc/android/Media.java
@@ -31,7 +31,7 @@ public class Media implements Comparable<Media> {
     public final static String TAG = "VLC/MediaItem";
 
     public final static String[] EXTENTIONS = {
-            ".3g2", ".3gp", ".3gp2", ".3gpp", ".amv", ".asf", ".avi", ".bin", ".divx", ".dv", "f4v",
+            ".3g2", ".3gp", ".3gp2", ".3gpp", ".amv", ".asf", ".avi", ".divx", ".dv", "f4v",
             ".flv", ".gxf", ".iso", ".m1v", ".m2v", ".m2t", ".m2ts", ".m4v", ".mkv", ".mov", ".mp2",
             ".mp2v", ".mp4", ".mp4v", ".mpa", ".mpe", ".mpeg", ".mpeg1", ".mpeg2", ".mpeg4", ".mpg",
             ".mpv2", ".mts", ".mxf", ".nsv", ".nuv", ".ogg", ".ogm", ".ogv", ".ogx", ".ps", ".rec",
