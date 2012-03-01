1bf432ee5fa91138aaf25f049b02c59149a357f0
Sébastien Toque <xilasz@gmail.com>
2012-03-01 22:09:49 +0100

    fix callback leak + pause playback for both incoming and outgoing calls

--- vlc-android/src/org/videolan/vlc/PhoneStateReceiver.java
+++ vlc-android/src/org/videolan/vlc/PhoneStateReceiver.java
@@ -23,27 +23,21 @@ package org.videolan.vlc;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 
 public class PhoneStateReceiver extends BroadcastReceiver {
 
-    private PhoneStateListener mListener = new PhoneStateListener() {
-        @Override
-        public void onCallStateChanged(int state, String incomingNumber) {
-            if (state == TelephonyManager.CALL_STATE_RINGING) {
-                LibVLC libVLC = LibVLC.getExistingInstance();
-                if (libVLC != null && libVLC.isPlaying())
-                    libVLC.pause();
-            }
-        }
-    };
-
     @Override
     public void onReceive(Context context, Intent intent) {
-        TelephonyManager tm = (TelephonyManager)
-                context.getSystemService(Context.TELEPHONY_SERVICE);
-        tm.listen(mListener, PhoneStateListener.LISTEN_CALL_STATE);
+        String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+
+        if (state.equals(TelephonyManager.EXTRA_STATE_RINGING) ||
+                state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
+
+            LibVLC libVLC = LibVLC.getExistingInstance();
+            if (libVLC != null && libVLC.isPlaying())
+                libVLC.pause();
+        }
     }
 
 }
