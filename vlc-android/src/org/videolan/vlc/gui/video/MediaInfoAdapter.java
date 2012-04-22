ce5be2558dca6c04b205bb779d0e680928c61e16
Sébastien Toque <xilasz@gmail.com>
2012-04-22 12:05:35 +0200

    Preview: don't display language if unknown

--- vlc-android/src/org/videolan/vlc/gui/video/MediaInfoAdapter.java
+++ vlc-android/src/org/videolan/vlc/gui/video/MediaInfoAdapter.java
@@ -52,33 +52,49 @@ public class MediaInfoAdapter extends ArrayAdapter<TrackInfo> {
             holder = (ViewHolder) v.getTag();
 
         TrackInfo track = getItem(position);
-        String language = track.Language != null ? track.Language : "und";
         String title;
-        String text;
+        StringBuilder textBuilder = new StringBuilder(1024);
         Resources res = getContext().getResources();
         switch (track.Type)
         {
             case TrackInfo.TYPE_AUDIO:
                 title = res.getString(R.string.track_audio);
-                text = res.getQuantityString(R.plurals.track_audio_info, track.Channels, track.Codec, language, track.Channels, track.Samplerate);
+                appendCommon(textBuilder, res, track);
+                appendAudio(textBuilder, res, track);
                 break;
             case TrackInfo.TYPE_VIDEO:
                 title = res.getString(R.string.track_video);
-                text = res.getString(R.string.track_video_info, track.Codec, language, track.Width, track.Height, track.Framerate);
+                appendCommon(textBuilder, res, track);
+                appendVideo(textBuilder, res, track);
                 break;
             case TrackInfo.TYPE_TEXT:
                 title = res.getString(R.string.track_text);
-                text = res.getString(R.string.track_text_info, track.Codec, language);
+                appendCommon(textBuilder, res, track);
                 break;
             default:
                 title = res.getString(R.string.track_unknown);
-                text = "";
         }
 
         holder.title.setText(title);
-        holder.text.setText(text);
+        holder.text.setText(textBuilder.toString());
 
         return v;
+    }
+
+    private void appendCommon(StringBuilder textBuilder, Resources res, TrackInfo track) {
+        textBuilder.append(res.getString(R.string.track_codec_info, track.Codec));
+        if (track.Language != null && !track.Language.equalsIgnoreCase("und"))
+            textBuilder.append(res.getString(R.string.track_language_info, track.Language));
+    }
+
+    private void appendAudio(StringBuilder textBuilder, Resources res, TrackInfo track) {
+        textBuilder.append(res.getQuantityString(R.plurals.track_channels_info, track.Channels, track.Channels));
+        textBuilder.append(res.getString(R.string.track_samplerate_info, track.Samplerate));
+    }
+
+    private void appendVideo(StringBuilder textBuilder, Resources res, TrackInfo track) {
+        textBuilder.append(res.getString(R.string.track_resolution_info, track.Width, track.Height));
+        textBuilder.append(res.getString(R.string.track_framerate_info, track.Framerate));
     }
 
     static class ViewHolder {
