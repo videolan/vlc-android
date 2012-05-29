360a22a84aed321b8967f4725f921e88a4b4922f
Jean-Baptiste Kempf <jb@videolan.org>
2012-05-29 19:01:06 +0200

    Remove patch about libxml. Upstream has a better one

--- compile.sh
+++ compile.sh
@@ -40,7 +40,7 @@ fi
 export PATH=${ANDROID_NDK}/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin:${PATH}
 
 # 1/ libvlc, libvlccore and its plugins
-TESTED_HASH=e354bf04a542
+TESTED_HASH=f8aa395031
 if [ ! -d "vlc" ]; then
     echo "VLC source not found, cloning"
     git clone git://git.videolan.org/vlc.git vlc
