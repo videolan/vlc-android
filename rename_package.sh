92522d4e9d35360e0cac0c9b66430ab7069a89f4
Jean-Baptiste Kempf <jb@videolan.org>
2012-06-23 19:10:35 +0200

    Fix rename script

--- rename_package.sh
+++ rename_package.sh
@@ -3,9 +3,12 @@
 OLD_NAME=org.videolan.vlc
 NEW_NAME=org.videolan.vlc.beta_v7neon
 
-OLD_PATH=org/videolan/vlc/
-NEW_PATH=org/videolan/vlc_beta_v7neon/
+OLD_PATH=$(echo $OLD_NAME |sed 's/\./\//g')
+NEW_PATH=$(echo $NEW_NAME |sed 's/\./\//g')
 
-mv vlc-android/src/${OLD_PATH} vlc-android/src/${NEW_PATH}
-find vlc-android \( -name "*.xml" -o -name "*.java" -o -name "*.cfg" -o -name "*.aidl" \) -print0 | xargs -0 sed -ri "s/${OLD_PATH}/${NEW_PATH}/g'
+mv vlc-android/src/${OLD_PATH} vlc-android/src/tmp
+mkdir -p vlc-android/src/${OLD_PATH}
+mv vlc-android/src/tmp vlc-android/src/${NEW_PATH}
+
+find vlc-android \( -name "*.xml" -o -name "*.java" -o -name "*.cfg" -o -name "*.aidl" \) -print0 | xargs -0 sed -ri "s/${OLD_NAME}/${NEW_NAME}/g"
 
