9f9d7a5716e8c68229e3a938851f6b7d024e23ae
Jean-Baptiste Kempf <jb@videolan.org>
2012-07-04 12:17:08 +0200

    Remove unused imports

--- vlc-android/src/org/videolan/vlc/Util.java
+++ vlc-android/src/org/videolan/vlc/Util.java
@@ -27,13 +27,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.text.DecimalFormat;
-import java.util.Enumeration;
 import java.util.Properties;
 
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.net.Uri;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
