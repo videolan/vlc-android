a3bba6d41f71234b1fa75b0252e5146455343df0
Edward Wang <edward.c.wang@compdigitec.com>
2012-08-07 21:29:15 -0400

    compile.sh: Bump tested hash to fix interlace compile

--- compile.sh
+++ compile.sh
@@ -88,7 +88,7 @@ export PLATFORM_SHORT_ARCH
 export PATH=${ANDROID_NDK}/toolchains/${PATH_HOST}-${GCCVER}/prebuilt/`uname|tr A-Z a-z`-x86/bin:${PATH}
 
 # 1/ libvlc, libvlccore and its plugins
-TESTED_HASH=b089c84
+TESTED_HASH=9eb524d
 if [ ! -d "vlc" ]; then
     echo "VLC source not found, cloning"
     git clone git://git.videolan.org/vlc.git vlc
