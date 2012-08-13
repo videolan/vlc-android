203034012369aaf59bd89d7b4634cdc36fd87ab4
Edward Wang <edward.c.wang@compdigitec.com>
2012-08-12 20:33:26 -0400

    Allow CompatErrorActivity to show runtime errors

--- vlc-android/src/org/videolan/vlc/gui/CompatErrorActivity.java
+++ vlc-android/src/org/videolan/vlc/gui/CompatErrorActivity.java
@@ -24,12 +24,28 @@ import android.widget.TextView;
 public class CompatErrorActivity extends Activity {
     public final static String TAG = "VLC/CompatErrorActivity";
 
+    /**
+     * Simple friendly activity to tell the user something's wrong.
+     *
+     * Intent parameters (all optional):
+     * runtimeError (bool) - Set to true if you want to show a runtime error
+     *                       (defaults to a compatibility error)
+     * message (string) - the more detailed problem
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.not_compatible);
 
+        String errorMsg = Util.getErrorMsg();
+        if(getIntent().getBooleanExtra("runtimeError", false))
+            if(getIntent().getStringExtra("message") != null) {
+                errorMsg = getIntent().getStringExtra("message");
+                TextView tvo = (TextView)findViewById(R.id.message);
+                tvo.setText(R.string.error_problem);
+            }
+
         TextView tv = (TextView)findViewById(R.id.errormsg);
-        tv.setText(getResources().getString(R.string.error_not_compatible) + "\n" + Util.getErrorMsg());
+        tv.setText(getResources().getString(R.string.error_message_is) + "\n" + errorMsg);
 
         super.onCreate(savedInstanceState);
 
