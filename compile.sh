d5d2050141cab599c554384c6409b3fcd50d6d10
Rafaël Carré <funman@videolan.org>
2012-09-11 00:08:34 +0200

    threads patch: Remove useless hunk

--- compile.sh
+++ compile.sh
@@ -88,7 +88,7 @@ export PLATFORM_SHORT_ARCH
 export PATH=${ANDROID_NDK}/toolchains/${PATH_HOST}-${GCCVER}/prebuilt/`uname|tr A-Z a-z`-x86/bin:${PATH}
 
 # 1/ libvlc, libvlccore and its plugins
-TESTED_HASH=7a216cf
+TESTED_HASH=a597327a
 if [ ! -d "vlc" ]; then
     echo "VLC source not found, cloning"
     git clone git://git.videolan.org/vlc.git vlc
