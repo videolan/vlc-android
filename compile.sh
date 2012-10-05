ed2898f244f982c9c699b393a5832c99ae837137
Ludovic Fauvet <etix@videolan.org>
2012-10-05 17:51:32 +0200

    Bump the tested hash because of broken build

--- compile.sh
+++ compile.sh
@@ -89,7 +89,7 @@ export PLATFORM_SHORT_ARCH
 export PATH=${ANDROID_NDK}/toolchains/${PATH_HOST}-${GCCVER}/prebuilt/`uname|tr A-Z a-z`-x86/bin:${PATH}
 
 # 1/ libvlc, libvlccore and its plugins
-TESTED_HASH=cd11cbfeadc
+TESTED_HASH=91b10ca8
 if [ ! -d "vlc" ]; then
     echo "VLC source not found, cloning"
     git clone git://git.videolan.org/vlc.git vlc
