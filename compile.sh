90c2a5ed0efcf7fe7319298b3e4be57d138ac558
Jean-Baptiste Kempf <jb@videolan.org>
2012-10-30 16:27:58 +0100

    Update the tested VLC hash

--- compile.sh
+++ compile.sh
@@ -82,7 +82,7 @@ export PLATFORM_SHORT_ARCH
 export PATH=${ANDROID_NDK}/toolchains/${PATH_HOST}-${GCCVER}/prebuilt/`uname|tr A-Z a-z`-x86/bin:${PATH}
 
 # 1/ libvlc, libvlccore and its plugins
-TESTED_HASH=826ec51f
+TESTED_HASH=9aef14d5e5
 if [ ! -d "vlc" ]; then
     echo "VLC source not found, cloning"
     git clone git://git.videolan.org/vlc.git vlc
