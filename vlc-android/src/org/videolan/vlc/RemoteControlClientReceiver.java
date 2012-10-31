d18fe163775ae716c6f776d4193a61d2693ddaea
Ludovic Fauvet <etix@videolan.org>
2012-10-31 10:57:22 +0100

    RemoteControlClientReceiver: Remove redundant check

--- vlc-android/src/org/videolan/vlc/RemoteControlClientReceiver.java
+++ vlc-android/src/org/videolan/vlc/RemoteControlClientReceiver.java
@@ -44,32 +44,30 @@ public class RemoteControlClientReceiver extends BroadcastReceiver {
             return;
 
         if(action.equalsIgnoreCase(Intent.ACTION_MEDIA_BUTTON)) {
-            if (intent.getAction().equals(Intent.ACTION_MEDIA_BUTTON)) {
-                KeyEvent keyEvent = (KeyEvent) intent.getExtras().get(Intent.EXTRA_KEY_EVENT);
-                if (keyEvent.getAction() != KeyEvent.ACTION_DOWN)
-                    return;
+            KeyEvent keyEvent = (KeyEvent) intent.getExtras().get(Intent.EXTRA_KEY_EVENT);
+            if (keyEvent.getAction() != KeyEvent.ACTION_DOWN)
+                return;
 
-                Intent i = null;
-                switch (keyEvent.getKeyCode()) {
-                    case KeyEvent.KEYCODE_HEADSETHOOK:
-                    case KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE:
-                    case KeyEvent.KEYCODE_MEDIA_PLAY:
-                    case KeyEvent.KEYCODE_MEDIA_PAUSE:
-                        i = new Intent(AudioService.ACTION_WIDGET_PLAY);
-                        break;
-                    case KeyEvent.KEYCODE_MEDIA_STOP:
-                        i = new Intent(AudioService.ACTION_WIDGET_STOP);
-                        break;
-                    case KeyEvent.KEYCODE_MEDIA_NEXT:
-                        i = new Intent(AudioService.ACTION_WIDGET_FORWARD);
-                        break;
-                    case KeyEvent.KEYCODE_MEDIA_PREVIOUS:
-                        i = new Intent(AudioService.ACTION_WIDGET_BACKWARD);
-                        break;
-                }
-                if(i != null)
-                    context.sendBroadcast(i);
+            Intent i = null;
+            switch (keyEvent.getKeyCode()) {
+                case KeyEvent.KEYCODE_HEADSETHOOK:
+                case KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE:
+                case KeyEvent.KEYCODE_MEDIA_PLAY:
+                case KeyEvent.KEYCODE_MEDIA_PAUSE:
+                    i = new Intent(AudioService.ACTION_WIDGET_PLAY);
+                    break;
+                case KeyEvent.KEYCODE_MEDIA_STOP:
+                    i = new Intent(AudioService.ACTION_WIDGET_STOP);
+                    break;
+                case KeyEvent.KEYCODE_MEDIA_NEXT:
+                    i = new Intent(AudioService.ACTION_WIDGET_FORWARD);
+                    break;
+                case KeyEvent.KEYCODE_MEDIA_PREVIOUS:
+                    i = new Intent(AudioService.ACTION_WIDGET_BACKWARD);
+                    break;
             }
+            if(i != null)
+                context.sendBroadcast(i);
         }
     }
 }
