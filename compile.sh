e55898a0b3977232a4dfc14ccfd3b82b9ec93dc1
Edward Wang <edward.c.wang@compdigitec.com>
2012-12-04 15:39:45 -0500

    Temporarily uncrash the build in AudioTrack

--- compile.sh
+++ compile.sh
@@ -83,7 +83,7 @@ NDK_TOOLCHAIN_PATH=${ANDROID_NDK}/toolchains/${PATH_HOST}-${GCCVER}/prebuilt/`un
 export PATH=${NDK_TOOLCHAIN_PATH}:${PATH}
 
 # 1/ libvlc, libvlccore and its plugins
-TESTED_HASH=9aef14d5e5
+TESTED_HASH=4f42f23
 if [ ! -d "vlc" ]; then
     echo "VLC source not found, cloning"
     git clone git://git.videolan.org/vlc.git vlc
