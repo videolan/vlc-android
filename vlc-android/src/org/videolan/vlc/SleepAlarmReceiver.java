7c1191d7abb12a35a537fe9924c4de079a8bedf2
Edward Wang <edward.c.wang@compdigitec.com>
2012-12-28 11:30:18 -0500

    SleepAlarmReceiver: missing copyright header

--- vlc-android/src/org/videolan/vlc/SleepAlarmReceiver.java
+++ vlc-android/src/org/videolan/vlc/SleepAlarmReceiver.java
@@ -1,3 +1,22 @@
+/*****************************************************************************
+ * SleepAlarmReceiver.java
+ *****************************************************************************
+ * Copyright © 2012 VLC authors and VideoLAN
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
 package org.videolan.vlc;
 
 import android.content.BroadcastReceiver;
@@ -5,17 +24,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-
 public class SleepAlarmReceiver extends BroadcastReceiver {
-
     public final static String TAG = "VLC/SleepAlarmReceiver";
 
     public final static String SLEEP_INTENT = "org.videolan.vlc.SleepIntent";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-
-        if (context == null)
+        if(context == null)
             return;
         Log.i(TAG,"VLC is about to sleep");
         Intent intentSleep = new Intent();
@@ -23,4 +39,3 @@ public class SleepAlarmReceiver extends BroadcastReceiver {
         context.getApplicationContext().sendBroadcast(intentSleep);
     }
 }
-
