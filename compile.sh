03e841d3c45e89d4f3d55ab5936ff76bda09794e
Martin Storsjö <martin@martin.st>
2013-02-04 16:43:07 +0200

    Bump the tested hash to the latest one from today
    This includes support for WMV1/2/3 in the omxil decoder, fixes
    a few other omxil decoder bugs, and disables a few known-broken
    omxil audio codec mappings.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- compile.sh
+++ compile.sh
@@ -78,7 +78,7 @@ NDK_TOOLCHAIN_PATH=${ANDROID_NDK}/toolchains/${PATH_HOST}-${GCCVER}/prebuilt/`un
 export PATH=${NDK_TOOLCHAIN_PATH}:${PATH}
 
 # 1/ libvlc, libvlccore and its plugins
-TESTED_HASH=0912461
+TESTED_HASH=9b0916a50e2
 if [ ! -d "vlc" ]; then
     echo "VLC source not found, cloning"
     git clone git://git.videolan.org/vlc.git vlc
