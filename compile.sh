c95dc3138e9280c2beb7a721d23fb385ffd6a511
Jean-Baptiste Kempf <jb@videolan.org>
2013-02-18 04:20:43 +0100

    Update tested hash (again :) )
    OpenSL update and OMX fixes :)

--- compile.sh
+++ compile.sh
@@ -78,7 +78,7 @@ NDK_TOOLCHAIN_PATH=${ANDROID_NDK}/toolchains/${PATH_HOST}-${GCCVER}/prebuilt/`un
 export PATH=${NDK_TOOLCHAIN_PATH}:${PATH}
 
 # 1/ libvlc, libvlccore and its plugins
-TESTED_HASH=1ba853a
+TESTED_HASH=010dc8c
 if [ ! -d "vlc" ]; then
     echo "VLC source not found, cloning"
     git clone git://git.videolan.org/vlc.git vlc
