0a6b5ac8ca02284feb6643cb0b98f8a2781e58e9
Sébastien Toque <xilasz@gmail.com>
2013-04-02 20:15:48 +0200

    UI: introduce ExpandableLayout

---
+++ vlc-android/src/org/videolan/vlc/widget/ExpandableLayout.java
@@ -0,0 +1,99 @@
+/*****************************************************************************
+ * ExpandableLayout.java
+ *****************************************************************************
+ * Copyright © 2013 VLC authors and VideoLAN
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+package org.videolan.vlc.widget;
+
+import org.videolan.vlc.R;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class ExpandableLayout extends LinearLayout {
+
+    private final View mHeaderLayout;
+    private final ImageView mIcon;
+    private final TextView mTitle;
+    private final TextView mText;
+    private final ImageView mMore;
+    private final LinearLayout mContent;
+    private Boolean mExpanded;
+
+    public ExpandableLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        LayoutInflater.from(context).inflate(R.layout.expandable_layout, this, true);
+
+        mHeaderLayout = findViewById(R.id.header_layout);
+        mIcon = (ImageView) findViewById(R.id.icon);
+        mTitle = (TextView) findViewById(R.id.title);
+        mText = (TextView) findViewById(R.id.text);
+        mMore = (ImageView) findViewById(R.id.more);
+        mContent = (LinearLayout) findViewById(R.id.content);
+
+        mHeaderLayout.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SetState(!mExpanded);
+            }
+        });
+
+        SetState(isInEditMode());
+    }
+
+    private void SetState(Boolean expanded) {
+        mExpanded = expanded;
+        mMore.setImageResource(expanded ? R.drawable.ic_up : R.drawable.ic_down);
+        mContent.setVisibility(expanded ? View.VISIBLE : View.GONE);
+    }
+
+    public void setTitle(int resid) {
+        mTitle.setText(resid);
+    }
+
+    public void setText(String text) {
+        mText.setText(text);
+        mText.setVisibility(text != null ? View.VISIBLE : View.GONE);
+    }
+
+    public void setIcon(int resid) {
+        mIcon.setImageResource(resid);
+        mIcon.setVisibility(View.VISIBLE);
+    }
+
+    public void setContent(Context context, int resid) {
+        View view = LayoutInflater.from(context).inflate(resid, null, true);
+        mContent.addView(view);
+    }
+
+    public void Expand()
+    {
+        SetState(true);
+    }
+
+    public void Collapse()
+    {
+        SetState(false);
+    }
+}
