9d08e7628515efcad9048e8a3cad2f24aa1c8db1
Martin Storsjö <martin@martin.st>
2013-04-16 18:44:18 +0300

    Revert "Set SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION in the video player"
    This reverts commit a12edf9d192b79f3b8fa0044395bf66ae8afc5db and
    a8822b341483da26338926058831c2076e2377cd.
    
    If this flag is set, the layout needs to be adjusted to make sure
    that the vlc controls aren't laid out under the navigation bar.
    That is, we'd want the effect of this flag on the video surface,
    but not on the rest of the video player UI.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- vlc-android/src/org/videolan/vlc/gui/video/VideoPlayerActivity.java
+++ vlc-android/src/org/videolan/vlc/gui/video/VideoPlayerActivity.java
@@ -1266,14 +1266,11 @@ public class VideoPlayerActivity extends Activity {
     private void dimStatusBar(boolean dim) {
         if (!Util.isHoneycombOrLater() || !Util.hasNavBar())
             return;
-        int layout = 0;
-        if (!Util.hasCombBar() && Util.isJellyBeanOrLater())
-            layout = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
         mSurface.setSystemUiVisibility(
-                (dim ? (Util.hasCombBar()
+                dim ? (Util.hasCombBar()
                         ? View.SYSTEM_UI_FLAG_LOW_PROFILE
                         : View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
-                    : View.SYSTEM_UI_FLAG_VISIBLE) | layout);
+                    : View.SYSTEM_UI_FLAG_VISIBLE);
     }
 
     private void updateOverlayPausePlay() {
