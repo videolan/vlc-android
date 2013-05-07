be98da1606e1f3d56edbbc56ab115d52910c47f6
Jean-Baptiste Kempf <jb@videolan.org>
2013-05-07 15:51:53 +0200

    Update VLC hash

--- compile.sh
+++ compile.sh
@@ -80,7 +80,7 @@ export PATH=${NDK_TOOLCHAIN_PATH}:${PATH}
 ANDROID_PATH="`pwd`"
 
 # 1/ libvlc, libvlccore and its plugins
-TESTED_HASH=e8dc8779b
+TESTED_HASH=5648d4e
 if [ ! -d "vlc" ]; then
     echo "VLC source not found, cloning"
     git clone git://git.videolan.org/vlc.git vlc
