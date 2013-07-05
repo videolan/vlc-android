4161cd02306e99f5523850d86b42fd3a018fe126
Edward Wang <edward.c.wang@compdigitec.com>
2013-07-04 21:44:03 -0400

    HistoryFragment: add a context menu

--- vlc-android/src/org/videolan/vlc/gui/HistoryFragment.java
+++ vlc-android/src/org/videolan/vlc/gui/HistoryFragment.java
@@ -20,15 +20,21 @@
  *****************************************************************************/
 package org.videolan.vlc.gui;
 
+import org.videolan.libvlc.LibVLC;
 import org.videolan.vlc.AudioServiceController;
 import org.videolan.vlc.R;
 import org.videolan.vlc.gui.audio.AudioPlayerActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ListView;
 
 import com.actionbarsherlock.app.SherlockListFragment;
@@ -54,17 +60,49 @@ public class HistoryFragment extends SherlockListFragment {
     {
         View v = inflater.inflate(R.layout.history_list, container, false);
         setListAdapter(mHistoryAdapter);
+        final ListView listView = (ListView)v.findViewById(android.R.id.list);
+        registerForContextMenu(listView);
         return v;
     }
 
     @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+        MenuInflater menuInflater = getActivity().getMenuInflater();
+        menuInflater.inflate(R.menu.history_view, menu);
+    }
+
+    @Override
     public void onListItemClick(ListView l, View v, int p, long id) {
+        playListIndex(p);
+    }
+
+    private void playListIndex(int position) {
         AudioServiceController audioController = AudioServiceController.getInstance();
 
-        audioController.load(mHistoryAdapter.getAllURIs(), p, true, true);
+        audioController.load(mHistoryAdapter.getAllURIs(), position, true, true);
         Intent intent = new Intent(getActivity(), AudioPlayerActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if(!getUserVisibleHint()) return super.onContextItemSelected(item);
+
+        AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+        if(info == null) // info can be null
+            return super.onContextItemSelected(item);
+        int id = item.getItemId();
+
+        if(id == R.id.history_view_play) {
+            playListIndex(info.position);
+            return true;
+        } else if(id == R.id.history_view_delete) {
+            LibVLC.getExistingInstance().removeIndex(info.position);
+            mHistoryAdapter.notifyDataSetChanged();
+            return true;
+        }
+        return super.onContextItemSelected(item);
     }
 
     public void refresh() {
