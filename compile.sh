b13c550fb6e32da8b2f84e15e221f7be20e27883
Rafaël Carré <funman@videolan.org>
2013-08-12 17:03:27 +0200

    No need to match NDKr9 and NDKr9a differently (for now)
    (cherry picked from commit 5bb852b6d50a695d4644d983bb203e3d67ee36c5)
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- compile.sh
+++ compile.sh
@@ -15,7 +15,7 @@ fi
 # try to detect NDK version
 REL=$(grep -o '^r[0-9]*.*' $ANDROID_NDK/RELEASE.TXT 2>/dev/null|cut -b2-)
 case "$REL" in
-    9?*)
+    9*)
         GCCVER=4.8
         CXXSTL="/"${GCCVER}
     ;;
