84e465c7e7e56b849c4f592590bca3ee5a51e99e
Edward Wang <edward.c.wang@compdigitec.com>
2013-08-29 16:20:34 +0200

    HistoryFragment: fix viewing from currently-playing list

--- vlc-android/src/org/videolan/vlc/gui/HistoryFragment.java
+++ vlc-android/src/org/videolan/vlc/gui/HistoryFragment.java
@@ -79,7 +79,8 @@ public class HistoryFragment extends SherlockListFragment {
     private void playListIndex(int position) {
         AudioServiceController audioController = AudioServiceController.getInstance();
 
-        audioController.load(mHistoryAdapter.getAllURIs(), position, true);
+        LibVLC.getExistingInstance().setMediaList();
+        audioController.playIndex(position);
         Intent intent = new Intent(getActivity(), AudioPlayerActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
