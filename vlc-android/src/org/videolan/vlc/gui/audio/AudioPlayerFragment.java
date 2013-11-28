eb1ed12f96f8cf6821d65cea41ef4a2e3812bec7
Adrien Maglo <magsoft@videolan.org>
2013-11-28 11:53:48 +0100

    Audio player: remove useless imports

--- vlc-android/src/org/videolan/vlc/gui/audio/AudioPlayerFragment.java
+++ vlc-android/src/org/videolan/vlc/gui/audio/AudioPlayerFragment.java
@@ -42,8 +42,6 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.media.AudioManager;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
