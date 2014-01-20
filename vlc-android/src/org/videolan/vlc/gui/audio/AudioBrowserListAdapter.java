063f16a8e029ef8dd091ff15f37426e5bf68f719
Edward Wang <edward.c.wang@compdigitec.com>
2014-01-20 16:29:24 -0500

    AudioSongsListAdapter: unused import

--- vlc-android/src/org/videolan/vlc/gui/audio/AudioBrowserListAdapter.java
+++ vlc-android/src/org/videolan/vlc/gui/audio/AudioBrowserListAdapter.java
@@ -36,7 +36,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.widget.BaseAdapter;
-import android.widget.LinearLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
