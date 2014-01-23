42e7c351cf8b7983a9dec96fad6d1756d2862018
Adrien Maglo <magsoft@videolan.org>
2014-01-23 22:20:10 +0100

    Revert "MediaList: signal events"
    This reverts commit d8391ce90038093fc33c406de6ba2cbe05c196d9.
    
    This commit breaks the playlist item reordering functionality. Indeed, an item move does not correspond to an addition and a deletion. If needed, a specific signal should be added for playlist moves.

--- vlc-android/src/org/videolan/libvlc/MediaList.java
+++ vlc-android/src/org/videolan/libvlc/MediaList.java
@@ -146,16 +146,11 @@ public class MediaList {
             throw new IndexOutOfBoundsException("Indexes out of range");
 
         MediaHolder toMove = mInternalList.get(startPosition);
-        String l = toMove.m.getLocation();
         mInternalList.remove(startPosition);
-        signal_list_event(EventHandler.CustomMediaListItemDeleted, startPosition, l);
-        if(startPosition >= endPosition) {
+        if (startPosition >= endPosition)
             mInternalList.add(endPosition, toMove);
-            signal_list_event(EventHandler.CustomMediaListItemAdded, endPosition, l);
-        } else {
+        else
             mInternalList.add(endPosition - 1, toMove);
-            signal_list_event(EventHandler.CustomMediaListItemAdded, endPosition - 1, l);
-        }
     }
 
     public void remove(int position) {
