eeaaaae3236aa8e5ba406adafc1782acaf330ddd
Adrien Maglo <magsoft@videolan.org>
2014-01-24 12:55:15 +0100

    Audio player playlist: fix a crash on API < 16

--- vlc-android/src/org/videolan/vlc/gui/audio/AudioPlaylistView.java
+++ vlc-android/src/org/videolan/vlc/gui/audio/AudioPlaylistView.java
@@ -55,7 +55,7 @@ public class AudioPlaylistView extends ListView {
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         super.onLayout(changed, l, t, r, b);
-        mDragShadow.layout(l, t, l + mDragShadow.getMeasuredWidth(), t + mDragShadow.getMinimumHeight());
+        mDragShadow.layout(l, t, l + mDragShadow.getMeasuredWidth(), t + mDragShadow.getMeasuredHeight());
     }
 
     @Override
