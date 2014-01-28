ea794400376fe50254619a50d33f8f3bda67242c
Rafaël Carré <funman@videolan.org>
2014-01-28 11:50:13 +0100

    No need to set --intf
    We're not using libvlc_add_intf

--- vlc-android/jni/libvlcjni.c
+++ vlc-android/jni/libvlcjni.c
@@ -261,7 +261,6 @@ void Java_org_videolan_libvlc_LibVLC_nativeInit(JNIEnv *env, jobject thiz)
 
     /* Don't add any invalid options, otherwise it causes LibVLC to crash */
     const char *argv[] = {
-        "-I", "dummy",
         "--no-osd",
         "--no-video-title-show",
         "--no-stats",
