882b3e2b933c5468c982a22b6a5e90c09c0bee36
Rafaël Carré <funman@videolan.org>
2014-01-31 11:56:46 +0100

    rename_package.sh: fix script
    Remove file backups left by sed -i.orig

--- rename_package.sh
+++ rename_package.sh
@@ -9,17 +9,20 @@ NEW_NAME=org.videolan.vlc.$1
 
 echo $NEW_NAME $2
 
-sed -i.orig -e "s/versionCode\(.*\)0\"/versionCode\1$2\"/" vlc-android/AndroidManifest.xml
-
 OLD_PATH=$(echo $OLD_NAME |sed 's/\./\//g')
 NEW_PATH=$(echo $NEW_NAME |sed 's/\./\//g')
 
-mv vlc-android/src/${OLD_PATH} vlc-android/src/tmp
-mkdir -p vlc-android/src/${OLD_PATH}
-mv vlc-android/src/tmp vlc-android/src/${NEW_PATH}
-
-find vlc-android \( -name "*.xml" -o -name "*.java" -o -name "*.cfg" -o -name "*.aidl" \) -print0 | xargs -0 sed -i.orig "s/${OLD_NAME}/${NEW_NAME}/g"
-find vlc-android \( -name "*.c" \) -print0 | xargs -0 sed -i.orig "s,${OLD_PATH},${NEW_PATH},g"
 sed -i.orig "s,${OLD_PATH},${NEW_PATH},g" Makefile
 
-find vlc-android/jni -type f -print0 | xargs -0  sed -i.orig "s,$(echo $OLD_NAME |sed 's/\./_/g'),$(echo $NEW_NAME |sed 's/\./_/g'),g"
+cd vlc-android/
+
+sed -i.orig -e "s/versionCode\(.*\)0\"/versionCode\1$2\"/" AndroidManifest.xml
+
+mv src/${OLD_PATH} src/tmp
+mkdir -p src/${OLD_PATH}
+mv src/tmp src/${NEW_PATH}
+
+find . \( -name "*.xml" -o -name "*.java" -o -name "*.cfg" -o -name "*.aidl" \) -print0 | xargs -0 sed -i.orig "s/${OLD_NAME}/${NEW_NAME}/g"
+find . \( -name "*.c" \) -print0 | xargs -0 sed -i.orig "s,${OLD_PATH},${NEW_PATH},g"
+find jni -type f -print0 | xargs -0  sed -i.orig "s,$(echo $OLD_NAME |sed 's/\./_/g'),$(echo $NEW_NAME |sed 's/\./_/g'),g"
+find . -name \*.orig -exec rm -f {} \;
