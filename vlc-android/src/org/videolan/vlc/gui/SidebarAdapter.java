ca0060f70222f5da4c6c91892612e62ff528cd86
Adrien Maglo <magsoft@videolan.org>
2014-03-17 15:35:40 +0100

    SidebarAdapter: remove a useless constructor

--- vlc-android/src/org/videolan/vlc/gui/SidebarAdapter.java
+++ vlc-android/src/org/videolan/vlc/gui/SidebarAdapter.java
@@ -49,12 +49,6 @@ public class SidebarAdapter extends BaseAdapter {
         String name;
         int drawableID;
 
-        public SidebarEntry(String _id, String _name, int _drawableID) {
-            this.id = _id;
-            this.name = _name;
-            this.drawableID = _drawableID;
-        }
-
         public SidebarEntry(String _id, int _name, int _drawableID) {
             this.id = _id;
             this.name = VLCApplication.getAppContext().getString(_name);
