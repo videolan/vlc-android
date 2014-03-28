2a77e9781280725510ef63aaf862de4755f0909a
Adrien Maglo <magsoft@videolan.org>
2014-03-28 12:34:49 +0100

    Update tested hash

--- compile.sh
+++ compile.sh
@@ -108,7 +108,7 @@ ANDROID_PATH="`pwd`"
 if [ ! -z "$FETCH" ]
 then
     # 1/ libvlc, libvlccore and its plugins
-    TESTED_HASH=4ec53cf7
+    TESTED_HASH=8d6be5e2
     if [ ! -d "vlc" ]; then
         echo "VLC source not found, cloning"
         git clone git://git.videolan.org/vlc.git vlc
