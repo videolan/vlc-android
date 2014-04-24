cda21543c88e1814b9906dfa31cd04bfa0e7f6c7
Adrien Maglo <magsoft@videolan.org>
2014-04-24 17:51:21 +0200

    Update the TESTED_HASH to fix WMA playback

--- compile.sh
+++ compile.sh
@@ -108,7 +108,7 @@ ANDROID_PATH="`pwd`"
 if [ ! -z "$FETCH" ]
 then
     # 1/ libvlc, libvlccore and its plugins
-    TESTED_HASH=0b4c9bc
+    TESTED_HASH=3b7bfa1
     if [ ! -d "vlc" ]; then
         echo "VLC source not found, cloning"
         git clone git://git.videolan.org/vlc.git vlc
