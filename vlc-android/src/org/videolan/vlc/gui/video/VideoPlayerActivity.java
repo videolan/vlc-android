50023f410d65f24a4bea5bc20615774f1172e2e1
Alexandre Perraud <4leyx4ndre@gmail.com>
2014-04-29 12:16:06 +0200

    Change text (again) on Dual Display Mode
    The previous text was not clear about the cloning (it's not only
    displaying video on the other screen, it's a clone)
    The previous text was too negative. :)
    The previous was too Engrish :)
    
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- vlc-android/src/org/videolan/vlc/gui/video/VideoPlayerActivity.java
+++ vlc-android/src/org/videolan/vlc/gui/video/VideoPlayerActivity.java
@@ -166,7 +166,7 @@ public class VideoPlayerActivity extends Activity implements IVideoPlayer {
     private ImageButton mForward;
     private boolean mEnableJumpButtons;
     private boolean mEnableBrightnessGesture;
-    private boolean mEnableDualDisplay;
+    private boolean mEnableCloneMode;
     private boolean mDisplayRemainingTime = false;
     private int mScreenOrientation;
     private ImageButton mAudioTrack;
@@ -249,7 +249,7 @@ public class VideoPlayerActivity extends Activity implements IVideoPlayer {
 
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
 
-        mEnableDualDisplay = pref.getBoolean("enable_dual_display", false);
+        mEnableCloneMode = pref.getBoolean("enable_clone_mode", false);
         createPresentation();
         setContentView(mPresentation == null ? R.layout.player : R.layout.player_remote_control);
 
@@ -1987,7 +1987,7 @@ public class VideoPlayerActivity extends Activity implements IVideoPlayer {
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     private void createPresentation() {
-        if (mMediaRouter == null || !mEnableDualDisplay)
+        if (mMediaRouter == null || mEnableCloneMode)
             return;
 
         // Get the current route and its presentation display.
