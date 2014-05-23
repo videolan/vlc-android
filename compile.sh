de1641dd0ea5c8ef13b5080b18566453e03cb4b7
Jean-Baptiste Kempf <jb@videolan.org>
2014-05-23 11:58:11 +0200

    Update TESTED_HASH post-NAN fix

--- compile.sh
+++ compile.sh
@@ -108,7 +108,7 @@ ANDROID_PATH="`pwd`"
 if [ ! -z "$FETCH" ]
 then
     # 1/ libvlc, libvlccore and its plugins
-    TESTED_HASH=2e8d457
+    TESTED_HASH=602d02a
     if [ ! -d "vlc" ]; then
         echo "VLC source not found, cloning"
         git clone git://git.videolan.org/vlc.git vlc
