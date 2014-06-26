0d15e20caa846c086cfa5b0fa3b80f3b560b9cd2
Rafaël Carré <funman@videolan.org>
2014-06-26 17:03:12 +0200

    Ignore build directories

--- compile.sh
+++ compile.sh
@@ -113,6 +113,8 @@ then
         echo "VLC source not found, cloning"
         git clone git://git.videolan.org/vlc/vlc-2.2.git vlc
         cd vlc
+        echo android/ >> .git/info/exclude
+        echo contrib/android/ >> .git/info/exclude
         git checkout -B android ${TESTED_HASH}
     else
         echo "VLC source found"
