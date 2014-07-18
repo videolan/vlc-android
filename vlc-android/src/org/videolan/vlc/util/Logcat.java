c251827bc98f2ee2c08d01a595fa63d1c4f59387
Jean-Baptiste Kempf <jb@videolan.org>
2014-07-18 22:33:01 +0200

    Logcat: catch FileNotFoundException too

--- vlc-android/src/org/videolan/vlc/util/Logcat.java
+++ vlc-android/src/org/videolan/vlc/util/Logcat.java
@@ -22,6 +22,7 @@ package org.videolan.vlc.util;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -40,15 +41,22 @@ public class Logcat {
         String[] args = { "logcat", "-v", "time", "-d" };
 
         Process process = Runtime.getRuntime().exec(args);
-        InputStreamReader input = new InputStreamReader(
-                process.getInputStream());
-        OutputStreamWriter output = new OutputStreamWriter(
-                new FileOutputStream(filename));
+
+        InputStreamReader input = new InputStreamReader(process.getInputStream());
+
+        FileOutputStream fileStream;
+        try {
+            fileStream = new FileOutputStream(filename);
+        } catch( FileNotFoundException e) {
+            return;
+        }
+
+        OutputStreamWriter output = new OutputStreamWriter(fileStream);
         BufferedReader br = new BufferedReader(input);
         BufferedWriter bw = new BufferedWriter(output);
-        String line;
 
         try {
+            String line;
             while ((line = br.readLine()) != null) {
                 bw.write(line);
                 bw.newLine();
