ffb923c99fb6126e7166a5080aba2e320b6feeb9
Martin Storsjö <martin@martin.st>
2014-07-24 23:20:26 +0300

    Don't use iomx-ics on jbmr2 (4.3)
    For the normal, non-direct-rendering iomx, the headers from ICS work
    on 4.1 and 4.2, but not on 4.3.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- vlc-android/src/org/videolan/libvlc/LibVLC.java
+++ vlc-android/src/org/videolan/libvlc/LibVLC.java
@@ -100,7 +100,7 @@ public class LibVLC {
                 System.loadLibrary("iomx-gingerbread");
             else if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.HONEYCOMB_MR2)
                 System.loadLibrary("iomx-hc");
-            else if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.JELLY_BEAN_MR2)
+            else if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.JELLY_BEAN_MR1)
                 System.loadLibrary("iomx-ics");
         } catch (Throwable t) {
             Log.w(TAG, "Unable to load the iomx library: " + t);
