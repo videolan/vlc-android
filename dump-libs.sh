a853973dce46ee1f7525f1abaebd85844ebaee4a
Martin Storsjö <martin@martin.st>
2014-07-25 23:22:57 +0300

    Include more symbols in libui from the honeycomb version of this library
    For input, libui.so from a 4.0 build is used, and libui-hc.so from
    a 3.2 build. The generated symbol list in libui.symbols contains
    symbols from both, to allow linking for all generations of libui.so.
    
    Signed-off-by: Martin Storsjö <martin@martin.st>

--- dump-libs.sh
+++ dump-libs.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-for i in stagefright media utils cutils binder ui; do
+for i in stagefright media utils cutils binder ui ui-hc; do
 	nm -D --defined-only lib$i.so | awk '{print $3}' | grep -v ^__aeabi | grep '\(IInterface\|OMXObserver\|MemoryDealer\|Binder\|RefBase\|String\|OMXClient\|IMemory\|GraphicBuffer\|android_atomic\|^str\)' > lib$i.symbols
 done
+
+cat libui.symbols libui-hc.symbols | sort | uniq > tmp
+mv tmp libui.symbols
+rm -f libui-hc.symbols
 
