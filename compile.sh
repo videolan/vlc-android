030a20a1695e61a2150daaeebb3ccfec6ccbedd6
Jean-Baptiste Kempf <jb@videolan.org>
2014-08-15 20:42:52 +0200

    Update HASH to fix m4a and avi regressions

--- compile.sh
+++ compile.sh
@@ -96,7 +96,7 @@ ANDROID_PATH="`pwd`"
 if [ ! -z "$FETCH" ]
 then
     # 1/ libvlc, libvlccore and its plugins
-    TESTED_HASH=f2db364
+    TESTED_HASH=39daee3
     if [ ! -d "vlc" ]; then
         echo "VLC source not found, cloning"
         git clone git://git.videolan.org/vlc/vlc-2.2.git vlc
