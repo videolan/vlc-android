35499b5452ecf23e067b7bc9cc22d0f421b88c8f
Thomas Guillem <thomas.guillem@gmail.com>
2014-10-03 17:42:42 +0200

    Video Player: always set FLAG_LOW_PROFILE on hide
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- vlc-android/src/org/videolan/vlc/gui/video/VideoPlayerActivity.java
+++ vlc-android/src/org/videolan/vlc/gui/video/VideoPlayerActivity.java
@@ -1827,9 +1827,8 @@ public class VideoPlayerActivity extends Activity implements IVideoPlayer {
 
         int visibility = layout;
         if (dim) {
-            if (AndroidDevices.hasCombBar()) {
-                visibility |= View.SYSTEM_UI_FLAG_LOW_PROFILE;
-            } else {
+            visibility |= View.SYSTEM_UI_FLAG_LOW_PROFILE;
+            if (!AndroidDevices.hasCombBar()) {
                 visibility |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
                 if (mOverlayUseStatusBar)
                     visibility |= View.SYSTEM_UI_FLAG_FULLSCREEN;
