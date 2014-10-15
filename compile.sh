2d27bceef9e04d82525fd8a9c4f8f66644034bba
Jean-Baptiste Kempf <jb@videolan.org>
2014-10-15 10:51:57 +0200

    Update the TESTED_HASH for mediacodec Android-L compat

--- compile.sh
+++ compile.sh
@@ -96,7 +96,7 @@ ANDROID_PATH="`pwd`"
 if [ ! -z "$FETCH" ]
 then
     # 1/ libvlc, libvlccore and its plugins
-    TESTED_HASH=7d4f14c
+    TESTED_HASH=e33e5de
     if [ ! -d "vlc" ]; then
         echo "VLC source not found, cloning"
         git clone git://git.videolan.org/vlc/vlc-2.2.git vlc
