425d7939ff0c8b2f01706dfd37b56650cf7c7da9
Thomas Guillem <tom@gllm.fr>
2014-10-16 09:41:46 +0200

    Update tested hash
    Signed-off-by: Jean-Baptiste Kempf <jb@videolan.org>

--- compile.sh
+++ compile.sh
@@ -125,7 +125,7 @@ ANDROID_PATH="`pwd`"
 if [ ! -z "$FETCH" ]
 then
     # 1/ libvlc, libvlccore and its plugins
-    TESTED_HASH=5636603
+    TESTED_HASH=6e2f490
     if [ ! -d "vlc" ]; then
         echo "VLC source not found, cloning"
         git clone git://git.videolan.org/vlc.git vlc
