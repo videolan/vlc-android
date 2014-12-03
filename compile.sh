4d2c30fa66b1f03af3a300f48cbcfbce6e25319c
Jean-Baptiste Kempf <jb@videolan.org>
2014-12-03 16:16:26 +0100

    Update HASH from vlc-2.2
    Ref #12870

--- compile.sh
+++ compile.sh
@@ -96,7 +96,7 @@ ANDROID_PATH="`pwd`"
 if [ ! -z "$FETCH" ]
 then
     # 1/ libvlc, libvlccore and its plugins
-    TESTED_HASH=0f9d349
+    TESTED_HASH=036010e
     if [ ! -d "vlc" ]; then
         echo "VLC source not found, cloning"
         git clone git://git.videolan.org/vlc/vlc-2.2.git vlc
