1c5f32cf6815ac9f73d8a941a673db4d3c988e0a
Geoffrey Métais <geoffrey.metais@gmail.com>
2014-12-16 17:55:08 +0100

    Fix status bar display

--- vlc-android/src/org/videolan/vlc/gui/video/VideoPlayerActivity.java
+++ vlc-android/src/org/videolan/vlc/gui/video/VideoPlayerActivity.java
@@ -2069,10 +2069,7 @@ public class VideoPlayerActivity extends ActionBarActivity implements IVideoPlay
 
         if (!AndroidDevices.hasCombBar() && LibVlcUtil.isJellyBeanOrLater()) {
             visibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
-            navbar = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
         }
-        visibility |= View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
-
         if (dim) {
             navbar |= View.SYSTEM_UI_FLAG_LOW_PROFILE;
             if (!AndroidDevices.hasCombBar()) {
@@ -2082,6 +2079,7 @@ public class VideoPlayerActivity extends ActionBarActivity implements IVideoPlay
                 visibility |= View.SYSTEM_UI_FLAG_FULLSCREEN;
             }
         } else {
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
             visibility |= View.SYSTEM_UI_FLAG_VISIBLE;
         }
 
