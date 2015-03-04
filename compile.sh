2b1fc4b65ce48623a7af6d12f6513ed83e98f429
Jean-Baptiste Kempf <jb@videolan.org>
2015-03-04 10:04:58 +0100

    Update VLC for AudioTrack issues

--- compile.sh
+++ compile.sh
@@ -131,7 +131,7 @@ fi
 # Fetch VLC source #
 ####################
 
-TESTED_HASH=96e66d5
+TESTED_HASH=a9b19e4
 if [ ! -d "vlc" ]; then
     echo "VLC source not found, cloning"
     git clone git://git.videolan.org/vlc.git vlc
