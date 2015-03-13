623bd9f9873b466a1dd8d20cd04e5f7a072b5878
Geoffrey Métais <geoffrey.metais@gmail.com>
2015-03-13 18:26:14 +0100

    Auto hide network when device is disconnected

--- vlc-android/tv/src/org/videolan/vlc/gui/tv/MainTvActivity.java
+++ vlc-android/tv/src/org/videolan/vlc/gui/tv/MainTvActivity.java
@@ -38,13 +38,19 @@ import org.videolan.vlc.gui.tv.browser.MusicFragment;
 import org.videolan.vlc.gui.tv.browser.VerticalGridActivity;
 import org.videolan.vlc.interfaces.IVideoBrowser;
 import org.videolan.vlc.gui.video.VideoListHandler;
+import org.videolan.vlc.util.AndroidDevices;
 import org.videolan.vlc.util.VLCInstance;
 
 import android.app.Activity;
 import android.app.FragmentManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
@@ -89,6 +95,7 @@ public class MainTvActivity extends Activity implements IVideoBrowser, OnItemVie
     ArrayObjectAdapter mCategoriesAdapter;
     ArrayObjectAdapter mNetworkAdapter;
     ArrayObjectAdapter mOtherAdapter;
+    View mRootContainer;
     HashMap<String, Integer> mVideoIndex;
     Drawable mDefaultBackground;
     Activity mContext;
@@ -137,6 +144,7 @@ public class MainTvActivity extends Activity implements IVideoBrowser, OnItemVie
         mBrowseFragment.setOnItemViewSelectedListener(this);
 
         mBrowseFragment.setOnSearchClickedListener(this);
+        mRootContainer = mBrowseFragment.getView();
         mMediaLibrary.loadMediaItems(this, true);
         BackgroundManager.getInstance(this).attach(getWindow());
 
@@ -148,10 +156,12 @@ public class MainTvActivity extends Activity implements IVideoBrowser, OnItemVie
     public void onResume() {
         super.onResume();
         mMediaLibrary.addUpdateHandler(mHandler);
-        if (!mMediaLibrary.isWorking())
-            updateList();
         if (sThumbnailer != null)
             sThumbnailer.setVideoBrowser(this);
+
+        //Handle network connection state
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(networkReceiver, filter);
     }
 
     public void onPause() {
@@ -162,6 +172,7 @@ public class MainTvActivity extends Activity implements IVideoBrowser, OnItemVie
         if (sThumbnailer != null)
             sThumbnailer.setVideoBrowser(null);
         mBarrier.reset();
+        unregisterReceiver(networkReceiver);
     }
 
     @Override
@@ -284,11 +295,11 @@ public class MainTvActivity extends Activity implements IVideoBrowser, OnItemVie
             mRowsAdapter = new ArrayObjectAdapter(new ListRowPresenter());
             mProgressBar.setVisibility(View.VISIBLE);
         }
+
         @Override
         protected Void doInBackground(Void... params) {
-            ArrayList<MediaWrapper> videoList = mMediaLibrary.getVideoItems();
+            final ArrayList<MediaWrapper> videoList = mMediaLibrary.getVideoItems();
             int size;
-            MediaWrapper item;
 
             // Update video section
             if (!videoList.isEmpty()) {
@@ -298,43 +309,67 @@ public class MainTvActivity extends Activity implements IVideoBrowser, OnItemVie
                         new CardPresenter(mContext));
                 if (NUM_ITEMS_PREVIEW < size)
                     size = NUM_ITEMS_PREVIEW;
-                for (int i = 0 ; i < size ; ++i) {
-                    item = videoList.get(i);
+                final int total = size;
+                final HeaderItem videoHeader = new HeaderItem(HEADER_VIDEO, getString(R.string.video), null);
+                mRootContainer.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        MediaWrapper item;
+                        for (int i = 0; i < total; ++i) {
+                            item = videoList.get(i);
 
-                    mVideoAdapter.add(item);
-                    mVideoIndex.put(item.getLocation(), i);
-                }
-                // Empty item to launch grid activity
-                mVideoAdapter.add(new CardPresenter.SimpleCard(0, "Browse more", R.drawable.ic_video_collection_big));
-
-                HeaderItem header = new HeaderItem(HEADER_VIDEO, getString(R.string.video), null);
-                mRowsAdapter.add(new ListRow(header, mVideoAdapter));
+                            mVideoAdapter.add(item);
+                            mVideoIndex.put(item.getLocation(), i);
+                        }
+                        // Empty item to launch grid activity
+                        mVideoAdapter.add(new CardPresenter.SimpleCard(0, "Browse more", R.drawable.ic_video_collection_big));
+                        mRowsAdapter.add(new ListRow(videoHeader, mVideoAdapter));
+                    }
+                });
             }
 
             mCategoriesAdapter = new ArrayObjectAdapter(new CardPresenter(mContext));
-            mCategoriesAdapter.add(new CardPresenter.SimpleCard(MusicFragment.CATEGORY_ARTISTS, getString(R.string.artists), R.drawable.ic_artist_big));
-            mCategoriesAdapter.add(new CardPresenter.SimpleCard(MusicFragment.CATEGORY_ALBUMS, getString(R.string.albums), R.drawable.ic_album_big));
-            mCategoriesAdapter.add(new CardPresenter.SimpleCard(MusicFragment.CATEGORY_GENRES, getString(R.string.genres), R.drawable.ic_genre_big));
-            mCategoriesAdapter.add(new CardPresenter.SimpleCard(MusicFragment.CATEGORY_SONGS, getString(R.string.songs), R.drawable.ic_song_big));
-            HeaderItem header = new HeaderItem(HEADER_CATEGORIES, getString(R.string.audio), null);
-            mRowsAdapter.add(new ListRow(header, mCategoriesAdapter));
-
-            mNetworkAdapter = new ArrayObjectAdapter(new CardPresenter(mContext));
-            mNetworkAdapter.add(new CardPresenter.SimpleCard(0, getString(R.string.network_browsing), R.drawable.ic_menu_network_big));
-            ArrayList<MediaWrapper> favs = MediaDatabase.getInstance().getAllNetworkFav();
-            if (!favs.isEmpty()) {
-                for (MediaWrapper fav : favs) {
-                    mNetworkAdapter.add(fav);
+            final HeaderItem musicHeader = new HeaderItem(HEADER_CATEGORIES, getString(R.string.audio), null);
+            mRootContainer.post(new Runnable() {
+                @Override
+                public void run() {
+                    mCategoriesAdapter.add(new CardPresenter.SimpleCard(MusicFragment.CATEGORY_ARTISTS, getString(R.string.artists), R.drawable.ic_artist_big));
+                    mCategoriesAdapter.add(new CardPresenter.SimpleCard(MusicFragment.CATEGORY_ALBUMS, getString(R.string.albums), R.drawable.ic_album_big));
+                    mCategoriesAdapter.add(new CardPresenter.SimpleCard(MusicFragment.CATEGORY_GENRES, getString(R.string.genres), R.drawable.ic_genre_big));
+                    mCategoriesAdapter.add(new CardPresenter.SimpleCard(MusicFragment.CATEGORY_SONGS, getString(R.string.songs), R.drawable.ic_song_big));
+                    mRowsAdapter.add(new ListRow(musicHeader, mCategoriesAdapter));
                 }
-            }
-            header = new HeaderItem(HEADER_NETWORK, getString(R.string.network_browsing), null);
-            mRowsAdapter.add(new ListRow(header, mNetworkAdapter));
+            });
 
+            if (AndroidDevices.hasLANConnection()) {
+                mNetworkAdapter = new ArrayObjectAdapter(new CardPresenter(mContext));
+                final ArrayList<MediaWrapper> favs = MediaDatabase.getInstance().getAllNetworkFav();
+                final HeaderItem networkHeader = new HeaderItem(HEADER_NETWORK, getString(R.string.network_browsing), null);
+
+                mRootContainer.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mNetworkAdapter.add(new CardPresenter.SimpleCard(0, getString(R.string.network_browsing), R.drawable.ic_menu_network_big));
+                        if (!favs.isEmpty()) {
+                            for (MediaWrapper fav : favs) {
+                                mNetworkAdapter.add(fav);
+                            }
+                        }
+                        mRowsAdapter.add(new ListRow(networkHeader, mNetworkAdapter));
+                    }
+                });
+            }
 
             mOtherAdapter = new ArrayObjectAdapter(new CardPresenter(mContext));
-            mOtherAdapter.add(new CardPresenter.SimpleCard(0, getString(R.string.preferences), R.drawable.ic_menu_preferences_big));
-            header = new HeaderItem(HEADER_MISC, getString(R.string.other), null);
-            mRowsAdapter.add(new ListRow(header, mOtherAdapter));
+            final HeaderItem miscHeader = new HeaderItem(HEADER_MISC, getString(R.string.other), null);
+
+            mRootContainer.post(new Runnable() {
+                @Override
+                public void run() {
+                    mOtherAdapter.add(new CardPresenter.SimpleCard(0, getString(R.string.preferences), R.drawable.ic_menu_preferences_big));
+                    mRowsAdapter.add(new ListRow(miscHeader, mOtherAdapter));
+                }
+            });
 
             return null;
         }
@@ -370,4 +405,29 @@ public class MainTvActivity extends Activity implements IVideoBrowser, OnItemVie
     public static Thumbnailer getThumbnailer(){
         return sThumbnailer;
     }
+
+    private final BroadcastReceiver networkReceiver = new BroadcastReceiver() {
+        boolean connected = true;
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (mMediaLibrary.isWorking())
+                return;
+            String action = intent.getAction();
+            if (ConnectivityManager.CONNECTIVITY_ACTION.equals(action)) {
+                final NetworkInfo networkInfo = ((ConnectivityManager)context.getSystemService(
+                        Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
+                if (networkInfo == null || networkInfo.getState() == NetworkInfo.State.CONNECTED) {
+                    if (networkInfo == null){
+                        if (connected)
+                            connected = false;
+                        else
+                            return; //block consecutive calls when disconnected
+                    } else
+                        connected = true;
+                    updateList();
+                }
+
+            }
+        }
+    };
 }
