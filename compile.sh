23a7873061408f2e119a3d543998a40245ff1279
Jean-Baptiste Kempf <jb@videolan.org>
2015-03-17 14:06:14 +0100

    Update vlc.git HASH to have the mediacodec fix

--- compile.sh
+++ compile.sh
@@ -131,7 +131,7 @@ fi
 # Fetch VLC source #
 ####################
 
-TESTED_HASH=e6b4585
+TESTED_HASH=551b670
 if [ ! -d "vlc" ]; then
     echo "VLC source not found, cloning"
     git clone git://git.videolan.org/vlc.git vlc
