3ba055e32c2f00c0b722305ce795bd6e949b7dda
Thomas Guillem <thomas@gllm.fr>
2015-04-30 15:14:45 +0200

    build: remove unused export

--- compile-libvlc.sh
+++ compile-libvlc.sh
@@ -462,8 +462,6 @@ checkfail "contribs: bootstrap failed"
 
 echo "EXTRA_CFLAGS= -g ${EXTRA_CFLAGS}" >> config.mak
 echo "EXTRA_LDFLAGS= ${EXTRA_LDFLAGS}" >> config.mak
-export VLC_EXTRA_CFLAGS="${EXTRA_CFLAGS}"                   # Makefile
-export VLC_EXTRA_LDFLAGS="${EXTRA_LDFLAGS}"                 # Makefile
 
 make fetch
 checkfail "contribs: make fetch failed"
@@ -513,7 +511,7 @@ if [ ${ANDROID_ABI} = "x86" -a ${ANDROID_API} != "android-21" ] ; then
 fi
 if [ ! -e ./config.h -o "$RELEASE" = 1 ]; then
 CPPFLAGS="$CPPFLAGS" \
-CFLAGS="$CFLAGS ${VLC_EXTRA_CFLAGS} ${EXTRA_CFLAGS}" \
+CFLAGS="$CFLAGS ${EXTRA_CFLAGS}" \
 CXXFLAGS="$CFLAGS" \
 LDFLAGS="$LDFLAGS" \
 CC="${CROSS_COMPILE}gcc --sysroot=${SYSROOT}" \
@@ -631,7 +629,7 @@ $ANDROID_NDK/ndk-build -C libvlc \
     VLC_BUILD_DIR="$VLC_SRC_DIR/$VLC_BUILD_DIR" \
     VLC_CONTRIB="$VLC_CONTRIB" \
     VLC_MODULES="$VLC_MODULES" \
-    TARGET_CFLAGS="$VLC_EXTRA_CFLAGS" \
+    TARGET_CFLAGS="$EXTRA_CFLAGS" \
     EXTRA_LDFLAGS="$EXTRA_LDFLAGS" \
     LIBVLC_LIBS="$LIBVLC_LIBS" \
     LIBIOMX_LIBS="$LIBIOMX_LIBS" \
