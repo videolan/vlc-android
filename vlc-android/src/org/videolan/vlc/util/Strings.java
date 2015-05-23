cf9824ebd4d820c5018c6255d7bbf6c3d1e57fcd
Jean-Baptiste Kempf <jb@videolan.org>
2015-05-23 17:46:33 +0200

    Strings: Use KiB/MiB over kB/MB

--- vlc-android/src/org/videolan/vlc/util/Strings.java
+++ vlc-android/src/org/videolan/vlc/util/Strings.java
@@ -119,7 +119,7 @@ public class Strings {
 
     public static String readableFileSize(long size) {
         if(size <= 0) return "0";
-        final String[] units = new String[] { "B", "kB", "MB", "GB", "TB" };
+        final String[] units = new String[] { "B", "KiB", "MiB", "GiB", "TiB" };
         int digitGroups = (int) (Math.log10(size)/Math.log10(1024));
         return new DecimalFormat("#,##0.#").format(size/Math.pow(1024, digitGroups)) + " " + units[digitGroups];
     }
