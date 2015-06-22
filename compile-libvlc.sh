8be93785a25e36c32378f79ea4b7a86926115d62
Thomas Guillem <thomas@gllm.fr>
2015-06-22 11:00:43 +0200

    build: use gcc 4.9

--- compile-libvlc.sh
+++ compile-libvlc.sh
@@ -281,11 +281,10 @@ fi
 REL=$(grep -o '^r[0-9]*.*' $ANDROID_NDK/RELEASE.TXT 2>/dev/null|cut -b2-)
 case "$REL" in
     10*)
+        GCCVER=4.9
         if [ "${HAVE_64}" = 1 ];then
-            GCCVER=4.9
             ANDROID_API=android-21
         else
-            GCCVER=4.8
             ANDROID_API=android-9
         fi
     ;;
