c5e98c098f82b74b14bb53a58d5e2ead31552c47
Thomas Guillem <thomas@gllm.fr>
2015-06-22 16:50:48 +0200

    VLCOptions: fix typo breaking chroma option

--- vlc-android/src/org/videolan/vlc/util/VLCOptions.java
+++ vlc-android/src/org/videolan/vlc/util/VLCOptions.java
@@ -104,7 +104,7 @@ public class VLCOptions {
             options.add("--network-caching=" + networkCaching);
         options.add(aout == AOUT_OPENSLES ? "--aout=opensles" : (aout == AOUT_AUDIOTRACK ? "--aout=android_audiotrack" : "--aout=dummy"));
         options.add("--androidwindow-chroma");
-        options.add(chroma.indexOf(0) == 0 ? chroma : "RV32");
+        options.add(chroma.indexOf(0) != 0 ? chroma : "RV32");
 
         if (sHdmiAudioEnabled) {
             options.add("--spdif");
