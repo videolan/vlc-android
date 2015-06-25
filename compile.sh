586e64c859573575766028fbf00c9e70d5498f8f
Thomas Guillem <thomas@gllm.fr>
2015-06-25 19:10:20 +0200

    Update libVLC HASH

--- compile.sh
+++ compile.sh
@@ -131,7 +131,7 @@ fi
 # Fetch VLC source #
 ####################
 
-TESTED_HASH=ecfd0b7
+TESTED_HASH=e29f8ec
 if [ ! -d "vlc" ]; then
     echo "VLC source not found, cloning"
     git clone git://git.videolan.org/vlc.git vlc
