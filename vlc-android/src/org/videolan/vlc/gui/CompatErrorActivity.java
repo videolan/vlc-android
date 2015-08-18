2499b037d015a9fc18eef1162744d6d8f28d7a22
Jean-Baptiste Kempf <jb@videolan.org>
2015-08-18 13:12:21 +0200

    Remove the blacklist post

--- vlc-android/src/org/videolan/vlc/gui/CompatErrorActivity.java
+++ vlc-android/src/org/videolan/vlc/gui/CompatErrorActivity.java
@@ -6,13 +6,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
 import org.videolan.libvlc.util.VLCUtil;
 import org.videolan.vlc.R;
 
@@ -46,43 +39,5 @@ public class CompatErrorActivity extends Activity {
 
         TextView tv = (TextView)findViewById(R.id.errormsg);
         tv.setText(getResources().getString(R.string.error_message_is) + "\n" + errorMsg);
-
-        //AsyncHttpRequest asyncHttpRequest = new AsyncHttpRequest();
-        //asyncHttpRequest.execute(Build.MODEL, Build.DEVICE);
-    }
-
-    public static class AsyncHttpRequest extends AsyncTask<String, String, Boolean> {
-
-        public AsyncHttpRequest() { }
-
-        @Override
-        protected Boolean doInBackground(String... params) {
-            if (params[0].length() == 0)
-                return false;
-            HttpClient httpClient = new DefaultHttpClient();
-            HttpPost httpPost = new HttpPost("http://people.videolan.org/~jb/blacklist/vlc-devices.php");
-
-            try {
-                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
-                nameValuePairs.add(new BasicNameValuePair("model", params[0]));
-                nameValuePairs.add(new BasicNameValuePair("device", params[1]));
-                httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-
-                httpClient.execute(httpPost);
-            } catch (ClientProtocolException e) {
-                e.printStackTrace();
-                return false;
-            } catch (IOException e) {
-                e.printStackTrace();
-                return false;
-            }
-            Log.d(TAG, "Device model sent.");
-            return true;
-        }
-
-        @Override
-        protected void onPostExecute(Boolean result) {
-
-        }
     }
 }
