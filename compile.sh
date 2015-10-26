21284cd5c4fa0a4aa669bfc2f2290022a8415e19
Jean-Baptiste Kempf <jb@videolan.org>
2015-10-26 19:33:22 +0100

    Update HASH to fix SPU bug

--- compile.sh
+++ compile.sh
@@ -131,7 +131,7 @@ fi
 # Fetch VLC source #
 ####################
 
-TESTED_HASH=a43dbcf
+TESTED_HASH=7740747
 if [ ! -d "vlc" ]; then
     echo "VLC source not found, cloning"
     git clone git://git.videolan.org/vlc.git vlc
