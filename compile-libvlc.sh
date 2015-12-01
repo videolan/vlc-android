9b4e5c5b7ced2e0c6be85162f03a2653d6a78936
Thomas Guillem <thomas@gllm.fr>
2015-12-01 17:04:52 +0100

    build: export all symbols in debug (fixes gdb)

--- compile-libvlc.sh
+++ compile-libvlc.sh
@@ -652,6 +652,10 @@ fi
 
 echo "Building NDK"
 
+if [ "$RELEASE" = 1 ]; then
+    EXTRA_LDFLAGS="${EXTRA_LDFLAGS} -Wl,-soname -Wl,libvlc.so.5 -Wl,-version-script -Wl,$SRC_DIR/$VER_FILE"
+fi
+
 $ANDROID_NDK/ndk-build -C libvlc \
     VLC_SRC_DIR="$VLC_SRC_DIR" \
     ANDROID_SYS_HEADERS="$ANDROID_SYS_HEADERS" \
@@ -659,7 +663,7 @@ $ANDROID_NDK/ndk-build -C libvlc \
     VLC_CONTRIB="$VLC_CONTRIB" \
     VLC_MODULES="$VLC_MODULES" \
     TARGET_CFLAGS="$EXTRA_CFLAGS" \
-    EXTRA_LDFLAGS="$EXTRA_LDFLAGS -Wl,-soname -Wl,libvlc.so.5 -Wl,-version-script -Wl,$SRC_DIR/$VER_FILE" \
+    EXTRA_LDFLAGS="$EXTRA_LDFLAGS" \
     LIBVLC_LIBS="$LIBVLC_LIBS" \
     LIBIOMX_LIBS="$LIBIOMX_LIBS" \
     LIBANW_LIBS="$LIBANW_LIBS" \
