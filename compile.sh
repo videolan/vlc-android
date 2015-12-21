dec117423602660db075e41249d531916aa6ab2a
Thomas Guillem <thomas@gllm.fr>
2015-12-21 15:06:01 +0100

    update HASH

--- compile.sh
+++ compile.sh
@@ -131,7 +131,7 @@ fi
 # Fetch VLC source #
 ####################
 
-TESTED_HASH=c4cd717
+TESTED_HASH=4d7d1f0
 if [ ! -d "vlc" ]; then
     echo "VLC source not found, cloning"
     git clone git://git.videolan.org/vlc.git vlc
