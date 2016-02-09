eae964fea0815c4558b8d4aa3a66c41a8bfbb3f5
Geoffrey Métais <geoffrey.metais@gmail.com>
2016-02-09 14:39:49 +0100

    Remove logs

--- vlc-android/src/org/videolan/vlc/gui/tv/DetailsDescriptionPresenter.java
+++ vlc-android/src/org/videolan/vlc/gui/tv/DetailsDescriptionPresenter.java
@@ -21,14 +21,12 @@
 package org.videolan.vlc.gui.tv;
 
 import android.support.v17.leanback.widget.AbstractDetailsDescriptionPresenter;
-import android.util.Log;
 
 public class DetailsDescriptionPresenter extends
         AbstractDetailsDescriptionPresenter {
     public static final String TAG ="DetailsDescriptionPresenter";
 
     protected void onBindDescription(ViewHolder viewHolder, Object itemData) {
-        Log.d(TAG, "itemData "+itemData);
         MediaItemDetails details = (MediaItemDetails) itemData;
         // In a production app, the itemData object contains the information
         // needed to display details for the media item:
