36155f6e5f9a5980589e8dce5e0c2dca12a8bbc7
Geoffrey Métais <geoffrey.metais@gmail.com>
2016-02-12 11:53:27 +0100

    Do not parse subdirectories of servers

--- vlc-android/src/org/videolan/vlc/gui/browser/BaseBrowserFragment.java
+++ vlc-android/src/org/videolan/vlc/gui/browser/BaseBrowserFragment.java
@@ -483,7 +483,7 @@ public abstract class BaseBrowserFragment extends MediaBrowserFragment implement
     }
 
     protected void parseSubDirectories() {
-        if (mCurrentParsedPosition == -1 || mAdapter.isEmpty())
+        if (mRoot || mCurrentParsedPosition == -1 || mAdapter.isEmpty())
             return;
         mFoldersContentLists.clear();
         if (mMediaBrowser == null)
