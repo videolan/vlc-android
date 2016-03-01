329b040b97801044bd9e3bb66d5a0f5464f00ba3
Geoffrey Métais <geoffrey.metais@gmail.com>
2016-03-01 18:05:42 +0100

    Simplify dimStatusBar() algorithm

--- vlc-android/src/org/videolan/vlc/gui/video/VideoPlayerActivity.java
+++ vlc-android/src/org/videolan/vlc/gui/video/VideoPlayerActivity.java
@@ -2548,13 +2548,12 @@ public class VideoPlayerActivity extends AppCompatActivity implements IVLCVout.C
         int visibility = 0;
         int navbar = 0;
 
-        if (!AndroidDevices.hasCombBar() && AndroidUtil.isJellyBeanOrLater()) {
-            visibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+        if (AndroidUtil.isJellyBeanOrLater()) {
+            visibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
             navbar = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
         }
-        if (AndroidUtil.isJellyBeanOrLater())
-            visibility |= View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
         if (dim || mIsLocked) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
             if (AndroidUtil.isICSOrLater())
                 navbar |= View.SYSTEM_UI_FLAG_LOW_PROFILE;
             else
@@ -2566,8 +2565,7 @@ public class VideoPlayerActivity extends AppCompatActivity implements IVLCVout.C
                 if (AndroidUtil.isJellyBeanOrLater())
                     visibility |= View.SYSTEM_UI_FLAG_FULLSCREEN;
             }
-        }
-        if (!dim) {
+        } else {
             getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
             if (AndroidUtil.isICSOrLater())
                 visibility |= View.SYSTEM_UI_FLAG_VISIBLE;
