8f958de60e1c432e7cae7e3af8fca2d41e97314f
Geoffrey Métais <geoffrey.metais@gmail.com>
2016-04-01 17:13:42 +0200

    Hide video group queuing for Android 2.x

--- vlc-android/src/org/videolan/vlc/gui/video/VideoGridFragment.java
+++ vlc-android/src/org/videolan/vlc/gui/video/VideoGridFragment.java
@@ -342,7 +342,10 @@ public class VideoGridFragment extends MediaBrowserFragment implements ISortable
             return;
         MenuInflater inflater = getActivity().getMenuInflater();
         inflater.inflate(media instanceof MediaGroup ? R.menu.video_group_contextual : R.menu.video_list, menu);
-        if (!(media instanceof MediaGroup))
+        if (media instanceof MediaGroup && !AndroidUtil.isHoneycombOrLater()) {
+            menu.findItem(R.id.video_list_append).setVisible(false);
+            menu.findItem(R.id.video_group_play).setVisible(false);
+        } else
             setContextMenuItems(menu, media);
     }
 
