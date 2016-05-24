d5878f7a59abd3d8a6d6a53c344d35682fa8da63
Geoffrey Métais <geoffrey.metais@gmail.com>
2016-05-24 15:08:32 +0200

    Load local subs on playback starting

--- vlc-android/src/org/videolan/vlc/gui/video/VideoPlayerActivity.java
+++ vlc-android/src/org/videolan/vlc/gui/video/VideoPlayerActivity.java
@@ -2958,6 +2958,7 @@ public class VideoPlayerActivity extends AppCompatActivity implements IVLCVout.C
               catch (StreamCorruptedException e) {}
               catch (IOException e) {}
         }
+        prefsList.addAll(MediaDatabase.getInstance().getSubtitles(mUri.getPath()));
         for(String x : prefsList){
             if(!mSubtitleSelectedFiles.contains(x))
                 mSubtitleSelectedFiles.add(x);
