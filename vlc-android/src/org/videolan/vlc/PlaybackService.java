b7e0c77c80a9b76e2be5694cb54baa1cd7aea9e6
Geoffrey Métais <geoffrey.metais@gmail.com>
2016-06-05 13:12:56 +0200

    Restore correct audio volume after notifications
    (cherry picked from commit 1ec3f021368e399768036e2064e177f6fc6df52d)

--- vlc-android/src/org/videolan/vlc/PlaybackService.java
+++ vlc-android/src/org/videolan/vlc/PlaybackService.java
@@ -353,7 +353,7 @@ public class PlaybackService extends Service implements IVLCVout.Callback {
     private OnAudioFocusChangeListener createOnAudioFocusChangeListener() {
         return new OnAudioFocusChangeListener() {
             private boolean mLossTransient = false;
-            private boolean mLossTransientCanDuck = false;
+            private int mLossTransientVolume = -1;
             private boolean wasPlaying = false;
 
             @Override
@@ -380,16 +380,16 @@ public class PlaybackService extends Service implements IVLCVout.Callback {
                         Log.i(TAG, "AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK");
                         // Lower the volume
                         if (mMediaPlayer.isPlaying()) {
+                            mLossTransientVolume = mMediaPlayer.getVolume();
                             mMediaPlayer.setVolume(36);
-                            mLossTransientCanDuck = true;
                         }
                         break;
                     case AudioManager.AUDIOFOCUS_GAIN:
-                        Log.i(TAG, "AUDIOFOCUS_GAIN: " + mLossTransientCanDuck + ", " + mLossTransient);
+                        Log.i(TAG, "AUDIOFOCUS_GAIN: " + mLossTransientVolume + ", " + mLossTransient);
                         // Resume playback
-                        if (mLossTransientCanDuck) {
-                            mMediaPlayer.setVolume(100);
-                            mLossTransientCanDuck = false;
+                        if (mLossTransientVolume != -1) {
+                            mMediaPlayer.setVolume(mLossTransientVolume);
+                            mLossTransientVolume = -1;
                         } else if (mLossTransient) {
                             if (wasPlaying)
                                 mMediaPlayer.play();
