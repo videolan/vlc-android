029158cd4e5bbe3fbdb94061d00466f398089785
Geoffrey Métais <geoffrey.metais@gmail.com>
2016-06-15 12:37:37 +0200

    Reduce scope of size variable

--- vlc-android/src/org/videolan/vlc/PlaybackService.java
+++ vlc-android/src/org/videolan/vlc/PlaybackService.java
@@ -1208,9 +1208,9 @@ public class PlaybackService extends Service implements IVLCVout.Callback {
 
     @MainThread
     public void previous() {
-        int size = mMediaList.size();
         if (hasPrevious() && mCurrentIndex > 0 &&
                 (!mMediaPlayer.isSeekable() || mMediaPlayer.getTime() < 2000l)) {
+            int size = mMediaList.size();
             mCurrentIndex = mPrevIndex;
             if (mPrevious.size() > 0)
                 mPrevious.pop();
