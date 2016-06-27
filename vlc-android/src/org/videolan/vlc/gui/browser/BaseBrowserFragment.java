e053e5db4fbabaa78d558d64116ce6713502a22f
Geoffrey Métais <geoffrey.metais@gmail.com>
2016-06-27 17:16:32 +0200

    Add NoSlavesAutodetect browser flag to subs picker
    (cherry picked from commit 1c893b9455a3188df69372a7740cd4992e440410)

--- vlc-android/src/org/videolan/vlc/gui/browser/BaseBrowserFragment.java
+++ vlc-android/src/org/videolan/vlc/gui/browser/BaseBrowserFragment.java
@@ -353,7 +353,7 @@ public abstract class BaseBrowserFragment extends MediaBrowserFragment implement
         if (mRoot)
             browseRoot();
         else
-            mMediaBrowser.browse(mCurrentMedia != null ? mCurrentMedia.getUri() : Uri.parse(mMrl), MediaBrowser.Flag.Interact);
+            mMediaBrowser.browse(mCurrentMedia != null ? mCurrentMedia.getUri() : Uri.parse(mMrl), getBrowserFlags());
     }
 
     private void deleteMedia(final MediaWrapper mw) {
@@ -372,6 +372,10 @@ public abstract class BaseBrowserFragment extends MediaBrowserFragment implement
             case R.id.fab_add_custom_dir:
                 playAll(null);
         }
+    }
+
+    protected int getBrowserFlags() {
+        return MediaBrowser.Flag.Interact;
     }
 
     protected static class BrowserFragmentHandler extends WeakHandler<BaseBrowserFragment> {
