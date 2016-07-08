4caf03e6f9641737f9489daf85d2ee805acb8174
Jean-Baptiste Kempf <jb@videolan.org>
2016-07-08 11:51:09 +0200

    Update HASH for subtitles color

--- compile.sh
+++ compile.sh
@@ -153,7 +153,7 @@ fi
 # Fetch VLC source #
 ####################
 
-TESTED_HASH=dbc5056
+TESTED_HASH=994f10a
 if [ ! -d "vlc" ]; then
     echo "VLC source not found, cloning"
     git clone git://git.videolan.org/vlc.git vlc
