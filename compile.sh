682259471be257901133d5e326fd8e899681c6d8
Jean-Baptiste Kempf <jb@videolan.org>
2016-07-21 17:30:48 +0200

    Update the hash for libVLC
    This fixes MKV issues and build issues :)

--- compile.sh
+++ compile.sh
@@ -153,7 +153,7 @@ fi
 # Fetch VLC source #
 ####################
 
-TESTED_HASH=b0984a6
+TESTED_HASH=6689dcb
 if [ ! -d "vlc" ]; then
     echo "VLC source not found, cloning"
     git clone git://git.videolan.org/vlc.git vlc
