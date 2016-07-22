128c5716d1e32ae35ddd04b378dc247b38c82eb3
Thomas Guillem <thomas@gllm.fr>
2016-07-22 10:02:45 +0200

    Revert "LibVLC: Add demuxdump2 module"
    Not used for now, and this doesn't allow to get the size of a stream.
    
    This reverts commit 8c3c7a34e59670bc8b7510f73c9f9367b09f026c and partially
    0f135d649f444a1c6e6e30180d36f4e29ed3b9e3

--- libvlc/jni/Android.mk
+++ libvlc/jni/Android.mk
@@ -2,14 +2,6 @@ LOCAL_PATH := $(call my-dir)
 ANDROID_PRIVATE_LIBDIR := $(LOCAL_PATH)/../../android-libs
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := demuxdump2_plugin
-LOCAL_SRC_FILES += modules/demuxdump2.c
-LOCAL_C_INCLUDES := $(VLC_SRC_DIR)/include
-LOCAL_CFLAGS := -std=gnu99 -DMODULE_NAME=demuxdump2 -Wall
-
-include $(BUILD_STATIC_LIBRARY)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := libvlc
 ARCH=$(APP_ABI)
 ifeq ($(ARCH), armeabi)
@@ -49,7 +41,6 @@ LOCAL_LDLIBS := -L$(VLC_CONTRIB)/lib \
 ifeq ($(HAVE_LIBCOMPAT), 1)
 LOCAL_SHARED_LIBRARIES:= libcompat.7
 endif
-LOCAL_STATIC_LIBRARIES:= libdemuxdump2_plugin
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
