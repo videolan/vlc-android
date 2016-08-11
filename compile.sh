e346cd3423bf72020b28874c0905ec21b3c53baf
Jean-Baptiste Kempf <jb@videolan.org>
2016-08-11 12:13:43 +0200

    Revert "Update libVLC hash for FTP"
    This reverts commit da70960c7b05b03432c600755771bbf58f129b45.

--- compile.sh
+++ compile.sh
@@ -153,7 +153,7 @@ fi
 # Fetch VLC source #
 ####################
 
-TESTED_HASH=21b926c
+TESTED_HASH=6689dcb
 if [ ! -d "vlc" ]; then
     echo "VLC source not found, cloning"
     git clone git://git.videolan.org/vlc.git vlc
