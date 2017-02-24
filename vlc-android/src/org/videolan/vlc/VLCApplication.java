a790381d65224eaa5576d4dd4b35f7c43d4b4a7e
Geoffrey Métais <geoffrey.metais@gmail.com>
2017-02-24 17:43:15 +0100

    Set core threadpool size to 2
    We keep 2 threads ready for reuse

--- vlc-android/src/org/videolan/vlc/VLCApplication.java
+++ vlc-android/src/org/videolan/vlc/VLCApplication.java
@@ -67,7 +67,7 @@ public class VLCApplication extends Application {
     private static SimpleArrayMap<String, Object> sDataMap = new SimpleArrayMap<>();
 
     /* Up to 2 threads maximum, inactive threads are killed after 2 seconds */
-    private ThreadPoolExecutor mThreadPool = new ThreadPoolExecutor(0, AndroidUtil.isJellyBeanMR1OrLater() ? Runtime.getRuntime().availableProcessors() : 2, 2, TimeUnit.SECONDS,
+    private ThreadPoolExecutor mThreadPool = new ThreadPoolExecutor(2, AndroidUtil.isJellyBeanMR1OrLater() ? Runtime.getRuntime().availableProcessors() : 2, 2, TimeUnit.SECONDS,
             new LinkedBlockingQueue<Runnable>(), THREAD_FACTORY);
     public static final ThreadFactory THREAD_FACTORY = new ThreadFactory() {
         @Override
@@ -115,7 +115,6 @@ public class VLCApplication extends Application {
             getResources().updateConfiguration(config,
                     getResources().getDisplayMetrics());
         }
-
         instance = this;
 
         runBackground(new Runnable() {
