cd1eb767c6c89548a62adb1a90cb9f95bf47944c
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2017-04-04 16:06:35 +0200

    Bump TESTED_HASH

--- compile.sh
+++ compile.sh
@@ -167,7 +167,7 @@ fi
 # Fetch VLC source #
 ####################
 
-TESTED_HASH=e693c2a
+TESTED_HASH=691be57
 if [ ! -d "vlc" ]; then
     diagnostic "VLC source not found, cloning"
     git clone http://git.videolan.org/git/vlc.git vlc
