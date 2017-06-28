391e5619925e0b09fe58d709afc2ce35b4be4280
Geoffrey Métais <geoffrey.metais@gmail.com>
2017-06-28 10:23:47 +0200

    Fail ML init if context or folders aren't ready

--- medialibrary/src/org/videolan/medialibrary/Medialibrary.java
+++ medialibrary/src/org/videolan/medialibrary/Medialibrary.java
@@ -74,7 +74,13 @@ public class Medialibrary {
     }
 
     public boolean init(Context context) {
-        mIsInitiated = nativeInit(context.getCacheDir()+ VLC_MEDIA_DB_NAME, context.getExternalFilesDir(null).getAbsolutePath()+ THUMBS_FOLDER_NAME);
+        if (context == null)
+            return false;
+        File cacheDir = context.getCacheDir();
+        File extFilesDir = context.getExternalFilesDir(null);
+        if (extFilesDir == null || !cacheDir.exists() || !extFilesDir.exists())
+            return false;
+        mIsInitiated = nativeInit(cacheDir+ VLC_MEDIA_DB_NAME, extFilesDir.getAbsolutePath()+ THUMBS_FOLDER_NAME);
         return mIsInitiated;
     }
 
