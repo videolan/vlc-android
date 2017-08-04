056f815bdd41a854e8234400ad67f2c2574ffaec
Geoffrey Métais <geoffrey.metais@gmail.com>
2017-08-04 10:56:30 +0200

    Revert "Activate fast-seek"
    This reverts commit 973a690a1178019c58f2fe2a6c21fde3f437646a.
    
    Close #333

--- vlc-android/src/org/videolan/vlc/util/VLCOptions.java
+++ vlc-android/src/org/videolan/vlc/util/VLCOptions.java
@@ -122,6 +122,7 @@ public class VLCOptions {
         options.add(new File(context.getDir("keystore", Context.MODE_PRIVATE), "file").getAbsolutePath());
 
         options.add(verboseMode ? "-vv" : "-v");
+
         return options;
     }
 
@@ -196,7 +197,6 @@ public class VLCOptions {
             media.addOption(":no-video");
         if (paused)
             media.addOption(":start-paused");
-        media.addOption(":input-fast-seek");
     }
 
     @MainThread
