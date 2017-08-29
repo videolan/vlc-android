37afe2ce1027bd491dd120c0b7ac8fe0cfb79236
Geoffrey Métais <geoffrey.metais@gmail.com>
2017-08-29 13:22:33 +0200

    Update user visible hint on fragment show/hide

--- vlc-android/src/org/videolan/vlc/gui/browser/MediaBrowserFragment.java
+++ vlc-android/src/org/videolan/vlc/gui/browser/MediaBrowserFragment.java
@@ -102,6 +102,7 @@ public abstract class MediaBrowserFragment extends PlaybackServiceFragment imple
                 });
             }
         }
+        setUserVisibleHint(!hidden);
     }
 
     @Override
