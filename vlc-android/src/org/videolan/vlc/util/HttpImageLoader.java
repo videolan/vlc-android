e26f23fd9ff64a925a08c902803f8435180660a1
Geoffrey Métais <geoffrey.metais@gmail.com>
2017-09-01 17:37:25 +0200

    Http DL: Use default port if none is set

--- vlc-android/src/org/videolan/vlc/util/HttpImageLoader.java
+++ vlc-android/src/org/videolan/vlc/util/HttpImageLoader.java
@@ -113,8 +113,6 @@ public class HttpImageLoader implements Callbacks {
             return icon;
         try {
             URL url = new URL(imageUrl);
-            if (url.getPort() <= 0)
-                return null;
             urlConnection = (HttpURLConnection) url.openConnection();
             InputStream in = new BufferedInputStream(urlConnection.getInputStream());
             icon = BitmapFactory.decodeStream(in);
