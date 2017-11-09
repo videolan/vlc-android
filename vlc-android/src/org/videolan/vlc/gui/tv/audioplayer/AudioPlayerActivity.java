d1e57b87bf6fbdd8951588d2d8085ed30a955d81
Geoffrey Métais <geoffrey.metais@gmail.com>
2017-11-09 11:35:33 +0100

    Remove ic_tv_icon_big pngs

--- vlc-android/src/org/videolan/vlc/gui/tv/audioplayer/AudioPlayerActivity.java
+++ vlc-android/src/org/videolan/vlc/gui/tv/audioplayer/AudioPlayerActivity.java
@@ -165,7 +165,7 @@ public class AudioPlayerActivity extends BaseTvActivity implements PlaybackServi
                     @Override
                     public void run() {
                         if (cover == null) {
-                            mBinding.albumCover.setImageResource(R.drawable.ic_tv_icon_big);
+                            mBinding.albumCover.setImageResource(R.drawable.ic_song_big);
                             mBinding.background.clearColorFilter();
                             mBinding.background.setImageResource(0);
                         } else {
