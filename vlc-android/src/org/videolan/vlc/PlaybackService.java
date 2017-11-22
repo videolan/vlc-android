dcd7828e81e45ff399495a33ae6f098f4b57880b
Geoffrey Métais <geoffrey.metais@gmail.com>
2017-11-22 16:04:49 +0100

    Do not end media session when service is killed

--- vlc-android/src/org/videolan/vlc/PlaybackService.java
+++ vlc-android/src/org/videolan/vlc/PlaybackService.java
@@ -357,7 +357,6 @@ public class PlaybackService extends MediaBrowserServiceCompat implements IVLCVo
         super.onDestroy();
         stop(true);
         if (mMediaSession != null) {
-            mMediaSession.setActive(false);
             mMediaSession.release();
             mMediaSession = null;
         }
