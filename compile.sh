8d16064973b8b16d759c2a6f72c11f01a716de86
Geoffrey Métais <geoffrey.metais@gmail.com>
2017-12-12 17:04:15 +0100

    Add additionnal license for Android SDK

--- compile.sh
+++ compile.sh
@@ -219,6 +219,7 @@ init_local_props local.properties || { echo "Error initializing local.properties
 if [ ! -d "$ANDROID_SDK/licenses" ]; then
     mkdir "$ANDROID_SDK/licenses"
     echo "8933bad161af4178b1185d1a37fbf41ea5269c55" > "$ANDROID_SDK/licenses/android-sdk-license"
+    echo "d56f5187479451eabf01fb78af6dfcb131a6481e" >> "$ANDROID_SDK/licenses/android-sdk-license"
     echo "84831b9409646a918e30573bab4c9c91346d8abd" > "$ANDROID_SDK/licenses/android-sdk-preview-license"
 fi
 
