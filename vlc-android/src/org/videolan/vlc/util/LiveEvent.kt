f19c89eccf014854062824352f32aba6f8e8f972
Geoffrey Métais <geoffrey.metais@gmail.com>
2018-04-04 14:16:48 +0200

    LiveEvent class, for single events with LiveData

---
+++ vlc-android/src/org/videolan/vlc/util/LiveEvent.kt
@@ -0,0 +1,32 @@
+package org.videolan.vlc.util
+
+import android.arch.lifecycle.LifecycleOwner
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Observer
+import android.support.annotation.MainThread
+import android.support.annotation.Nullable
+import android.util.Log
+import java.util.concurrent.atomic.AtomicBoolean
+
+private const val TAG = "VLC/LiveEvent"
+
+class LiveEvent<T> : MutableLiveData<T>() {
+    private val pending = AtomicBoolean(false)
+    @MainThread
+    override fun observe(owner: LifecycleOwner, observer: Observer<T>) {
+        if (hasActiveObservers()) Log.w(TAG, "Multiple observers registered but only one will be notified of changes.")
+        // Observe the internal MutableLiveData
+        super.observe(owner, Observer<T> { t -> if (pending.compareAndSet(true, false)) observer.onChanged(t) })
+    }
+
+    @MainThread
+    override fun setValue(@Nullable t: T?) {
+        pending.set(true)
+        super.setValue(t)
+    }
+
+    @MainThread
+    fun call() {
+        value = null
+    }
+}
