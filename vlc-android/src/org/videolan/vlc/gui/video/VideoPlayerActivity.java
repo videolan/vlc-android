04533842007307db7a74ebe33b34ba3d9d76fe8e
Hamza Parnica <hparnica@gmail.com>
2018-04-13 18:23:12 +0200

    Filter accidental touch events in video player

--- vlc-android/src/org/videolan/vlc/gui/video/VideoPlayerActivity.java
+++ vlc-android/src/org/videolan/vlc/gui/video/VideoPlayerActivity.java
@@ -298,6 +298,7 @@ public class VideoPlayerActivity extends AppCompatActivity implements IVLCVout.C
     private static final int TOUCH_BRIGHTNESS = 2;
     private static final int TOUCH_MOVE = 3;
     private static final int TOUCH_SEEK = 4;
+    private static final int TOUCH_IGNORE = 5;
     private int mTouchAction = TOUCH_NONE;
     private int mSurfaceYDisplayRange, mSurfaceXDisplayRange;
     private float mFov;
@@ -2044,6 +2045,7 @@ public class VideoPlayerActivity extends AppCompatActivity implements IVLCVout.C
                 endPlaybackSetting();
             return true;
         } else if (mPlaylist.getVisibility() == View.VISIBLE) {
+            mTouchAction = TOUCH_IGNORE;
             togglePlaylist();
             return true;
         }
@@ -2093,6 +2095,8 @@ public class VideoPlayerActivity extends AppCompatActivity implements IVLCVout.C
                 sendMouseEvent(MotionEvent.ACTION_DOWN, xTouch, yTouch);
                 break;
             case MotionEvent.ACTION_MOVE:
+                if (mTouchAction == TOUCH_IGNORE) break;
+
                 // Mouse events for the core
                 sendMouseEvent(MotionEvent.ACTION_MOVE, xTouch, yTouch);
 
@@ -2119,6 +2123,7 @@ public class VideoPlayerActivity extends AppCompatActivity implements IVLCVout.C
                 }
                 break;
             case MotionEvent.ACTION_UP:
+                if (mTouchAction == TOUCH_IGNORE) mTouchAction = TOUCH_NONE;
                 // Mouse events for the core
                 sendMouseEvent(MotionEvent.ACTION_UP, xTouch, yTouch);
                 // Seek
