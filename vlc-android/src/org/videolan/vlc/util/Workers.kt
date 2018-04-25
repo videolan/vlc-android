6b314daef3fe93fea1ec0c85973836326b408304
Geoffrey Métais <geoffrey.metais@gmail.com>
2018-04-25 13:17:25 +0200

    Create a VLCIO coroutines context
    As IO operations are blocking, create a dedicated coroutines dispatcher
    to not block the CommonPool

--- vlc-android/src/org/videolan/vlc/util/Workers.kt
+++ vlc-android/src/org/videolan/vlc/util/Workers.kt
@@ -3,15 +3,16 @@ package org.videolan.vlc.util
 import android.os.Looper
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.newSingleThreadContext
+
+val VLCIO = newSingleThreadContext("vlc-io")
 
 fun runBackground(runnable: Runnable) {
-    if (Looper.myLooper() != Looper.getMainLooper())
-        runnable.run()
+    if (Looper.myLooper() != Looper.getMainLooper()) runnable.run()
     else launch { runnable.run() }
 }
 
 fun runOnMainThread(runnable: Runnable) {
-    if (Looper.myLooper() == Looper.getMainLooper())
-        runnable.run()
+    if (Looper.myLooper() == Looper.getMainLooper()) runnable.run()
     else launch(UI) { runnable.run() }
 }
