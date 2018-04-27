6428e8ea2fe0fb7858b225dc90810ba78bb68b15
Geoffrey Métais <geoffrey.metais@gmail.com>
2018-04-27 11:17:24 +0200

    Catch SecurityException in FileUtils.getUri()

--- vlc-android/src/org/videolan/vlc/util/FileUtils.java
+++ vlc-android/src/org/videolan/vlc/util/FileUtils.java
@@ -120,11 +120,10 @@ public class FileUtils {
             final int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
             cursor.moveToFirst();
             return cursor.getString(column_index);
-        } catch (IllegalArgumentException e) {
+        } catch (IllegalArgumentException|SecurityException e) {
                 return "";
         } finally {
-            if (cursor != null && !cursor.isClosed())
-                cursor.close();
+            if (cursor != null && !cursor.isClosed()) cursor.close();
         }
     }
 
