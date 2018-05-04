34c45f98fe1786106d38e154e90c3f38035c2dfb
Geoffrey Métais <geoffrey.metais@gmail.com>
2018-05-04 16:14:25 +0200

    Use view context to get preferences

--- vlc-android/src/org/videolan/vlc/gui/video/VideoGridFragment.java
+++ vlc-android/src/org/videolan/vlc/gui/video/VideoGridFragment.java
@@ -538,7 +538,7 @@ public class VideoGridFragment extends SortableFragment<VideoListAdapter> implem
             ((MainActivity)activity).showSecondaryFragment(SecondaryActivity.VIDEO_GROUP_LIST, title);
         } else {
             media.removeFlags(MediaWrapper.MEDIA_FORCE_AUDIO);
-            final SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(VLCApplication.getAppContext());
+            final SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(v.getContext());
             if (settings.getBoolean("force_play_all", false)) {
                 final List<MediaWrapper> playList = new ArrayList<>();
                 MediaUtils.openList(activity, playList, mAdapter.getListWithPosition(playList, position));
