794796942d92be953815bc002c671597abd6fedf
Geoffrey Métais <geoffrey.metais@gmail.com>
2018-05-22 16:08:17 +0200

    Update VLC HASH

--- compile.sh
+++ compile.sh
@@ -233,7 +233,7 @@ fi
 # Fetch VLC source #
 ####################
 
-TESTED_HASH=fb3a5ca
+TESTED_HASH=ec50971
 if [ ! -d "vlc" ]; then
     diagnostic "VLC source not found, cloning"
     git clone https://git.videolan.org/git/vlc/vlc-3.0.git vlc
