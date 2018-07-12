da4e3330d5981687c1f2a21eaa5397c06eba646e
Geoffrey Métais <geoffrey.metais@gmail.com>
2018-07-12 15:31:58 +0200

    Use a threadpool for I/O operations

--- vlc-android/src/org/videolan/vlc/util/Workers.kt
+++ vlc-android/src/org/videolan/vlc/util/Workers.kt
@@ -1,10 +1,21 @@
 package org.videolan.vlc.util
 
 import android.os.Looper
+import android.os.Process
 import kotlinx.coroutines.experimental.*
 import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.timeunit.TimeUnit
+import java.util.concurrent.SynchronousQueue
+import java.util.concurrent.ThreadFactory
+import java.util.concurrent.ThreadPoolExecutor
 
-val VLCIO = newSingleThreadContext("vlc-io")
+
+//val VLCIO = newSingleThreadContext("vlc-io")
+private val THREAD_FACTORY: ThreadFactory = ThreadFactory { runnable ->
+    Thread(runnable).apply { priority = Thread.NORM_PRIORITY + Process.THREAD_PRIORITY_LESS_FAVORABLE }
+}
+
+val VLCIO = ThreadPoolExecutor(1, Integer.MAX_VALUE, 60L, TimeUnit.SECONDS, SynchronousQueue<Runnable>(), THREAD_FACTORY).asCoroutineDispatcher()
 private val mainThread = Looper.getMainLooper().thread!!
 private val uiDispatch inline get() = mainThread != Thread.currentThread()
 
