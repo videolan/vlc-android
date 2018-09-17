fe9a579208a0c21bcc535f9cab3bc950acb0ed07
Geoffrey Métais <geoffrey.metais@gmail.com>
2018-09-17 09:27:43 +0200

    Parent coroutine job in ViewModels

--- vlc-android/src/org/videolan/vlc/viewmodels/ScopedModel.kt
+++ vlc-android/src/org/videolan/vlc/viewmodels/ScopedModel.kt
@@ -3,9 +3,16 @@ package org.videolan.vlc.viewmodels
 import android.arch.lifecycle.ViewModel
 import kotlinx.coroutines.experimental.CoroutineScope
 import kotlinx.coroutines.experimental.Dispatchers
+import kotlinx.coroutines.experimental.Job
 import kotlinx.coroutines.experimental.android.Main
 
 
 open class ScopedModel : ViewModel(), CoroutineScope {
-    override val coroutineContext = Dispatchers.Main.immediate
+    protected val job = Job()
+    override val coroutineContext = Dispatchers.Main.immediate+job
+
+    override fun onCleared() {
+        super.onCleared()
+        job.cancel()
+    }
 }
