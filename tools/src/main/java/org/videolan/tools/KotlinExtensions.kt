773b5a174e76148dcc389e395bb11b821a56d8be
Geoffrey Métais <geoffrey.metais@gmail.com>
2018-09-17 11:15:52 +0200

    CoroutineScope helper for LifecycleOwner classes

---
+++ tools/src/main/java/org/videolan/tools/KotlinExtensions.kt
@@ -0,0 +1,29 @@
+package org.videolan.tools
+
+import android.arch.lifecycle.GenericLifecycleObserver
+import android.arch.lifecycle.Lifecycle
+import android.arch.lifecycle.LifecycleOwner
+import kotlinx.coroutines.experimental.CoroutineScope
+import kotlinx.coroutines.experimental.Dispatchers
+import kotlinx.coroutines.experimental.Job
+import kotlinx.coroutines.experimental.android.Main
+
+fun LifecycleOwner.createJob(cancelEvent: Lifecycle.Event = Lifecycle.Event.ON_STOP): Job = Job().also { job ->
+    lifecycle.addObserver(object : GenericLifecycleObserver {
+        override fun onStateChanged(source: LifecycleOwner?, event: Lifecycle.Event) {
+            if (event == cancelEvent) {
+                lifecycle.removeObserver(this)
+                job.cancel()
+            }
+        }
+    })
+}
+private val lifecycleCoroutineScopes = mutableMapOf<Lifecycle, CoroutineScope>()
+
+val LifecycleOwner.coroutineScope: CoroutineScope
+    get() = lifecycleCoroutineScopes[lifecycle] ?: createJob().let {
+        val newScope = CoroutineScope(it + Dispatchers.Main.immediate)
+        lifecycleCoroutineScopes[lifecycle] = newScope
+        it.invokeOnCompletion { _ -> lifecycleCoroutineScopes -= lifecycle }
+        newScope
+    }
