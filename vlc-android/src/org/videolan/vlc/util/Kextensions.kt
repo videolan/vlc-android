02cb8513e9dc433de9f7abcb07b7126fb99dc8b6
Geoffrey Métais <geoffrey.metais@gmail.com>
2018-10-12 11:55:24 +0200

    Check if ML is started instead of initialized

--- vlc-android/src/org/videolan/vlc/util/Kextensions.kt
+++ vlc-android/src/org/videolan/vlc/util/Kextensions.kt
@@ -8,7 +8,6 @@ import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentActivity
 import android.support.v7.preference.PreferenceManager
 import kotlinx.coroutines.experimental.Dispatchers
-import kotlinx.coroutines.experimental.IO
 import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.withContext
 import org.videolan.libvlc.Media
@@ -66,7 +65,7 @@ fun Long.random() = (Random().nextFloat() * this).toLong()
 
 suspend inline fun <reified T> Context.getFromMl(crossinline block: Medialibrary.() -> T) = withContext(Dispatchers.IO) {
     val ml = Medialibrary.getInstance()
-    if (ml.isInitiated) block.invoke(ml)
+    if (ml.isStarted) block.invoke(ml)
     else suspendCoroutine { continuation ->
         ml.addOnMedialibraryReadyListener(object : Medialibrary.OnMedialibraryReadyListener {
             override fun onMedialibraryReady() {
