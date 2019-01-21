e13cf23e286c28fc76c1f4eaa6b6f8b98239a12b
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2019-01-21 17:17:31 +0100

    compile.sh: Update license hash

--- compile.sh
+++ compile.sh
@@ -230,7 +230,7 @@ init_local_props local.properties || { echo "Error initializing local.properties
 
 if [ ! -d "$ANDROID_SDK/licenses" ]; then
     mkdir "$ANDROID_SDK/licenses"
-    echo "8933bad161af4178b1185d1a37fbf41ea5269c55" > "$ANDROID_SDK/licenses/android-sdk-license"
+    echo "24333f8a63b6825ea9c5514f83c2829b004d1fee" > "$ANDROID_SDK/licenses/android-sdk-license"
     echo "d56f5187479451eabf01fb78af6dfcb131a6481e" >> "$ANDROID_SDK/licenses/android-sdk-license"
 fi
 
