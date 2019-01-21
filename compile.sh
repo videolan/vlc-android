c536e3bc5d5d392d12055494dec81acc583e3d10
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2019-01-21 17:16:46 +0100

    compile.sh: Remove unrequired license

--- compile.sh
+++ compile.sh
@@ -232,7 +232,6 @@ if [ ! -d "$ANDROID_SDK/licenses" ]; then
     mkdir "$ANDROID_SDK/licenses"
     echo "8933bad161af4178b1185d1a37fbf41ea5269c55" > "$ANDROID_SDK/licenses/android-sdk-license"
     echo "d56f5187479451eabf01fb78af6dfcb131a6481e" >> "$ANDROID_SDK/licenses/android-sdk-license"
-    echo "84831b9409646a918e30573bab4c9c91346d8abd" > "$ANDROID_SDK/licenses/android-sdk-preview-license"
 fi
 
 if [ "$GRADLE_SETUP" = 1 ]; then
