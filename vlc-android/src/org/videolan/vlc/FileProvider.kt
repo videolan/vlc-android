86051dd9753a126e454726d9141566d4b1999262
Geoffrey Métais <geoffrey.metais@gmail.com>
2019-02-05 15:00:36 +0100

    FileProvider: check canonical path for access
    Thank Shivasurya S <s.shivasurya@gmail.com> for this security report.

--- vlc-android/src/org/videolan/vlc/FileProvider.kt
+++ vlc-android/src/org/videolan/vlc/FileProvider.kt
@@ -27,8 +27,8 @@ class FileProvider : ContentProvider() {
     override fun getType(uri: Uri) = "image/${uri.path?.substringAfterLast('.')}"
 
     override fun openFile(uri: Uri, mode: String?): ParcelFileDescriptor {
-        if (uri.path.startsWith("/data")) throw SecurityException("Illegal access")
         val file = File(uri.path)
+        if (file.canonicalPath.startsWith("/data")) throw SecurityException("Illegal access")
         if (file.exists()) {
             return ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY)
         }
