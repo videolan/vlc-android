68b13aa8c832bddd8b7eeece14838555ff1d8d67
Geoffrey Métais <geoffrey.metais@gmail.com>
2019-02-08 10:32:16 +0100

    Remove the close icon from playback notif

--- vlc-android/src/org/videolan/vlc/gui/helpers/NotificationHelper.java
+++ vlc-android/src/org/videolan/vlc/gui/helpers/NotificationHelper.java
@@ -85,8 +85,6 @@ public class NotificationHelper {
                 R.drawable.ic_widget_next_w, ctx.getString(R.string.next),
                 MediaButtonReceiver.buildMediaButtonPendingIntent(ctx,
                         PlaybackStateCompat.ACTION_SKIP_TO_NEXT)));
-        builder.addAction(new NotificationCompat.Action(
-                R.drawable.ic_widget_close_w, ctx.getString(R.string.stop), piStop));
 
         if (AndroidDevices.showMediaStyle) {
             builder.setStyle(new androidx.media.app.NotificationCompat.MediaStyle()
