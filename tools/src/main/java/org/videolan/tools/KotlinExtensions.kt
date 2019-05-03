851aa7f30fbf02a624585d981ff738b5adf6f5f6
Geoffrey Métais <geoffrey.metais@gmail.com>
2019-05-03 13:56:44 +0200

    Lifecycle: Replace deprecated code

--- tools/src/main/java/org/videolan/tools/KotlinExtensions.kt
+++ tools/src/main/java/org/videolan/tools/KotlinExtensions.kt
@@ -1,20 +1,20 @@
 package org.videolan.tools
 
-import androidx.lifecycle.GenericLifecycleObserver
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.OnLifecycleEvent
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.Job
 
 fun LifecycleOwner.createJob(cancelEvent: Lifecycle.Event = Lifecycle.Event.ON_DESTROY): Job = Job().also { job ->
-    lifecycle.addObserver(object : GenericLifecycleObserver {
-        override fun onStateChanged(source: LifecycleOwner?, event: Lifecycle.Event) {
-            if (event == cancelEvent) {
-                lifecycle.removeObserver(this)
-                job.cancel()
-            }
+    lifecycle.addObserver(object : LifecycleObserver {
+        @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+        fun clear() {
+            lifecycle.removeObserver(this)
+            job.cancel()
         }
     })
 }
