fb90d0e1c42c35369a3859d1e867e964699be4f5
Thomas Guillem <thomas@gllm.fr>
2019-05-20 11:35:44 +0200

    libvlc: make VLC patches not mandatory
    Make compilation work even if we don't apply recording patches.
    
    Signed-off-by: Romain Vimont <rom1v@videolabs.io>

--- libvlc/jni/libvlcjni-mediaplayer.c
+++ libvlc/jni/libvlcjni-mediaplayer.c
@@ -47,7 +47,9 @@ static const libvlc_event_type_t mp_events[] = {
     libvlc_MediaPlayerSeekableChanged,
     libvlc_MediaPlayerPausableChanged,
     libvlc_MediaPlayerLengthChanged,
+#ifdef LIBVLC_MEDIA_PLAYER_HAS_RECORDING
     libvlc_MediaPlayerRecordChanged,
+#endif
     -1,
 };
 
@@ -110,10 +112,12 @@ MediaPlayer_event_cb(vlcjni_object *p_obj, const libvlc_event_t *p_ev,
         case libvlc_MediaPlayerLengthChanged:
             p_java_event->arg1 = p_ev->u.media_player_length_changed.new_length;
             break;
+#ifdef LIBVLC_MEDIA_PLAYER_HAS_RECORDING
         case libvlc_MediaPlayerRecordChanged:
             p_java_event->arg1 = p_ev->u.media_player_record_changed.recording;
             p_java_event->argc1 = p_ev->u.media_player_record_changed.file_path;
             break;
+#endif
     }
     p_java_event->type = p_ev->type;
     return true;
