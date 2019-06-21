d9858b115e5ce6a337681d0cbcbb189d56db2150
Geoffrey Métais <geoffrey.metais@gmail.com>
2019-06-21 15:09:50 +0200

    MediaUtils: Fix potential NPE

--- vlc-android/src/org/videolan/vlc/media/MediaUtils.kt
+++ vlc-android/src/org/videolan/vlc/media/MediaUtils.kt
@@ -299,6 +299,8 @@ object MediaUtils : CoroutineScope {
         data
     } catch (e: IllegalArgumentException) {
         data
+    } catch (e: NullPointerException) {
+        data
     }
 
     private fun getMediaString(ctx: Context?, id: Int): String {
