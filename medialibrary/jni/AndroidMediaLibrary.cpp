cfde19e1e5c38b3c2a9742b265a06bd5af92ebd5
Nicolas Pomepuy <nicolas.pomepuy@gmail.com>
2019-08-29 14:13:08 +0200

    Revert "Revert onHistoryChanged callback implementation"
    This reverts commit 82d7d7d6
    
    (cherry picked from commit fe0872aecdabe278ca64f3dde625dd493d486d70)

--- medialibrary/jni/AndroidMediaLibrary.cpp
+++ medialibrary/jni/AndroidMediaLibrary.cpp
@@ -890,6 +890,9 @@ void AndroidMediaLibrary::onParsingStatsUpdated( uint32_t percent)
     }
 }
 
+void AndroidMediaLibrary::onHistoryChanged( medialibrary::HistoryType )
+{
+}
 
 void AndroidMediaLibrary::onBackgroundTasksIdleChanged( bool isIdle )
 {
