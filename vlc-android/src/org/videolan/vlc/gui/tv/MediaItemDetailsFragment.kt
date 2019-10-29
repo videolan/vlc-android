530e398ca5a7492ce10f80e839e03396cd38610f
Geoffrey Métais <geoffrey.metais@gmail.com>
2019-10-29 14:31:38 +0100

    TV: Refactor media details view

--- vlc-android/src/org/videolan/vlc/gui/tv/MediaItemDetailsFragment.kt
+++ vlc-android/src/org/videolan/vlc/gui/tv/MediaItemDetailsFragment.kt
@@ -29,11 +29,16 @@ import android.os.Parcelable
 import android.text.TextUtils
 import android.widget.Toast
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.activityViewModels
 import androidx.leanback.app.BackgroundManager
 import androidx.leanback.app.DetailsSupportFragment
 import androidx.leanback.widget.*
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.ObsoleteCoroutinesApi
+import kotlinx.coroutines.withContext
 import org.videolan.libvlc.util.AndroidUtil
 import org.videolan.medialibrary.MLServiceLocator
 import org.videolan.medialibrary.interfaces.media.AbstractMediaWrapper
@@ -45,8 +50,16 @@ import org.videolan.vlc.media.MediaUtils
 import org.videolan.vlc.repository.BrowserFavRepository
 import org.videolan.vlc.util.ACTION_REMOTE_STOP
 import org.videolan.vlc.util.FileUtils
-import org.videolan.vlc.util.runIO
-import org.videolan.vlc.util.runOnMainThread
+
+private const val TAG = "MediaItemDetailsFragment"
+private const val ID_PLAY = 1
+private const val ID_LISTEN = 2
+private const val ID_FAVORITE_ADD = 3
+private const val ID_FAVORITE_DELETE = 4
+private const val ID_BROWSE = 5
+private const val ID_DL_SUBS = 6
+private const val ID_PLAY_FROM_START = 7
+private const val ID_PLAYLIST = 8
 
 @ExperimentalCoroutinesApi
 @ObsoleteCoroutinesApi
@@ -55,17 +68,25 @@ class MediaItemDetailsFragment : DetailsSupportFragment() {
 
     private lateinit var backgroundManager: BackgroundManager
     private lateinit var rowsAdapter: ArrayObjectAdapter
-    private lateinit var mediaItemDetails: MediaItemDetails
-    private lateinit var mediaWrapper: AbstractMediaWrapper
     private lateinit var browserFavRepository: BrowserFavRepository
-    private var mediaStarted: Boolean = false
+    private val viewModel : MediaItemDetailsModel by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         backgroundManager = BackgroundManager.getInstance(requireActivity())
         backgroundManager.isAutoReleaseOnStop = false
         browserFavRepository = BrowserFavRepository.getInstance(requireContext())
-        mediaStarted = false
+        viewModel.mediaStarted = false
+
+        val extras = requireActivity().intent.extras ?: savedInstanceState ?: return
+        viewModel.mediaItemDetails = extras.getParcelable("item") as MediaItemDetails
+        val hasMedia = extras.containsKey("media")
+        val media = (extras.getParcelable<Parcelable>("media")
+                ?: MLServiceLocator.getAbstractMediaWrapper(AndroidUtil.LocationToUri(viewModel.mediaItemDetails.location))) as AbstractMediaWrapper
+
+        viewModel.media = media
+        if (!hasMedia) viewModel.media.setDisplayTitle(viewModel.mediaItemDetails.title)
+        title = viewModel.media.title
         buildDetails()
     }
 
@@ -77,29 +98,25 @@ class MediaItemDetailsFragment : DetailsSupportFragment() {
     override fun onPause() {
         backgroundManager.release()
         super.onPause()
-        if (mediaStarted) {
-            val ctx = context
-            ctx?.sendBroadcast(Intent(ACTION_REMOTE_STOP))
+        if (viewModel.mediaStarted) {
+            context?.sendBroadcast(Intent(ACTION_REMOTE_STOP))
         }
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putParcelable("item", viewModel.mediaItemDetails)
+        outState.putParcelable("media", viewModel.media)
+        super.onSaveInstanceState(outState)
+    }
+
     private fun buildDetails() {
-        val extras = requireActivity().intent.extras ?: return
-        mediaItemDetails = extras.getParcelable("item") as MediaItemDetails
-        val hasMedia = extras.containsKey("media")
         val selector = ClassPresenterSelector()
-        val media = (if (hasMedia) extras.getParcelable<Parcelable>("media") else MLServiceLocator.getAbstractMediaWrapper(AndroidUtil.LocationToUri(mediaItemDetails.location))) as AbstractMediaWrapper
-        if (!hasMedia) {
-            media.setDisplayTitle(mediaItemDetails.title)
-        }
-        mediaWrapper = media
-        title = media.title
 
         // Attach your media item details presenter to the row presenter:
         val rowPresenter = FullWidthDetailsOverviewRowPresenter(DetailsDescriptionPresenter())
 
         val activity = requireActivity()
-        val detailsOverview = DetailsOverviewRow(mediaItemDetails)
+        val detailsOverview = DetailsOverviewRow(viewModel.mediaItemDetails)
         val actionAdd = Action(ID_FAVORITE_ADD.toLong(), getString(R.string.favorites_add))
         val actionDelete = Action(ID_FAVORITE_DELETE.toLong(), getString(R.string.favorites_remove))
 
@@ -107,43 +124,41 @@ class MediaItemDetailsFragment : DetailsSupportFragment() {
         rowPresenter.onActionClickedListener = OnActionClickedListener { action ->
             when (action.id.toInt()) {
                 ID_LISTEN -> {
-                    MediaUtils.openMedia(activity, mediaWrapper)
-                    mediaStarted = true
+                    MediaUtils.openMedia(activity, viewModel.media)
+                    viewModel.mediaStarted = true
                 }
                 ID_PLAY -> {
-                    mediaStarted = false
-                    TvUtil.playMedia(activity, media)
+                    viewModel.mediaStarted = false
+                    TvUtil.playMedia(activity, viewModel.media)
                     activity.finish()
                 }
-                ID_PLAYLIST -> {
-                    UiTools.addToPlaylist(requireActivity(), arrayListOf(mediaWrapper))
-                }
+                ID_PLAYLIST -> UiTools.addToPlaylist(activity, arrayListOf(viewModel.media))
                 ID_FAVORITE_ADD -> {
-                    val uri = Uri.parse(mediaItemDetails.location)
+                    val uri = Uri.parse(viewModel.mediaItemDetails.location)
                     val local = "file" == uri.scheme
                     if (local)
-                        browserFavRepository.addLocalFavItem(uri, mediaItemDetails.title
-                                ?: "", mediaItemDetails.artworkUrl)
+                        browserFavRepository.addLocalFavItem(uri, viewModel.mediaItemDetails.title
+                                ?: "", viewModel.mediaItemDetails.artworkUrl)
                     else
-                        browserFavRepository.addNetworkFavItem(uri, mediaItemDetails.title
-                                ?: "", mediaItemDetails.artworkUrl)
+                        browserFavRepository.addNetworkFavItem(uri, viewModel.mediaItemDetails.title
+                                ?: "", viewModel.mediaItemDetails.artworkUrl)
                     detailsOverview.removeAction(actionAdd)
                     detailsOverview.addAction(actionDelete)
                     rowsAdapter.notifyArrayItemRangeChanged(0, rowsAdapter.size())
                     Toast.makeText(activity, R.string.favorite_added, Toast.LENGTH_SHORT).show()
                 }
                 ID_FAVORITE_DELETE -> {
-                    runIO(Runnable { browserFavRepository.deleteBrowserFav(Uri.parse(mediaItemDetails.location)) })
+                    browserFavRepository.deleteBrowserFav(Uri.parse(viewModel.mediaItemDetails.location))
                     detailsOverview.removeAction(actionDelete)
                     detailsOverview.addAction(actionAdd)
                     rowsAdapter.notifyArrayItemRangeChanged(0, rowsAdapter.size())
                     Toast.makeText(activity, R.string.favorite_removed, Toast.LENGTH_SHORT).show()
                 }
-                ID_BROWSE -> TvUtil.openMedia(activity, media, null)
-                ID_DL_SUBS -> MediaUtils.getSubs(requireActivity(), media)
+                ID_BROWSE -> TvUtil.openMedia(activity, viewModel.media, null)
+                ID_DL_SUBS -> MediaUtils.getSubs(requireActivity(), viewModel.media)
                 ID_PLAY_FROM_START -> {
-                    mediaStarted = false
-                    VideoPlayerActivity.start(requireActivity(), media.uri, true)
+                    viewModel.mediaStarted = false
+                    VideoPlayerActivity.start(requireActivity(), viewModel.media.uri, true)
                     activity.finish()
                 }
             }
@@ -152,68 +167,57 @@ class MediaItemDetailsFragment : DetailsSupportFragment() {
         selector.addClassPresenter(ListRow::class.java,
                 ListRowPresenter())
         rowsAdapter = ArrayObjectAdapter(selector)
-        runIO(Runnable {
-            val cover = if (media.type == AbstractMediaWrapper.TYPE_AUDIO || media.type == AbstractMediaWrapper.TYPE_VIDEO)
-                AudioUtil.readCoverBitmap(mediaItemDetails.artworkUrl, 512)
-            else
-                null
-            val blurred = if (cover != null) UiTools.blurBitmap(cover) else null
-            val browserFavExists = browserFavRepository.browserFavExists(Uri.parse(mediaItemDetails.location))
-            val isDir = media.type == AbstractMediaWrapper.TYPE_DIR
-            val canSave = isDir && FileUtils.canSave(media)
-            runOnMainThread(Runnable {
-                if (isDetached) return@Runnable
-                val context = getActivity()
-                if (context == null || context.isFinishing) return@Runnable
-                val res = resources
-                if (isDir) {
-                    detailsOverview.imageDrawable = ContextCompat.getDrawable(context, if (TextUtils.equals(media.uri.scheme, "file"))
-                        R.drawable.ic_menu_folder_big
-                    else
-                        R.drawable.ic_menu_network_big)
-                    detailsOverview.isImageScaleUpAllowed = true
-                    detailsOverview.addAction(Action(ID_BROWSE.toLong(), res.getString(R.string.browse_folder)))
-                    if (canSave) detailsOverview.addAction(if (browserFavExists) actionDelete else actionAdd)
+        lifecycleScope.launchWhenStarted {
+            val cover = if (viewModel.media.type == AbstractMediaWrapper.TYPE_AUDIO || viewModel.media.type == AbstractMediaWrapper.TYPE_VIDEO)
+                withContext(Dispatchers.IO) { AudioUtil.readCoverBitmap(viewModel.mediaItemDetails.artworkUrl, 512) }
+            else null
+            val blurred = cover?.let { withContext(Dispatchers.IO) { UiTools.blurBitmap(it) } }
+            val browserFavExists = withContext(Dispatchers.IO) { browserFavRepository.browserFavExists(Uri.parse(viewModel.mediaItemDetails.location)) }
+            val isDir = viewModel.media.type == AbstractMediaWrapper.TYPE_DIR
+            val canSave = isDir && withContext(Dispatchers.IO) { FileUtils.canSave(viewModel.media) }
+            if (activity.isFinishing) return@launchWhenStarted
+            val res = resources
+            if (isDir) {
+                detailsOverview.imageDrawable = ContextCompat.getDrawable(activity, if (TextUtils.equals(viewModel.media.uri.scheme, "file"))
+                    R.drawable.ic_menu_folder_big
+                else
+                    R.drawable.ic_menu_network_big)
+                detailsOverview.isImageScaleUpAllowed = true
+                detailsOverview.addAction(Action(ID_BROWSE.toLong(), res.getString(R.string.browse_folder)))
+                if (canSave) detailsOverview.addAction(if (browserFavExists) actionDelete else actionAdd)
 
-                } else if (media.type == AbstractMediaWrapper.TYPE_AUDIO) {
-                    // Add images and action buttons to the details view
-                    if (cover == null)
-                        detailsOverview.imageDrawable = ContextCompat.getDrawable(context, R.drawable.ic_default_cone)
-                    else
-                        detailsOverview.setImageBitmap(context, cover)
-
-                    detailsOverview.addAction(Action(ID_PLAY.toLong(), res.getString(R.string.play)))
-                    detailsOverview.addAction(Action(ID_LISTEN.toLong(), res.getString(R.string.listen)))
-                    detailsOverview.addAction(Action(ID_PLAYLIST.toLong(), res.getString(R.string.add_to_playlist)))
-                } else if (media.type == AbstractMediaWrapper.TYPE_VIDEO) {
-                    // Add images and action buttons to the details view
-                    if (cover == null)
-                        detailsOverview.imageDrawable = ContextCompat.getDrawable(context, R.drawable.ic_default_cone)
-                    else
-                        detailsOverview.setImageBitmap(context, cover)
-
-                    detailsOverview.addAction(Action(ID_PLAY.toLong(), res.getString(R.string.play)))
-                    detailsOverview.addAction(Action(ID_PLAY_FROM_START.toLong(), res.getString(R.string.play_from_start)))
-                    if (FileUtils.canWrite(media.uri))
-                        detailsOverview.addAction(Action(ID_DL_SUBS.toLong(), res.getString(R.string.download_subtitles)))
-                    detailsOverview.addAction(Action(ID_PLAYLIST.toLong(), res.getString(R.string.add_to_playlist)))
+            } else if (viewModel.media.type == AbstractMediaWrapper.TYPE_AUDIO) {
+                // Add images and action buttons to the details view
+                if (cover == null) {
+                    detailsOverview.imageDrawable = ContextCompat.getDrawable(activity, R.drawable.ic_default_cone)
+                } else {
+                    detailsOverview.setImageBitmap(context, cover)
                 }
-                rowsAdapter.add(detailsOverview)
-                adapter = rowsAdapter
-                if (blurred != null) backgroundManager.setBitmap(blurred)
-            })
-        })
+                detailsOverview.addAction(Action(ID_PLAY.toLong(), res.getString(R.string.play)))
+                detailsOverview.addAction(Action(ID_LISTEN.toLong(), res.getString(R.string.listen)))
+                detailsOverview.addAction(Action(ID_PLAYLIST.toLong(), res.getString(R.string.add_to_playlist)))
+            } else if (viewModel.media.type == AbstractMediaWrapper.TYPE_VIDEO) {
+                // Add images and action buttons to the details view
+                if (cover == null) {
+                    detailsOverview.imageDrawable = ContextCompat.getDrawable(activity, R.drawable.ic_default_cone)
+                } else {
+                    detailsOverview.setImageBitmap(context, cover)
+                }
+                detailsOverview.addAction(Action(ID_PLAY.toLong(), res.getString(R.string.play)))
+                detailsOverview.addAction(Action(ID_PLAY_FROM_START.toLong(), res.getString(R.string.play_from_start)))
+                if (FileUtils.canWrite(viewModel.media.uri))
+                    detailsOverview.addAction(Action(ID_DL_SUBS.toLong(), res.getString(R.string.download_subtitles)))
+                detailsOverview.addAction(Action(ID_PLAYLIST.toLong(), res.getString(R.string.add_to_playlist)))
+            }
+            rowsAdapter.add(detailsOverview)
+            adapter = rowsAdapter
+            blurred?.let { backgroundManager.setBitmap(blurred) }
+        }
     }
+}
 
-    companion object {
-        private const val TAG = "MediaItemDetailsFragment"
-        private const val ID_PLAY = 1
-        private const val ID_LISTEN = 2
-        private const val ID_FAVORITE_ADD = 3
-        private const val ID_FAVORITE_DELETE = 4
-        private const val ID_BROWSE = 5
-        private const val ID_DL_SUBS = 6
-        private const val ID_PLAY_FROM_START = 7
-        private const val ID_PLAYLIST = 8
-    }
+class MediaItemDetailsModel : ViewModel() {
+    lateinit var mediaItemDetails: MediaItemDetails
+    lateinit var media: AbstractMediaWrapper
+    var mediaStarted = false
 }
