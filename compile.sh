994d698983eb9375535fa6a57a9036cc451280f3
Geoffrey Métais <geoffrey.metais@gmail.com>
2019-11-27 11:18:39 +0100

    Update SDK licenses
    (cherry picked from commit 685620ecc547878882e5ef90cac5054230b7f66c)

--- compile.sh
+++ compile.sh
@@ -236,6 +236,7 @@ if [ ! -d "$ANDROID_SDK/licenses" ]; then
     mkdir "$ANDROID_SDK/licenses"
     echo "24333f8a63b6825ea9c5514f83c2829b004d1fee" > "$ANDROID_SDK/licenses/android-sdk-license"
     echo "d56f5187479451eabf01fb78af6dfcb131a6481e" >> "$ANDROID_SDK/licenses/android-sdk-license"
+    echo "24333f8a63b6825ea9c5514f83c2829b004d1fee" >> "$ANDROID_SDK/licenses/android-sdk-license"
 fi
 
 ##########
