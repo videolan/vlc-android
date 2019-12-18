0f46c2ee97056806e1a5c76340dc4ad22a1a3a02
Geoffrey Métais <geoffrey.metais@gmail.com>
2019-12-18 10:55:14 +0000

    Medialibrary: Avoid double init
    (cherry picked from commit b1c99f29a3dcb8f0c21bac1195f2ab584437482c)

--- vlc-android/src/org/videolan/vlc/MediaParsingService.kt
+++ vlc-android/src/org/videolan/vlc/MediaParsingService.kt
@@ -421,6 +421,10 @@ class MediaParsingService : Service(), DevicesDiscoveryCb, CoroutineScope, Lifec
                 medialibrary.discover(action.path)
             }
             is Init -> {
+                if (medialibrary.isInitiated) {
+                    exitCommand()
+                    return@actor
+                }
                 val context = this@MediaParsingService
                 var shouldInit = !dbExists()
                 val initCode = medialibrary.init(context)
