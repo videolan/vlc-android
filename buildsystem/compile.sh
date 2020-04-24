c1b276f895478869e0da43b1315158067d9dfd86
Nicolas Pomepuy <nicolas@videolabs.io>
2020-04-24 14:52:42 +0200

    Update hash for libvlc + lib versions
    (cherry picked from commit b7eb2b0de400a12248092dc321befa873c0aaa37)

--- buildsystem/compile.sh
+++ buildsystem/compile.sh
@@ -269,7 +269,7 @@ fi
 # Fetch VLC source #
 ####################
 
-TESTED_HASH=17c2aa0
+TESTED_HASH=7f145af
 VLC_REPOSITORY=https://git.videolan.org/git/vlc/vlc-3.0.git
 if [ ! -d "vlc" ]; then
     diagnostic "VLC sources: not found, cloning"
