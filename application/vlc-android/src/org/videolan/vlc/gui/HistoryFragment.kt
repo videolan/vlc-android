89f7d993809ce5990d26b42e8cf49e98ad9172c8
Nicolas Pomepuy <nicolas@videolabs.io>
2020-12-03 14:08:32 +0100

    Add an option to browse the parent of a media on mobile and TV
    Fixes #1394
    
    (cherry picked from commit d9c6ef284e3b7236fb5b88a8b787d57725f0246e)

--- application/vlc-android/src/org/videolan/vlc/gui/HistoryFragment.kt
+++ application/vlc-android/src/org/videolan/vlc/gui/HistoryFragment.kt
@@ -38,6 +38,7 @@ import org.videolan.medialibrary.media.MediaLibraryItem
 import org.videolan.tools.KeyHelper
 import org.videolan.tools.MultiSelectHelper
 import org.videolan.tools.isStarted
+import org.videolan.tools.retrieveParent
 import org.videolan.vlc.R
 import org.videolan.vlc.gui.browser.MediaBrowserFragment
 import org.videolan.vlc.gui.helpers.*
@@ -177,6 +178,7 @@ class HistoryFragment : MediaBrowserFragment<HistoryModel>(), IRefreshable, IHis
         }
         menu.findItem(R.id.action_history_info).isVisible = selectionCount == 1
         menu.findItem(R.id.action_history_append).isVisible = PlaylistManager.hasMedia()
+        menu.findItem(R.id.action_go_to_folder).isVisible = selectionCount == 1 && multiSelectHelper.getSelection().first().uri.retrieveParent() != null
         return true
     }
 
@@ -187,7 +189,8 @@ class HistoryFragment : MediaBrowserFragment<HistoryModel>(), IRefreshable, IHis
             when (item.itemId) {
                 R.id.action_history_play -> MediaUtils.openList(activity, selection, 0)
                 R.id.action_history_append -> MediaUtils.appendMedia(activity, selection)
-                R.id.action_history_info -> showInfoDialog(selection[0])
+                R.id.action_history_info -> showInfoDialog(selection.first())
+                R.id.action_go_to_folder -> showParentFolder(selection.first())
                 else -> {
                     stopActionMode()
                     return false
