b725d447ed94c0493b4a52bc087f6c44a54b010e
Habib Kazemi <kazemihabib1996@gmail.com>
2020-12-31 17:44:06 +0330

    Use ViewPager2 for Onboarding to support RTL
    Fixes #1578 problem 1
    
    (cherry picked from commit 3557ff11d02725aef910339ec2fd8852836b40f5)

--- application/vlc-android/src/org/videolan/vlc/gui/onboarding/OnboardingFoldersFragment.kt
+++ application/vlc-android/src/org/videolan/vlc/gui/onboarding/OnboardingFoldersFragment.kt
@@ -7,17 +7,8 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import org.videolan.vlc.R
-import org.videolan.vlc.gui.browser.StorageBrowserFragment
 
 class OnboardingFoldersFragment : Fragment() {
-
-
-    private lateinit var storageBrowserFragment: StorageBrowserFragment
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        storageBrowserFragment = StorageBrowserFragment()
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.onboarding_folders, container, false)
@@ -34,14 +25,6 @@ class OnboardingFoldersFragment : Fragment() {
                 else -> title.visibility = View.VISIBLE
             }
         }
-
-
-
-        requireActivity().supportFragmentManager.beginTransaction()
-                .add(R.id.fragment_placeholder, storageBrowserFragment)
-                .commit()
-
-
     }
 
 
