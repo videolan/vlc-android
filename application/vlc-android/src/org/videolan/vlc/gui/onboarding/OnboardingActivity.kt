772eed8babb5821278da8bb55085d368fc98ed3c
Nicolas Pomepuy <nicolas@videolabs.io>
2021-06-21 07:21:36 +0200

    New onboarding color

--- application/vlc-android/src/org/videolan/vlc/gui/onboarding/OnboardingActivity.kt
+++ application/vlc-android/src/org/videolan/vlc/gui/onboarding/OnboardingActivity.kt
@@ -53,12 +53,6 @@ class OnboardingActivity : AppCompatActivity(), IOnScanningCustomizeChangedListe
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-            window.statusBarColor = ContextCompat.getColor(this, R.color.onboarding_grey_dark)
-        }
-
         viewModel.permissionGranted = Permissions.canReadStorage(applicationContext)
         setContentView(R.layout.activity_onboarding)
         viewPager = findViewById(R.id.pager)
