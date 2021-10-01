31ac8d0a32ffef2196f16d4aacda6635af61953a
Nicolas Pomepuy <nicolas@videolabs.io>
2021-10-01 09:26:30 +0200

    Prevent IllegalStateException when the EqualizerFragment is not attached

--- application/vlc-android/src/org/videolan/vlc/gui/audio/EqualizerFragment.kt
+++ application/vlc-android/src/org/videolan/vlc/gui/audio/EqualizerFragment.kt
@@ -316,6 +316,7 @@ class EqualizerFragment : VLCBottomSheetDialogFragment() {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) { }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (context == null) return
                 val newName = input.text.toString()
                 if (input.text.contains("_") || newName == newPresetName) {
                     input.error = getString(R.string.custom_set_wrong_input)
