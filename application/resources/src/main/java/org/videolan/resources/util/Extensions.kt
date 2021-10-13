875376d49b58235fc3029f8c07c05a9f305970f9
Nicolas Pomepuy <nicolas@videolabs.io>
2021-10-13 14:40:29 +0200

    Add a new waitForML method to block the current coroutine while ML init

--- application/resources/src/main/java/org/videolan/resources/util/Extensions.kt
+++ application/resources/src/main/java/org/videolan/resources/util/Extensions.kt
@@ -11,6 +11,11 @@ import java.io.File
 import kotlin.coroutines.resume
 
 
+/**
+ * Allows getting a ready medialibrary to query it.
+ * @param block: the unit to invoke when the medialibrary is ready
+ */
+@ObsoleteCoroutinesApi
 @ExperimentalCoroutinesApi
 suspend inline fun <reified T> Context.getFromMl(crossinline block: Medialibrary.() -> T) = withContext(Dispatchers.IO) {
     val ml = Medialibrary.getInstance()
@@ -32,6 +37,33 @@ suspend inline fun <reified T> Context.getFromMl(crossinline block: Medialibrary
             continuation.invokeOnCancellation { ml.removeOnMedialibraryReadyListener(listener) }
             ml.addOnMedialibraryReadyListener(listener)
             startMedialibrary(false, false, scan)
+        }
+    }
+}
+
+/**
+ * Blocks the current coroutine while the medialibrary is not ready.
+ * Useful when we know the medialibrary init has been launched,
+ * we already have an instance of it and we want to wait that it's ready to query it
+ */
+@ExperimentalCoroutinesApi
+suspend inline fun waitForML() = withContext(Dispatchers.IO) {
+    val ml = Medialibrary.getInstance()
+    if (!ml.isStarted){
+        suspendCancellableCoroutine<() -> Unit> { continuation ->
+            val listener = object : Medialibrary.OnMedialibraryReadyListener {
+                override fun onMedialibraryReady() {
+                    val cb = this
+                    if (!continuation.isCompleted) launch(start = CoroutineStart.UNDISPATCHED) {
+                        continuation.resume {}
+                        yield()
+                        ml.removeOnMedialibraryReadyListener(cb)
+                    }
+                }
+                override fun onMedialibraryIdle() {}
+            }
+            continuation.invokeOnCancellation { ml.removeOnMedialibraryReadyListener(listener) }
+            ml.addOnMedialibraryReadyListener(listener)
         }
     }
 }
