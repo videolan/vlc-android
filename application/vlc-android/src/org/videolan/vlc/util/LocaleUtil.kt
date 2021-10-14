03598d257a4b7ba6d1ca7ff2b5414d02a12213af
Nicolas Pomepuy <nicolas@videolabs.io>
2021-10-14 13:32:53 +0200

    Import VLC's locale list

---
+++ application/vlc-android/src/org/videolan/vlc/util/LocaleUtil.kt
@@ -0,0 +1,116 @@
+/*
+ * ************************************************************************
+ *  LocaleUtil.kt
+ * *************************************************************************
+ * Copyright © 2021 VLC authors and VideoLAN
+ * Author: Nicolas POMEPUY
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ * **************************************************************************
+ *
+ *
+ */
+
+package org.videolan.vlc.util
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import org.videolan.resources.AppContextProvider
+import org.videolan.tools.firstLetterUppercase
+import java.util.*
+
+object LocaleUtil {
+
+    /**
+     * Get a locale name from a locale extracted from libvlc
+     * @param from the track language string from libvlc
+     *
+     * @return a localized string for this language.
+     * Fallback on VLC's language name if Android doesn't know the locale and falls back on returning [from] if nothing is found
+     */
+    fun getLocaleName(from: String): String {
+        vlcLocaleList.forEach {
+            if (it.language == from) return toTranslatedLanguage(it)
+            it.values.forEach { sub ->
+                if (sub == from) return toTranslatedLanguage(it)
+            }
+        }
+        return from
+    }
+
+    /**
+     * Get an ISO-639-1 locale string from a Track language sent by libvlc
+     * @param from the track language string from libvlc
+     *
+     * @return an ISO-639-1 locale string. Returns null if nothing is found
+     */
+    fun getLocaleFromVLC(from: String): String? {
+        vlcLocaleList.forEach {
+            if (it.language == from) return it.values[0]
+            it.values.forEach { sub ->
+                if (sub == from) return it.values[0]
+            }
+        }
+        return null
+    }
+
+    /**
+     * Get a translated language name from a [VLCLocale]
+     * @param entry: the [VLCLocale] to get the language from
+     *
+     * @return a language string in the user's locale if found by Android.
+     * Falls back on VLC's language name if not found
+     */
+    private fun toTranslatedLanguage(entry: VLCLocale): String {
+        val it = entry.values[0]
+        val androidLocale = Locale(it).displayLanguage
+        if (androidLocale != it) return androidLocale.firstLetterUppercase()
+        return entry.language
+    }
+
+    /**
+     * The VLC locale list extracted from the assets
+     */
+    private val vlcLocaleList by lazy {
+
+        val jsonData = AppContextProvider.appContext.assets.open("vlc_locales.json").bufferedReader().use {
+            it.readText()
+        }
+
+        val moshi = Moshi.Builder().build()
+        val type = Types.newParameterizedType(MutableList::class.java, VLCLocale::class.java)
+
+        val jsonAdapter: JsonAdapter<List<VLCLocale>> = moshi.adapter(type)
+
+        jsonAdapter.fromJson(jsonData)!!
+
+    }
+
+    /**
+     * VLC locale object
+     * @param language: the language VLC uses
+     * @param values: the ISO values (ISO-639-1, ISO-639-2T, ISO-639-2B) when applicable
+     *
+     * @see [https://code.videolan.org/videolan/vlc/-/blob/master/src/text/iso-639_def.h]
+     */
+    data class VLCLocale(
+            @field:Json(name = "language")
+            val language: String,
+            @field:Json(name = "values")
+            val values: List<String>
+    )
+
+}
