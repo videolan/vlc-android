4b2db496e461be8e1dc4d2b200e0f6ef2c408ad6
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2021-12-03 16:57:09 +0100

    medialibrary: Android.mk: Include CLEAN_VARS
    This will clean the variables and will prevent us from having to
    override some defaults that were polluted by the previous Android.mk
    builds

--- medialibrary/jni/Android.mk
+++ medialibrary/jni/Android.mk
@@ -1,4 +1,5 @@
 LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
 LOCAL_SRC_FILES := medialibrary.cpp AndroidMediaLibrary.cpp AndroidDeviceLister.cpp utils.cpp
 LOCAL_MODULE    := mla
 LOCAL_MODULE_FILENAME := libmla
