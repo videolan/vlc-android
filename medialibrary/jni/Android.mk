aa28ff5b153cba40fc30755534d2a966bacee0b7
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2021-12-03 16:58:00 +0100

    medialibrary: Android.mk: Remove explicit set of a default value
    LOCAL_MODULE_FILENAME defaults to lib<modulename>.so

--- medialibrary/jni/Android.mk
+++ medialibrary/jni/Android.mk
@@ -2,7 +2,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := medialibrary.cpp AndroidMediaLibrary.cpp AndroidDeviceLister.cpp utils.cpp
 LOCAL_MODULE    := mla
-LOCAL_MODULE_FILENAME := libmla
 LOCAL_LDLIBS    := $(MEDIALIBRARY_LDLIBS) -llog
 LOCAL_C_INCLUDES := $(MEDIALIBRARY_INCLUDE_DIR)
 include $(BUILD_SHARED_LIBRARY)
