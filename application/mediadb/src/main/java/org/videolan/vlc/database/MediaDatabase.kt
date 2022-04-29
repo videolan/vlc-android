856c1cb44c56ea7fb20714e1564c46e7a3bcfd30
Robert Stone <rhstone@gmail.com>
2022-04-29 10:39:45 -0700

    Update headers and disable schema export

--- application/mediadb/src/main/java/org/videolan/vlc/database/MediaDatabase.kt
+++ application/mediadb/src/main/java/org/videolan/vlc/database/MediaDatabase.kt
@@ -1,27 +1,3 @@
-/*
- * ************************************************************************
- *  MediaDatabase.kt
- * *************************************************************************
- * Copyright © 2020 VLC authors and VideoLAN
- * Author: Nicolas POMEPUY
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
- * **************************************************************************
- *
- *
- */
-
 /*******************************************************************************
  *  MediaDatabase.kt
  * ****************************************************************************
@@ -60,7 +36,7 @@ import org.videolan.vlc.mediadb.models.Slave
 
 private const val DB_NAME = "vlc_database"
 
-@Database(entities = [ExternalSub::class, Slave::class, BrowserFav::class, CustomDirectory::class], version = 30)
+@Database(entities = [ExternalSub::class, Slave::class, BrowserFav::class, CustomDirectory::class], version = 30, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class MediaDatabase: RoomDatabase() {
     abstract fun externalSubDao(): ExternalSubDao
