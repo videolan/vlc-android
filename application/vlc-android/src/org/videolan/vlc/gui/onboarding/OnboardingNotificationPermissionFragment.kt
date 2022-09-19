a277b16f51c6b31e8dcb26c57942467e90e18173
Nicolas Pomepuy <nicolas@videolabs.io>
2022-09-19 09:31:55 +0200

    Target API 33 and ask for notification permission in onboarding

---
+++ application/vlc-android/src/org/videolan/vlc/gui/onboarding/OnboardingNotificationPermissionFragment.kt
@@ -0,0 +1,55 @@
+/*
+ * ************************************************************************
+ *  OnboardingNotificationPermissionFragment.kt
+ * *************************************************************************
+ * Copyright © 2022 VLC authors and VideoLAN
+ * Author: Nicolas POMEPUY
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ * **************************************************************************
+ *
+ *
+ */
+
+package org.videolan.vlc.gui.onboarding
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import org.videolan.vlc.R
+
+class OnboardingNotificationPermissionFragment : OnboardingFragment() {
+    private lateinit var titleView: TextView
+    override fun getDefaultViewForTalkback() = titleView
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.onboarding_notification_permission, container, false)
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        titleView = view.findViewById(R.id.permission_title)
+    }
+
+    companion object {
+        fun newInstance(): OnboardingNotificationPermissionFragment {
+            return OnboardingNotificationPermissionFragment()
+        }
+    }
+}
