40b05d1f13343adfa3adaf0e6f9559ad0927287f
Nicolas Pomepuy <nicolas@videolabs.io>
2022-10-06 12:23:08 +0200

    New webpack project to generate a static website

---
+++ buildsystem/network-sharing-server/js/app.js
@@ -0,0 +1,12 @@
+import {MDCRipple} from '@material/ripple';
+import {MDCTopAppBar} from '@material/top-app-bar';
+import {MDCDataTable} from '@material/data-table';
+
+
+import icon from "../asset/resource/icon.png"
+import android_chrome_192 from "../asset/resource/android-chrome-192x192.png"
+import android_chrome_512 from "../asset/resource/android-chrome-512x512.png"
+import favicon from "../asset/resource/favicon.ico"
+import favicon_16 from "../asset/resource/favicon-16x16.png"
+import favicon_32 from "../asset/resource/favicon-32x32.png"
+import index from "../html/index.html"
---
+++ buildsystem/network-sharing-server/webpack.config.js
@@ -0,0 +1,73 @@
+var path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = [{
+  mode: "production",
+  entry: ['./scss/app.scss', './js/app.js'],
+  output: {
+    // This is necessary for webpack to compile
+    // But we never use style-bundle.js
+    filename: 'style-bundle.js',
+    path: path.join(__dirname, "../../application/vlc-android/assets/web/public"),
+    assetModuleFilename: 'images/[name][ext]'
+  },
+  plugins: [
+      new CopyPlugin({
+        patterns: [
+          { from: "asset/static", to: "" },
+          { from: "asset/templates", to: "" },
+        ],
+      }),
+    ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'bundle.css',
+            },
+          },
+          {
+            loader: "extract-loader",
+            options: {
+              publicPath: path.join(__dirname, "public"),
+            }
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              url: false
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              // Prefer Dart Sass
+              implementation: require('sass'),
+
+              // See https://github.com/webpack-contrib/sass-loader/issues/804
+              webpackImporter: false,
+              sassOptions: {
+                includePaths: ['./node_modules']
+              },
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: '[name][ext]'
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  },
+}];
