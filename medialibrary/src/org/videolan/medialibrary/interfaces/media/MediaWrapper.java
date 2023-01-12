1cf5481d90740cdb5296bb9fe37430cbac7bee84
Duncan McNamara <dcn.mcnamara@gmail.com>
2023-01-12 18:43:34 +0100

    MediaWrapper: fix issue with smb title being empty
    Filenames with smb share titles will be null, meaning that title from
    libvlc will never be chosen, causing an empty string to be chosen down
    the line as title.

--- medialibrary/src/org/videolan/medialibrary/interfaces/media/MediaWrapper.java
+++ medialibrary/src/org/videolan/medialibrary/interfaces/media/MediaWrapper.java
@@ -394,7 +394,7 @@ public abstract class MediaWrapper extends MediaLibraryItem implements Parcelabl
     private String updateTitleMeta(IMedia media) {
         String libvlcTitle = getMetaId(media, mTitle, Media.Meta.Title, true);
         String fileName = getFileName();
-        if (!TextUtils.isEmpty(libvlcTitle) && !TextUtils.isEmpty(fileName) && !libvlcTitle.equals(fileName))
+        if (TextUtils.isEmpty(fileName) || fileName.equals(libvlcTitle))
             return libvlcTitle;
         return getTitle();
     }
