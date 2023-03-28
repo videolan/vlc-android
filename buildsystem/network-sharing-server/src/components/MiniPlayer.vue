bc942a39163b628e0d62ab7de8e7f8ddedcfff2a
Duncan McNamara <dcn.mcnamara@gmail.com>
2023-03-28 18:00:41 +0200

    WebServer: move player js to component

--- buildsystem/network-sharing-server/src/components/MiniPlayer.vue
+++ buildsystem/network-sharing-server/src/components/MiniPlayer.vue
@@ -31,11 +31,113 @@
 
 <script>
 import PlayerButton from './PlayerButton.vue'
+import { API_IP, API_URL } from '../config.js'
 
 export default {
   components: {
-    PlayerButton
+    PlayerButton,
   },
+  data() {
+    return {
+      playerWS: WebSocket,
+    }
+  },
+  computed: {
+  },
+  methods: {
+    msecToTime(ms) {
+      const seconds = Math.floor((ms / 1000) % 60)
+      const minutes = Math.floor((ms / (60 * 1000)) % 60)
+      const hours = Math.floor((ms / (3600 * 1000)) % 3600)
+      return `${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds
+        }`
+    },
+    initEventListeners() {
+      const play = document.getElementById("player_play");
+      const pause = document.getElementById("player_pause");
+      const previous = document.getElementById("player_previous");
+      const next = document.getElementById("player_next");
+      const shuffle = document.getElementById("player_shuffle");
+      const repeat = document.getElementById("player_repeat");
+      const previous10 = document.getElementById("player_previous_10");
+      const next10 = document.getElementById("player_next_10");
+      play.addEventListener('click', () => {
+        this.playerWS.send("play");
+      });
+      pause.addEventListener('click', () => {
+        this.playerWS.send("pause");
+      });
+      previous.addEventListener('click', () => {
+        this.playerWS.send("previous");
+      });
+      next.addEventListener('click', () => {
+        this.playerWS.send("next");
+      });
+      shuffle.addEventListener('click', () => {
+        this.playerWS.send("shuffle");
+      });
+      repeat.addEventListener('click', () => {
+        this.playerWS.send("repeat");
+      });
+      previous10.addEventListener('click', () => {
+        this.playerWS.send("previous10");
+      });
+      next10.addEventListener('click', () => {
+        this.playerWS.send("next10");
+      });
+    },
+    removeEventListeners() {
+      const play = document.getElementById("player_play");
+      const pause = document.getElementById("player_pause");
+      const previous = document.getElementById("player_previous");
+      const next = document.getElementById("player_next");
+      const shuffle = document.getElementById("player_shuffle");
+      const repeat = document.getElementById("player_repeat");
+      const previous10 = document.getElementById("player_previous_10");
+      const next10 = document.getElementById("player_next_10");
+      play.removeEventListeners()
+      pause.removeEventListeners()
+      previous.removeEventListeners()
+      next.removeEventListeners()
+      shuffle.removeEventListeners()
+      repeat.removeEventListeners()
+      previous10.removeEventListeners()
+      next10.removeEventListeners()
+    },
+  },
+  mounted: function () {
+    const play = document.getElementById("player_play");
+    const pause = document.getElementById("player_pause");
+    this.playerWS = new WebSocket("ws://" + API_IP + "/echo", "player");
+    this.initEventListeners();
+    var lastLoadedMediaUri = ""
+    this.playerWS.onmessage = (event) => {
+      const msg = JSON.parse(event.data);
+
+      const title = document.getElementById("title");
+      const artist = document.getElementById("artist");
+      const time = document.getElementById("time");
+      const duration = document.getElementById("duration");
+      const artwork = document.getElementById("player_artwork");
+      title.textContent = msg.title
+      artist.textContent = msg.artist
+      time.textContent = this.msecToTime(new Date(msg.progress))
+      duration.textContent = this.msecToTime(new Date(msg.duration))
+      if (lastLoadedMediaUri != msg.uri) {
+        artwork.src = API_URL + "/artwork?randomizer=" + Date.now()
+        lastLoadedMediaUri = msg.uri
+      }
+
+      if (msg.playing) {
+        play.style.display = "none";
+        pause.style.display = "inline-block";
+      } else {
+        play.style.display = "inline-block";
+        pause.style.display = "none";
+      }
+
+    }
+  }
 }
 </script>
 
