88b459795af1da579f3bacafbd6fcdd650e0c8c1
Nicolas Pomepuy <nicolas@videolabs.io>
2023-04-20 15:36:27 +0200

    Webserver: hide the play queue on mobile back button

--- buildsystem/network-sharing-server/src/main.js
+++ buildsystem/network-sharing-server/src/main.js
@@ -5,16 +5,30 @@ import './scss/app.scss'
 import routes from './routes'
 import { createPinia } from 'pinia'
 import { initI18n } from './i18n'
-import vlcUtils  from './plugins/vlcUtils'
+import vlcUtils from './plugins/vlcUtils'
 import VueLazyload from 'vue-lazyload'
+import { playerStore } from './stores/PlayerStore'
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes,
+  history: createWebHashHistory(),
+  routes,
+})
+
+
+
+router.beforeEach((to, from, next) => {
+  const store = playerStore()
+  if (store.responsivePlayerShowing) {
+    store.playqueueShowing = false
+    store.responsivePlayerShowing = false
+    next(false)
+  } else {
+    next()
+  }
 })
 
 const pinia = createPinia()
-initI18n().then(function(i18n) {
-    createApp(App).use(i18n).use(VueLazyload).use(router).use(pinia).use(vlcUtils).mount('#app')
+initI18n().then(function (i18n) {
+  createApp(App).use(i18n).use(VueLazyload).use(router).use(pinia).use(vlcUtils).mount('#app')
 })
 
