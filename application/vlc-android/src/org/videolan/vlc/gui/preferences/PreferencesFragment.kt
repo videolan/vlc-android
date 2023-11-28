c381480befd6f99534f59fedc0a87555fbe44255
Nicolas Pomepuy <nicolas@videolabs.io>
2023-11-28 08:14:24 +0100

    Disable the webserver on API 22
    Ktor has been tested on Android 21+. However, when using it on Android 21
    we still get a crash that we should try and fix

--- application/vlc-android/src/org/videolan/vlc/gui/preferences/PreferencesFragment.kt
+++ application/vlc-android/src/org/videolan/vlc/gui/preferences/PreferencesFragment.kt
@@ -26,6 +26,7 @@ package org.videolan.vlc.gui.preferences
 import android.app.Activity
 import android.content.Intent
 import android.content.SharedPreferences
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
@@ -46,9 +47,12 @@ import org.videolan.resources.KEY_CURRENT_MEDIA_RESUME
 import org.videolan.resources.KEY_MEDIA_LAST_PLAYLIST
 import org.videolan.resources.KEY_MEDIA_LAST_PLAYLIST_RESUME
 import org.videolan.resources.util.parcelable
-import org.videolan.tools.*
+import org.videolan.tools.AUDIO_RESUME_PLAYBACK
+import org.videolan.tools.PLAYBACK_HISTORY
+import org.videolan.tools.RESULT_RESTART
 import org.videolan.tools.Settings
 import org.videolan.tools.Settings.isPinCodeSet
+import org.videolan.tools.VIDEO_RESUME_PLAYBACK
 import org.videolan.vlc.R
 import org.videolan.vlc.gui.PinCodeActivity
 import org.videolan.vlc.gui.PinCodeReason
@@ -78,6 +82,11 @@ class PreferencesFragment : BasePreferenceFragment(), SharedPreferences.OnShared
     override fun onStop() {
         super.onStop()
         preferenceScreen.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        findPreference<Preference>("webserver_category")?.isVisible = Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
