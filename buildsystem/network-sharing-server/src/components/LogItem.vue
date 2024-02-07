9dbd9b9ea6b0030bccb423b1f5275c9e7dfe3925
Nicolas Pomepuy <nicolas@videolabs.io>
2024-02-07 10:10:56 +0100

    Fix vue warnings for logs

--- buildsystem/network-sharing-server/src/components/LogItem.vue
+++ buildsystem/network-sharing-server/src/components/LogItem.vue
@@ -29,6 +29,8 @@ export default {
   components: {
     ImageButton,
   },
+  emits: ['refresh-logs'],
+
   props: {
     logfile: Object
   },
--- buildsystem/network-sharing-server/src/components/LogList.vue
+++ buildsystem/network-sharing-server/src/components/LogList.vue
@@ -31,7 +31,7 @@ export default {
     LogItem,
   },
   props: {
-    logs: []
+    logs: Array
   }
 
 }
