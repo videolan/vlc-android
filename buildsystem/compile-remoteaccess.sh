3223b6297f2cffd3cd3b866a60cac6fe41cf4b95
Nicolas Pomepuy <nicolas@videolabs.io>
2024-06-12 07:01:13 +0200

    Bump the remote access hash

--- buildsystem/compile-remoteaccess.sh
+++ buildsystem/compile-remoteaccess.sh
@@ -57,7 +57,7 @@ done
 ##############################
   diagnostic "Setting up the Remote Access project"
 
-  REMOTE_ACCESS_TESTED_HASH=b99f099666e4101a36e4454c38ab6e982fde149b
+  REMOTE_ACCESS_TESTED_HASH=b2bca33f7db315abd1c09c4dffc830f1ad48e40a
   REMOTE_ACCESS_REPOSITORY=https://code.videolan.org/videolan/remoteaccess
 
   : ${VLC_REMOTE_ACCESS_PATH:="$(pwd -P)/remoteaccess"}
