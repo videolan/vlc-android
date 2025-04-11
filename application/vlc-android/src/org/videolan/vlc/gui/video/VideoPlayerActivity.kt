videolan/vlc-android

commit 95bb1f3a5bfc49d758ddb3ea7ed2a01e929f31ee

VideoPlayer: fix audio playback when leaving pip
When you open pip with play in background, when you quit pip, in the
onStop, it will switch to audio. If you check if system Pip is running
you can stop that, but it will stop playback from running with the screen off,
so check for screen off also.
This means that isInPiPMode will stay true,
even though PiP has been killed a while ago, audio playback in
background is still running.

application/vlc-android/src/org/videolan/vlc/gui/video/VideoPlayerActivity.kt
@@ -968,15 +968,16 @@ open class VideoPlayerActivity : AppCompatActivity(), PlaybackService.Callback, 
 
         unregisterReceiver(btReceiver)
         alertDialog?.dismiss()
+        val isPlayingPopup = service?.isPlayingPopup ?: false
+        val isSystemPip = (service?.isInPiPMode?.value ?: false) && !isPlayingPopup
         if (displayManager.isPrimary && !isFinishing && service?.isPlaying == true
-                && "1" == settings.getString(KEY_VIDEO_APP_SWITCH, "0") && !PlaybackService.hasRenderer()) {
+                && "1" == settings.getString(KEY_VIDEO_APP_SWITCH, "0") && !PlaybackService.hasRenderer()
+                && ((!isSystemPip && isInteractive) || (isSystemPip && !isInteractive))) {
             switchToAudioMode(false)
         }
-
         cleanUI()
         stopPlayback()
         service?.playlistManager?.videoStatsOn?.postValue(false)
-        val isPlayingPopup = service?.isPlayingPopup ?: false
         if (isInteractive && !isPlayingPopup)
             service?.isInPiPMode?.value = false
 

commit 6fe7bf3123f35c19a6ef8f4503ffacc3a5c95a77

VideoPlayer: fix playback in background regression
The video player should switch to audio only if it is not the system pip
(if not pressing the cross to kill it, will keep audio running) or if
the screen is off.

The previous condition stopped the player from switching to audio when
 playback in background was enabled

Regression introduced in 95bb1f3a5bfc49d758ddb3ea7ed2a01e929f31ee

Fixes #3200

application/vlc-android/src/org/videolan/vlc/gui/video/VideoPlayerActivity.kt
@@ -973,7 +973,7 @@ open class VideoPlayerActivity : AppCompatActivity(), PlaybackService.Callback, 
         val isSystemPip = (service?.isInPiPMode?.value ?: false) && !isPlayingPopup
         if (displayManager.isPrimary && !isFinishing && service?.isPlaying == true
                 && "1" == settings.getString(KEY_VIDEO_APP_SWITCH, "0") && !PlaybackService.hasRenderer()
-                && ((!isSystemPip && isInteractive) || (isSystemPip && !isInteractive))) {
+                && (!isSystemPip || !isInteractive)) {
             switchToAudioMode(false)
         }
         cleanUI()