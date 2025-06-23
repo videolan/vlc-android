8329091e1ac6d5105076145fab598e54b25ea4f8
Steve Lhomme <robux4@ycbcr.xyz>
2025-06-23 15:37:36 +0200

    buildsystem: get the NDK path and full version
    Otherwise stick to the version from the Dockers and let gradle handle the path.
    
    If we want gradle to use the NDK we used to build libvlc/medialibrary we need
    to set the full ndkVersion and the path to that NDK.
    
    The default NDK version 28.2.13676358 correspond to the one we have the current
    Docker image.

--- buildsystem/compile.sh
+++ buildsystem/compile.sh
@@ -203,6 +203,8 @@ init_local_props() {
     echo_props() {
         echo "sdk.dir=$ANDROID_SDK"
         echo "android.ndkPath=$ANDROID_NDK"
+        NDK_FULL_VERSION=$(grep -o '^Pkg.Revision.*[0-9]*.*' $ANDROID_NDK/source.properties |cut -d " " -f 3)
+        echo "android.ndkFullVersion=$NDK_FULL_VERSION"
         if [ $(command -v cmake) >/dev/null 2>&1 ]; then
             # prefix of the cmake installation, not the cmake path or the dir that contains the cmake executable
             echo "cmake.dir=$(dirname $(dirname $(command -v cmake)))"
@@ -245,8 +247,9 @@ init_local_props() {
         while IFS= read -r LINE || [ -n "$LINE" ]; do
             line_sdk_dir="${LINE#sdk.dir=}"
             line_ndk_dir="${LINE#android.ndkPath=}"
+            line_ndk_version="${LINE#android.ndkFullVersion=}"
             line_cmake_dir="${LINE#cmake.dir=}"
-            if [ "x$line_sdk_dir" = "x$LINE" ] && [ "x$line_ndk_dir" = "x$LINE" ] && [ "x$line_cmake_dir" = "x$LINE" ]; then
+            if [ "x$line_sdk_dir" = "x$LINE" ] && [ "x$line_ndk_dir" = "x$LINE" ] && [ "x$line_ndk_version" = "x$LINE" ] && [ "x$line_cmake_dir" = "x$LINE" ]; then
                 echo "$LINE"
             fi
         done <"$1" >"$temp_props"
