d4f0ffa7dbf24204357687f068fe4be8b725043a
Nicolas Pomepuy <nicolas@videolabs.io>
2025-09-05 08:58:16 +0200

    Create the compose theme, type and color classes

---
+++ application/television/src/main/java/org/videolan/television/ui/compose/theme/Color.kt
@@ -0,0 +1,47 @@
+/*
+ * ************************************************************************
+ *  Color.kt
+ * *************************************************************************
+ * Copyright © 2025 VLC authors and VideoLAN
+ * Author: Nicolas POMEPUY
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ * **************************************************************************
+ *
+ *
+ */
+
+package org.videolan.television.ui.compose.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BackgroundColor = Color(0xFF34434e)
+val BackgroundColorDark = Color(0xFF011422)
+
+
+
+val Orange50 = Color(0XFFFFF3E0)
+val Orange100 = Color(0XFFFFDFAE)
+val Orange200 = Color(0XFFFFCA7D)
+val Orange300 = Color(0XFFFFB54C)
+val Orange400 = Color(0XFFFFA11A)
+val Orange500 = Color(0XFFFF8800)
+val Orange600 = Color(0XFFFF7D00)
+val Orange700 = Color(0XFFFF7200)
+val Orange800 = Color(0XFFFF610A)
+val Orange900 = Color(0XFFFF5014)
+
+val White = Color(0XFFFFFFFF)
+val Black = Color(0X00000000)
+val Transparent = Color(0X00FFFFFF)
