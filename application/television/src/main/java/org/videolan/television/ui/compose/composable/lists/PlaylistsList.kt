a8020131ec2522760f62a901988acc93a21c76a9
Nicolas Pomepuy <nicolas@videolabs.io>
2025-09-08 11:32:54 +0200

    First naive implementation of the main TV screen using compose

---
+++ application/television/src/main/java/org/videolan/television/ui/compose/composable/lists/PlaylistsList.kt
@@ -0,0 +1,33 @@
+/*
+ * ************************************************************************
+ *  PlaylistsList.kt
+ * *************************************************************************
+ * Copyright © 2025 VLC authors and VideoLAN
+ * Author: Nicolas POMEPUY
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ * **************************************************************************
+ *
+ *
+ */
+
+package org.videolan.television.ui.compose.composable.lists
+
+import androidx.compose.runtime.Composable
+import androidx.tv.material3.Text
+
+@Composable
+fun PlaylistsList() {
+    Text("Not implemented yet")
+}
