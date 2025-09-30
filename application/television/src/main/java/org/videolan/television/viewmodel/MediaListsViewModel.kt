d21c7269d6ba87e055792fdce909e8f9d262f8c6
Nicolas Pomepuy <nicolas@videolabs.io>
2025-09-30 09:36:16 +0200

    Compose TV: add loading state in view models and use it in a composable

--- application/television/src/main/java/org/videolan/television/viewmodel/MediaListsViewModel.kt
+++ application/television/src/main/java/org/videolan/television/viewmodel/MediaListsViewModel.kt
@@ -26,7 +26,6 @@ package org.videolan.television.viewmodel
 
 import android.app.Application
 import android.util.Log
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
@@ -34,19 +33,14 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.videolan.medialibrary.interfaces.Medialibrary
 import org.videolan.medialibrary.interfaces.media.Playlist
-import org.videolan.medialibrary.media.DummyItem
 import org.videolan.medialibrary.media.MediaLibraryItem
-import org.videolan.resources.AppContextProvider
 import org.videolan.resources.BuildConfig
-import org.videolan.resources.HEADER_PERMISSION
 import org.videolan.resources.MEDIALIBRARY_PAGE_SIZE
 import org.videolan.resources.util.getFromMl
-import org.videolan.television.R
-import org.videolan.tools.getContextWithLocale
 import org.videolan.vlc.util.Permissions
 
 private const val TAG = "VLC/MediaListsViewModel"
-class MediaListsViewModel(app: Application) : AndroidViewModel(app) {
+class MediaListsViewModel(app: Application) : TvMediaViewModel(app) {
 
 
     val audioArtists: MutableLiveData<List<MediaLibraryItem>> = MutableLiveData()
@@ -54,23 +48,30 @@ class MediaListsViewModel(app: Application) : AndroidViewModel(app) {
     val audioTracks: MutableLiveData<List<MediaLibraryItem>> = MutableLiveData()
     val audioGenres: MutableLiveData<List<MediaLibraryItem>> = MutableLiveData()
     val audioPlaylists: MutableLiveData<List<MediaLibraryItem>> = MutableLiveData()
+    val allPlaylists: MutableLiveData<List<MediaLibraryItem>> = MutableLiveData()
     val videos: MutableLiveData<List<MediaLibraryItem>> = MutableLiveData()
-    var videosLoaded = false
+
+    val audioArtistsLoading = MutableLiveData(false)
+    val audioAlbumsLoading = MutableLiveData(false)
+    val audioTracksLoading = MutableLiveData(false)
+    val audioGenresLoading = MutableLiveData(false)
+    val audioPlaylistsLoading = MutableLiveData(false)
+    val allPlaylistsLoading = MutableLiveData(false)
+    val videoLoading = MutableLiveData(false)
+
     var audioArtistLoaded = false
     var audioAlbumLoaded = false
     var audioTrackLoaded = false
     var audioGenreLoaded = false
     var audioPlaylistLoaded = false
-
-    private fun getContext() = getApplication<Application>().applicationContext.getContextWithLocale(AppContextProvider.locale)
-
-    private suspend fun showPermissionItem(list: MutableLiveData<List<MediaLibraryItem>>) = withContext(Dispatchers.Main) {
-        list.value = listOf(DummyItem(HEADER_PERMISSION, getContext().getString(org.videolan.vlc.R.string.permission_media), getContext().getString(org.videolan.vlc.R.string.permission_ask_again)))
-    }
+    var allPlaylistLoaded = false
+    var videosLoaded = false
 
     fun updateVideos() = viewModelScope.launch {
         if (videosLoaded) return@launch
         videosLoaded = true
+        if (BuildConfig.DEBUG) Log.d(TAG, "updateVideos -> true")
+        setLoading(videoLoading, true)
         if (!Permissions.canReadStorage(getContext())) {
             showPermissionItem(videos)
             return@launch
@@ -82,12 +83,15 @@ class MediaListsViewModel(app: Application) : AndroidViewModel(app) {
                 addAll(it)
             }
         }
+        setLoading(videoLoading, false)
+        if (BuildConfig.DEBUG) Log.d(TAG, "updateVideos -> false")
     }
 
     fun updateAudioTracks() = viewModelScope.launch(Dispatchers.IO) {
         if (audioTrackLoaded) return@launch
         audioTrackLoaded = true
         if (BuildConfig.DEBUG) Log.d(TAG, "updateAudioTracks")
+        setLoading(audioTracksLoading, true)
         if (!Permissions.canReadStorage(getContext())) {
             showPermissionItem(audioTracks)
             return@launch
@@ -99,11 +103,13 @@ class MediaListsViewModel(app: Application) : AndroidViewModel(app) {
                 audioTracks.value = it.toMutableList()
             }
         }
+        setLoading(audioTracksLoading, false)
     }
 
     fun updateAudioArtists() = viewModelScope.launch(Dispatchers.IO) {
         if (audioArtistLoaded) return@launch
         audioArtistLoaded = true
+        setLoading(audioArtistsLoading, true)
         if (BuildConfig.DEBUG) Log.d(TAG, "updateAudioArtists")
         if (!Permissions.canReadStorage(getContext())) {
             showPermissionItem(audioArtists)
@@ -116,11 +122,13 @@ class MediaListsViewModel(app: Application) : AndroidViewModel(app) {
                 audioArtists.value = it.toMutableList()
             }
         }
+        setLoading(audioArtistsLoading, false)
     }
 
     fun updateAudioAlbums() = viewModelScope.launch(Dispatchers.IO) {
         if (audioAlbumLoaded) return@launch
         audioAlbumLoaded = true
+        setLoading(audioAlbumsLoading, true)
         if (BuildConfig.DEBUG) Log.d(TAG, "updateAudioAlbums")
         if (!Permissions.canReadStorage(getContext())) {
             showPermissionItem(audioAlbums)
@@ -133,11 +141,13 @@ class MediaListsViewModel(app: Application) : AndroidViewModel(app) {
                 audioAlbums.value = it.toMutableList()
             }
         }
+        setLoading(audioAlbumsLoading, false)
     }
 
     fun updateAudioPlaylists() = viewModelScope.launch(Dispatchers.IO) {
         if (audioPlaylistLoaded) return@launch
         audioPlaylistLoaded = true
+        setLoading(audioPlaylistsLoading, true)
         if (BuildConfig.DEBUG) Log.d(TAG, "updateAudioPlaylists")
         if (!Permissions.canReadStorage(getContext())) {
             showPermissionItem(audioPlaylists)
@@ -150,11 +160,13 @@ class MediaListsViewModel(app: Application) : AndroidViewModel(app) {
                 audioPlaylists.value = it.toMutableList()
             }
         }
+        setLoading(audioPlaylistsLoading, false)
     }
 
     fun updateAudioGenres() = viewModelScope.launch(Dispatchers.IO) {
         if (audioGenreLoaded) return@launch
         audioGenreLoaded = true
+        setLoading(audioGenresLoading, true)
         if (BuildConfig.DEBUG) Log.d(TAG, "updateAudioGenres")
         if (!Permissions.canReadStorage(getContext())) {
             showPermissionItem(audioGenres)
@@ -167,5 +179,25 @@ class MediaListsViewModel(app: Application) : AndroidViewModel(app) {
                 audioGenres.value = it.toMutableList()
             }
         }
+        setLoading(audioGenresLoading, false)
+    }
+
+    fun updateAllPlaylists() = viewModelScope.launch(Dispatchers.IO) {
+        if (allPlaylistLoaded) return@launch
+        allPlaylistLoaded = true
+        setLoading(allPlaylistsLoading, true)
+        if (BuildConfig.DEBUG) Log.d(TAG, "updateAudioPlaylists")
+        if (!Permissions.canReadStorage(getContext())) {
+            showPermissionItem(audioPlaylists)
+            return@launch
+        }
+        getContext().getFromMl {
+            getPagedPlaylists(Playlist.Type.All,Medialibrary.SORT_INSERTIONDATE, true, true, false, MEDIALIBRARY_PAGE_SIZE, 0)
+        }.let {
+            withContext(Dispatchers.Main) {
+                allPlaylists.value = it.toMutableList()
+            }
+        }
+        setLoading(allPlaylistsLoading, false)
     }
 }
