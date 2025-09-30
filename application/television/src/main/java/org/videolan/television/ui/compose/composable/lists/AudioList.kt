7518e9fc94fa2ab10ffb5e7ec162bc0b7e8cd72e
Nicolas Pomepuy <nicolas@videolabs.io>
2025-09-30 15:03:59 +0200

    Compose TV: make the pager fill the whole size

--- application/television/src/main/java/org/videolan/television/ui/compose/composable/lists/AudioList.kt
+++ application/television/src/main/java/org/videolan/television/ui/compose/composable/lists/AudioList.kt
@@ -32,6 +32,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -173,7 +174,10 @@ fun AudioListScreen(onFocusExit: () -> Unit, onFocusEnter: () -> Unit, viewModel
                 )
             }
         }
-        HorizontalPager(pagerState, userScrollEnabled = false) { page ->
+        HorizontalPager(pagerState,
+            userScrollEnabled = false,
+            verticalAlignment = Alignment.Top,
+            modifier = Modifier.fillMaxSize()) { page ->
             when (page) {
                 0 -> ArtistsList()
                 1 -> AlbumsList()
