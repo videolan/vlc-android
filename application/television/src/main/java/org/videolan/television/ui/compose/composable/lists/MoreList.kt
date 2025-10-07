9af4d14d41610095976a3f6834d3aaa903a9672c
Nicolas Pomepuy <nicolas@videolabs.io>
2025-10-07 10:26:41 +0200

    Compose TV: refactor the ContentLine in its own file

--- application/television/src/main/java/org/videolan/television/ui/compose/composable/lists/MoreList.kt
+++ application/television/src/main/java/org/videolan/television/ui/compose/composable/lists/MoreList.kt
@@ -26,49 +26,25 @@ package org.videolan.television.ui.compose.composable.lists
 
 import android.content.Intent
 import androidx.activity.compose.LocalActivity
-import androidx.compose.foundation.background
 import androidx.compose.foundation.focusGroup
-import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.focusProperties
-import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import org.videolan.medialibrary.media.MediaLibraryItem
 import org.videolan.resources.ACTIVITY_RESULT_PREFERENCES
 import org.videolan.television.R
 import org.videolan.television.ui.MainTvActivity
-import org.videolan.television.ui.compose.composable.items.AudioItem
+import org.videolan.television.ui.compose.composable.components.ContentLine
 import org.videolan.television.ui.compose.composable.components.VLCButton
-import org.videolan.television.ui.compose.composable.components.VlcLoader
-import org.videolan.television.ui.compose.theme.Transparent
-import org.videolan.television.ui.compose.theme.WhiteTransparent10
 import org.videolan.television.ui.preferences.PreferencesActivity
 import org.videolan.television.viewmodel.MoreViewModel
 
@@ -109,55 +85,8 @@ fun MoreScreen(onFocusExit: () -> Unit, onFocusEnter: () -> Unit, viewModel: Mor
             }
 
         }
-        ContentLine(history, historyLoading, R.string.history)
+        if (!history.isNullOrEmpty())
+            ContentLine(history, historyLoading, R.string.history)
         ContentLine(streams, streamsLoading, R.string.streams)
-    }
-}
-
-@Composable
-fun ContentLine(items: List<MediaLibraryItem>?, historyLoading: Boolean?, text: Int) {
-    var focused by remember { mutableStateOf(false) }
-    Row(
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .fillMaxWidth()
-            .onFocusChanged {
-                focused = it.isFocused
-            }
-            .padding(top = 24.dp)
-            .clip(RoundedCornerShape(50))
-            .background(if (focused) WhiteTransparent10 else Transparent)
-            .focusable()
-    ) {
-        Text(
-            text = stringResource(text),
-            style = MaterialTheme.typography.headlineSmall,
-            modifier = Modifier
-                .weight(1F)
-                .padding(horizontal = 16.dp)
-        )
-        Icon(
-            painter = painterResource(R.drawable.ic_arrow_right),
-            tint = MaterialTheme.colorScheme.onSurface,
-            contentDescription = stringResource(id = text),
-            modifier = Modifier
-                .width(48.dp)
-                .height(48.dp)
-                .padding(end = 16.dp)
-        )
-    }
-    VlcLoader(historyLoading) {
-        LazyRow(
-            contentPadding = PaddingValues(top = 16.dp),
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
-            modifier = Modifier.focusGroup()
-        ) {
-            items(items?.size ?: 0) { index ->
-                Box(modifier = Modifier.width(150.dp)) {
-                    AudioItem(items!!, index)
-                }
-            }
-        }
     }
 }
