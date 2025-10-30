8ab645de0a4641a98654e07a693e98b00378c1a6
Nicolas Pomepuy <nicolas@videolabs.io>
2025-10-30 07:55:41 +0100

    Compose TV: move the MediaListEntry enum in a dedicated file

---
+++ application/television/src/main/java/org/videolan/television/ui/utils/UiUtils.kt
@@ -0,0 +1,84 @@
+/*
+ * ************************************************************************
+ *  UiUtils.kt
+ * *************************************************************************
+ * Copyright © 2025 VLC authors and VideoLAN
+ * Author: Nicolas POMEPUY
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ * **************************************************************************
+ *
+ *
+ */
+
+package org.videolan.television.ui.utils
+
+import android.content.Context
+import org.videolan.tools.KEY_VIDEOS_CARDS
+import org.videolan.tools.Settings
+import org.videolan.tools.putSingle
+
+enum class MediaListEntry(val inCardsKey: String, val defaultInCard: Boolean, val onlyFavsKey: String) {
+    ARTISTS(inCardsKey = "display_mode_audio_browser_artists", defaultInCard = true, onlyFavsKey = "ArtistsProvider_only_favs"),
+    ALBUMS(inCardsKey = "display_mode_audio_browser_albums", defaultInCard = true, onlyFavsKey = "AlbumsProvider_only_favs"),
+    TRACKS(inCardsKey = "display_mode_audio_browser_track", defaultInCard = false, onlyFavsKey = "TracksProvider_only_favs"),
+    GENRES(inCardsKey = "display_mode_audio_browser_genres", defaultInCard = false, onlyFavsKey = "GenresProvider_only_favs"),
+    AUDIO_PLAYLISTS(inCardsKey = "display_mode_playlists_AudioOnly", defaultInCard = true, onlyFavsKey = "PlaylistsProvider_only_favs"),
+    VIDEO_PLAYLISTS(inCardsKey = "display_mode_playlists_Video", defaultInCard = true, onlyFavsKey = "PlaylistsProvider_only_favs"),
+    ALL_PLAYLISTS(inCardsKey = "display_mode_playlists_All", defaultInCard = false, onlyFavsKey = "PlaylistsProvider_only_favs"),
+    VIDEO(inCardsKey = KEY_VIDEOS_CARDS, defaultInCard = true, onlyFavsKey = "VideosProvider_only_favs"),
+    VIDEO_GROUPS(inCardsKey = KEY_VIDEOS_CARDS, defaultInCard = true, onlyFavsKey = "VideoGroupsProvider_only_favs"),
+    VIDEO_FOLDER(inCardsKey = KEY_VIDEOS_CARDS, defaultInCard = true, onlyFavsKey = "FoldersProvider_only_favs"),
+    BROWSER(inCardsKey = KEY_VIDEOS_CARDS, defaultInCard = true, onlyFavsKey = "");
+
+    /**
+     * Display this entry in cards
+     *
+     * @param context Context used to retrieve the SharedPreferences
+     * @return True if this entry should be displayed in cards, false otherwise
+     */
+    fun displayInCard(context: Context): Boolean {
+        return Settings.getInstance(context).getBoolean(inCardsKey, defaultInCard)
+    }
+
+    /**
+     * Save if this entry should be displayed in card
+     *
+     * @param context Context used to retrieve the SharedPreferences
+     * @param value True if this entry should be displayed in cards, false otherwise
+     */
+    fun saveDisplayInCard(context: Context, value: Boolean) {
+        Settings.getInstance(context).putSingle(inCardsKey, value)
+    }
+
+    /**
+     * Display only favorites
+     *
+     * @param context Context used to retrieve the SharedPreferences
+     * @return True if only favorites should be displayed, false otherwise
+     */
+    fun onlyFavs(context: Context): Boolean {
+        return Settings.getInstance(context).getBoolean(onlyFavsKey, false)
+    }
+
+    /**
+     * Save if only favorites should be displayed
+     *
+     * @param context Context used to retrieve the SharedPreferences
+     * @param value True if only favorites should be displayed, false otherwise
+     */
+    fun saveOnlyFavs(context: Context, value: Boolean) {
+        Settings.getInstance(context).putSingle(onlyFavsKey, value)
+    }
+}
