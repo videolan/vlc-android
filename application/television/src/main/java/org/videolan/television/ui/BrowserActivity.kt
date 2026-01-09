6791987ad97223f0996c1b0bb1a9d5230ff0261a
Nicolas Pomepuy <nicolas@videolabs.io>
2026-01-09 07:35:23 +0100

    Compose TV: start migrating the file browser to compose

---
+++ application/television/src/main/java/org/videolan/television/ui/BrowserActivity.kt
@@ -0,0 +1,60 @@
+/*
+ * ************************************************************************
+ *  BrowserActivity.kt
+ * *************************************************************************
+ * Copyright © 2026 VLC authors and VideoLAN
+ * Author: Nicolas POMEPUY
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ * **************************************************************************
+ *
+ *
+ */
+
+package org.videolan.television.ui
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
+import org.videolan.medialibrary.media.MediaLibraryItem
+import org.videolan.resources.util.parcelable
+import org.videolan.television.ui.compose.composable.screens.BrowserScreen
+import org.videolan.television.ui.compose.theme.VlcTVTheme
+
+
+class BrowserActivity : DefaultTvActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        WindowCompat.getInsetsController(window, window.decorView).apply {
+            isAppearanceLightStatusBars = false
+        }
+        val storage = intent?.parcelable<MediaLibraryItem>(EXTRA_ITEM) ?: throw IllegalStateException("No storage provided")
+        setContent {
+            VlcTVTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    BrowserScreen(item = storage)
+                }
+            }
+        }
+
+    }
+
+}
