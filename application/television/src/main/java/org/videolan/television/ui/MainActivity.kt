3f4ed1617e6ac70ed7ebda4c4b0e53f711b5c9f8
Nicolas Pomepuy <nicolas@videolabs.io>
2026-02-04 09:26:21 +0100

    Compose TV: implement the context actions for the playlists

--- application/television/src/main/java/org/videolan/television/ui/MainActivity.kt
+++ application/television/src/main/java/org/videolan/television/ui/MainActivity.kt
@@ -25,27 +25,52 @@
 package org.videolan.television.ui
 
 import android.os.Bundle
-import android.view.KeyEvent
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.net.toUri
 import androidx.core.os.bundleOf
 import androidx.core.view.WindowCompat
 import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.videolan.medialibrary.interfaces.Medialibrary
+import org.videolan.medialibrary.interfaces.media.Folder
+import org.videolan.medialibrary.interfaces.media.MediaWrapper
+import org.videolan.medialibrary.interfaces.media.Playlist
+import org.videolan.medialibrary.media.MediaLibraryItem
+import org.videolan.resources.util.parcelable
+import org.videolan.resources.util.parcelableList
 import org.videolan.television.ui.compose.composable.screens.MainScreen
 import org.videolan.television.ui.compose.theme.VlcTVTheme
-import org.videolan.television.util.EventThrottler
+import org.videolan.television.viewmodel.MainActivityViewModel
 import org.videolan.tools.KEY_SHOW_UPDATE
 import org.videolan.tools.Settings
+import org.videolan.vlc.R
+import org.videolan.vlc.gui.dialogs.CONFIRM_DELETE_DIALOG_MEDIALIST
+import org.videolan.vlc.gui.dialogs.CONFIRM_DELETE_DIALOG_RESULT
+import org.videolan.vlc.gui.dialogs.CONFIRM_DELETE_DIALOG_RESULT_BAN_FOLDER
+import org.videolan.vlc.gui.dialogs.CONFIRM_DELETE_DIALOG_RESULT_DEFAULT_VALUE
+import org.videolan.vlc.gui.dialogs.CONFIRM_DELETE_DIALOG_RESULT_TYPE
+import org.videolan.vlc.gui.dialogs.CONFIRM_PLAYLIST_RENAME_DIALOG_RESULT
+import org.videolan.vlc.gui.dialogs.RENAME_DIALOG_MEDIA
+import org.videolan.vlc.gui.dialogs.RENAME_DIALOG_NEW_NAME
 import org.videolan.vlc.gui.dialogs.UPDATE_DATE
 import org.videolan.vlc.gui.dialogs.UPDATE_URL
 import org.videolan.vlc.gui.dialogs.UpdateDialog
+import org.videolan.vlc.gui.helpers.MedialibraryUtils
+import org.videolan.vlc.gui.helpers.UiTools
+import org.videolan.vlc.media.MediaUtils
 import org.videolan.vlc.util.AutoUpdate
+import org.videolan.vlc.util.MediaListEntry
+import org.videolan.vlc.util.Permissions
 
+private const val TAG = "VLC/MainActivity"
 
 class MainActivity : DefaultTvActivity() {
 
@@ -72,6 +97,59 @@ class MainActivity : DefaultTvActivity() {
                     arguments = bundleOf(UPDATE_URL to url, UPDATE_DATE to date.time)
                 }
                 updateDialog.show(supportFragmentManager, "fragment_update")
+            }
+        }
+
+        supportFragmentManager.setFragmentResultListener(CONFIRM_PLAYLIST_RENAME_DIALOG_RESULT, this) { requestKey, bundle ->
+            val media = bundle.parcelable<MediaLibraryItem>(RENAME_DIALOG_MEDIA) as? Playlist ?: return@setFragmentResultListener
+            val name = bundle.getString(RENAME_DIALOG_NEW_NAME) ?: return@setFragmentResultListener
+            lifecycleScope.launch {
+                withContext(Dispatchers.IO) {
+                    media.setName(name)
+                    val mainActivityViewModel: MainActivityViewModel by viewModels()
+                    mainActivityViewModel.invalidateList(MediaListEntry.AUDIO_PLAYLISTS)
+                }
+            }
+        }
+        supportFragmentManager.setFragmentResultListener(CONFIRM_DELETE_DIALOG_RESULT, this) { requestKey, bundle ->
+            val items: List<MediaLibraryItem> = bundle.parcelableList(CONFIRM_DELETE_DIALOG_MEDIALIST) ?: listOf()
+            val type = bundle.getInt(CONFIRM_DELETE_DIALOG_RESULT_TYPE)
+            if (type == CONFIRM_DELETE_DIALOG_RESULT_DEFAULT_VALUE) {
+                for (item in items) {
+                    items.forEach { mw ->
+                        if (mw is MediaWrapper) {
+                            val deleteAction = Runnable {
+                                lifecycleScope.launch {
+                                    MediaUtils.deleteItem(this@MainActivity, mw) { UiTools.snacker(this@MainActivity, getString(R.string.msg_delete_failed, mw.title)) }
+                                }
+                            }
+                            if (Permissions.checkWritePermission(this, (item as MediaWrapper), deleteAction)) deleteAction.run()
+                        } else MediaUtils.deleteItem(this, item) {
+//                            onDeleteFailed(it) 
+                        }
+                    }
+                }
+            } else if (type == CONFIRM_DELETE_DIALOG_RESULT_BAN_FOLDER) {
+                items.forEach {
+                    val path = if (it is Folder) it.mMrl.toUri().path else if (it is MediaWrapper) it.uri.path else null
+                    path?.let { path ->
+                        lifecycleScope.launch(Dispatchers.IO) {
+                            val roots: Array<String> = Medialibrary.getInstance().foldersList
+                            val strippedPath = path.removePrefix("file://")
+                            for (root in roots) {
+                                if (root.removePrefix("file://") == strippedPath) {
+                                    Log.w(TAG, "banFolder: trying to ban root: $root")
+                                    lifecycleScope.launch(Dispatchers.Main) {
+                                        UiTools.snacker(this@MainActivity, getString(R.string.cant_ban_root))
+                                    }
+                                    return@launch
+                                }
+                            }
+                            MedialibraryUtils.banDir(strippedPath)
+                        }
+
+                    } ?: Log.e(TAG, "banFolder: path is null")
+                }
             }
         }
     }
