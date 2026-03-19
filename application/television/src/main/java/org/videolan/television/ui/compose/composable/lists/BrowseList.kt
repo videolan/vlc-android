0260fe425f34b937cf56939df63fd437aafbd7cb
Nicolas Pomepuy <nicolas@videolabs.io>
2026-03-19 09:48:33 +0100

    Main browser ctx

--- application/television/src/main/java/org/videolan/television/ui/compose/composable/lists/BrowseList.kt
+++ application/television/src/main/java/org/videolan/television/ui/compose/composable/lists/BrowseList.kt
@@ -35,6 +35,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusProperties
 import androidx.compose.ui.platform.LocalContext
@@ -42,8 +43,12 @@ import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.viewmodel.MutableCreationExtras
 import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.videolan.medialibrary.interfaces.media.MediaWrapper
 import org.videolan.medialibrary.media.MediaLibraryItem
+import org.videolan.resources.MEDIALIBRARY_PAGE_SIZE
 import org.videolan.television.R
 import org.videolan.television.ui.TvUtil
 import org.videolan.television.ui.compose.composable.components.ContentLine
@@ -51,15 +56,30 @@ import org.videolan.television.ui.compose.composable.components.InvalidationComp
 import org.videolan.television.viewmodel.MainActivityViewModel
 import org.videolan.television.viewmodel.SnackbarContent
 import org.videolan.vlc.BuildConfig
+import org.videolan.vlc.gui.dialogs.SavePlaylistDialog
+import org.videolan.vlc.gui.helpers.UiTools.addToPlaylist
+import org.videolan.vlc.media.MediaUtils
+import org.videolan.vlc.util.ContextOption.CTX_ADD_TO_PLAYLIST
+import org.videolan.vlc.util.ContextOption.CTX_APPEND
+import org.videolan.vlc.util.ContextOption.CTX_FAV_ADD
+import org.videolan.vlc.util.ContextOption.CTX_FAV_REMOVE
+import org.videolan.vlc.util.ContextOption.CTX_INFORMATION
+import org.videolan.vlc.util.ContextOption.CTX_PLAY
+import org.videolan.vlc.util.ContextOption.CTX_PLAY_NEXT
+import org.videolan.vlc.util.ContextOption.CTX_PLAY_SHUFFLE
 import org.videolan.vlc.util.MediaListEntry
 import org.videolan.vlc.viewmodels.browser.BrowserFavoritesModel
 import org.videolan.vlc.viewmodels.browser.BrowserModel
 import org.videolan.vlc.viewmodels.browser.NetworkModel
 import org.videolan.vlc.viewmodels.browser.TYPE_STORAGE
+import java.security.SecureRandom
+import kotlin.math.min
 
 @Composable
 fun BrowseList(onFocusExit: () -> Unit, onFocusEnter: () -> Unit, mainActivityViewModel: MainActivityViewModel = viewModel()) {
     val context = LocalContext.current
+    val activity = LocalActivity.current
+    val coroutineScope = rememberCoroutineScope()
     val extras = MutableCreationExtras().apply {
         set(APPLICATION_KEY, context.applicationContext as Application)
     }
@@ -96,6 +116,24 @@ fun BrowseList(onFocusExit: () -> Unit, onFocusEnter: () -> Unit, mainActivityVi
     networks.value.forEach {
         if (BuildConfig.DEBUG) Log.d("NetworkDebug", "Network found: ${it.title} ${(it as MediaWrapper).uri}")
     }
+
+    mainActivityViewModel.addCtxClickListener(MediaListEntry.BROWSER) { item, position, ctxMenuItem ->
+        val showSnackbar: (String) -> Unit = {
+            mainActivityViewModel.showSnackbar(SnackbarContent(it))
+        }
+        when(ctxMenuItem.id) {
+            CTX_PLAY -> MediaUtils.openMedia(activity, (item as MediaWrapper))
+            CTX_PLAY_SHUFFLE -> MediaUtils.playTracks(activity!!, item, SecureRandom().nextInt(min(item.tracksCount, MEDIALIBRARY_PAGE_SIZE)), true)
+            CTX_APPEND -> MediaUtils.appendMedia(activity!!, item.tracks, showSnackbar)
+            CTX_PLAY_NEXT -> MediaUtils.insertNext(activity, item.tracks, showSnackbar)
+            CTX_INFORMATION -> mainActivityViewModel.showSnackbar(SnackbarContent(activity!!.resources.getString(R.string.not_implemented)))
+            CTX_ADD_TO_PLAYLIST -> (activity as FragmentActivity).addToPlaylist(item.tracks, SavePlaylistDialog.KEY_NEW_TRACKS)
+            CTX_FAV_ADD, CTX_FAV_REMOVE -> coroutineScope.launch { withContext(Dispatchers.IO) { item.isFavorite = ctxMenuItem.id == CTX_FAV_ADD } }
+            else -> {}
+        }
+    }
+
+
     Column(
         modifier = Modifier
             .focusProperties {
