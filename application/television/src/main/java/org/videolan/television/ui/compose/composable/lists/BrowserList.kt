42d00d6a55537b7edf5eeddf4b5798cf9061f8b8
Nicolas Pomepuy <nicolas@videolabs.io>
2026-03-19 08:46:23 +0100

    Implement the ctx actions for the TV browser

--- application/television/src/main/java/org/videolan/television/ui/compose/composable/lists/BrowserList.kt
+++ application/television/src/main/java/org/videolan/television/ui/compose/composable/lists/BrowserList.kt
@@ -27,6 +27,7 @@ package org.videolan.television.ui.compose.composable.lists
 import android.app.Application
 import android.util.Log
 import androidx.activity.compose.LocalActivity
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -54,14 +55,19 @@ import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.core.content.edit
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.viewmodel.MutableCreationExtras
 import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.videolan.medialibrary.interfaces.Medialibrary
 import org.videolan.medialibrary.interfaces.media.MediaWrapper
 import org.videolan.medialibrary.media.MediaLibraryItem
 import org.videolan.medialibrary.media.Storage
+import org.videolan.resources.MEDIALIBRARY_PAGE_SIZE
+import org.videolan.television.R
 import org.videolan.television.ui.compose.composable.components.BrowserItemCtxFlags
 import org.videolan.television.ui.compose.composable.components.InvalidationComposable
 import org.videolan.television.ui.compose.composable.components.MediaListSidePanel
@@ -72,14 +78,28 @@ import org.videolan.television.ui.compose.composable.items.AudioItemCard
 import org.videolan.television.ui.compose.composable.items.AudioItemList
 import org.videolan.television.viewmodel.FileBrowserViewModel
 import org.videolan.television.viewmodel.MainActivityViewModel
+import org.videolan.television.viewmodel.SnackbarContent
 import org.videolan.tools.Settings
 import org.videolan.vlc.BuildConfig
+import org.videolan.vlc.gui.dialogs.CONFIRM_DELETE_DIALOG_RESULT_BAN_FOLDER
+import org.videolan.vlc.gui.dialogs.ConfirmDeleteDialog
+import org.videolan.vlc.gui.dialogs.RenameDialog
+import org.videolan.vlc.gui.dialogs.SavePlaylistDialog
+import org.videolan.vlc.gui.helpers.UiTools.addToPlaylist
 import org.videolan.vlc.gui.view.EmptyLoadingState
+import org.videolan.vlc.media.MediaUtils
+import org.videolan.vlc.media.PlaylistManager
 import org.videolan.vlc.repository.BrowserFavRepository
+import org.videolan.vlc.util.ContextOption
+import org.videolan.vlc.util.ContextOption.*
 import org.videolan.vlc.util.MediaListEntry
 import org.videolan.vlc.util.Permissions
 import org.videolan.vlc.viewmodels.browser.BrowserModel
 import org.videolan.vlc.viewmodels.browser.TYPE_FILE
+import java.security.SecureRandom
+import java.util.LinkedList
+import kotlin.getValue
+import kotlin.math.min
 
 @Composable
 fun BrowserList(modifier: Modifier = Modifier, mainActivityViewModel: MainActivityViewModel = viewModel(), fileBrowserViewModel: FileBrowserViewModel = viewModel()) {
@@ -113,7 +133,9 @@ fun BrowserList(modifier: Modifier = Modifier, mainActivityViewModel: MainActivi
             key = path
         )
         entry.providerClass = browserModel.provider::class.java
-        val items by browserModel.dataset.observeAsState()
+        browserModel.dataset.convertToFlow()
+        val items by browserModel.dataset.datasetFlow.collectAsState()
+//        val items by browserModel.dataset.observeAsState()
         val descriptionUpdates = browserModel.provider.descriptionUpdate.observeAsState()
 
         val emptyState =
@@ -142,6 +164,84 @@ fun BrowserList(modifier: Modifier = Modifier, mainActivityViewModel: MainActivi
         entry.isRoot = (root.value as? MediaWrapper)?.uri.toString().isEmpty()
         mainActivityViewModel.addCtxClickListener(entry) { item, position, ctxMenuItem ->
             if (BuildConfig.DEBUG) Log.d("CtxClickListener", "Ctx clicked: ${ctxMenuItem.id} for $item in list $entry")
+            val showSnackbar: (String) -> Unit = {
+                mainActivityViewModel.showSnackbar(SnackbarContent(it))
+            }
+            when(ctxMenuItem.id) {
+                CTX_PLAY -> MediaUtils.openMedia(activity, (item as MediaWrapper))
+                CTX_PLAY_FROM_START -> {
+                    (item as MediaWrapper).addFlags(MediaWrapper.MEDIA_FROM_START)
+                    MediaUtils.openMedia(activity, (item as MediaWrapper))
+                }
+                CTX_PLAY_ALL -> coroutineScope.launch {
+                    var positionInPlaylist = 0
+                    val mediaLocations = LinkedList<MediaWrapper>()
+//                    scheduler.scheduleAction(MSG_SHOW_ENQUEUING, 1000L)
+                    withContext(Dispatchers.IO) {
+                        val files = if (browserModel.url?.startsWith("file") == true) browserModel.provider.browseUrl(browserModel.url!!) else browserModel.dataset.getList()
+                        for (file in files.filterIsInstance(MediaWrapper::class.java))
+                            if (file.type == MediaWrapper.TYPE_VIDEO || file.type == MediaWrapper.TYPE_AUDIO) {
+                                mediaLocations.add(browserModel.getMediaWithMeta(activity!!, file))
+                                if (file.equals(item))
+                                    positionInPlaylist = mediaLocations.size - 1
+                            }
+                    }
+//                    scheduler.startAction(MSG_HIDE_ENQUEUING)
+                    activity?.let { MediaUtils.openList(it, mediaLocations, positionInPlaylist, shuffle = PlaylistManager.shuffling.value) }
+                }
+                CTX_PLAY_SHUFFLE -> MediaUtils.playTracks(activity!!, item, SecureRandom().nextInt(min(item.tracksCount, MEDIALIBRARY_PAGE_SIZE)), true)
+                CTX_PLAY_AS_AUDIO -> coroutineScope.launch(Dispatchers.IO) {
+                    item.tracks?.let { trackArray ->
+                        MediaUtils.openList(activity, trackArray.map {
+                            it.addFlags(MediaWrapper.MEDIA_FORCE_AUDIO)
+                            it
+                        }.toList(), 0)
+                    }
+                }
+                CTX_DOWNLOAD_SUBTITLES -> MediaUtils.getSubs((activity as FragmentActivity), (item as MediaWrapper))
+                CTX_APPEND -> MediaUtils.appendMedia(activity!!, item.tracks, showSnackbar)
+                CTX_PLAY_NEXT -> MediaUtils.insertNext(activity, item.tracks, showSnackbar)
+                CTX_ADD_TO_PLAYLIST -> (activity as FragmentActivity).addToPlaylist(item.tracks, SavePlaylistDialog.KEY_NEW_TRACKS)
+                CTX_DELETE -> {
+                    val confirmDeleteDialog = ConfirmDeleteDialog.newInstance(arrayListOf(item))
+                    confirmDeleteDialog.show((activity as FragmentActivity).supportFragmentManager, ConfirmDeleteDialog::class.simpleName)
+                    confirmDeleteDialog.setListener {
+                        MediaUtils.deleteItem(activity, item) {
+                            browserModel.refresh()
+                        }
+                    }
+                }
+                CTX_FAV_ADD, CTX_FAV_REMOVE -> coroutineScope.launch(Dispatchers.IO) {
+                    item.isFavorite = ctxMenuItem.id == CTX_FAV_ADD
+                    browserModel.refresh()
+                }
+                CTX_BAN_FOLDER -> {
+                    val dialog = ConfirmDeleteDialog.newInstance(
+                        medias = arrayListOf(item),
+                        title = activity!!.resources.getString(R.string.group_ban_folder),
+                        description = activity.resources.getString(R.string.ban_folder_explanation, activity.resources.getString(R.string.medialibrary_directories)),
+                        buttonText = activity.resources.getString(R.string.ban_folder),
+                        resultType = CONFIRM_DELETE_DIALOG_RESULT_BAN_FOLDER
+                    )
+                    dialog.show((activity as FragmentActivity).supportFragmentManager, RenameDialog::class.simpleName)
+                }
+                CTX_MARK_AS_PLAYED -> coroutineScope.launch(Dispatchers.IO) {
+                    (item as? MediaWrapper)?.let { mw ->
+                        mw.playCount = mw.seen + 1L
+                        mw.seen += 1L
+                        browserModel.refresh()
+                    }
+                }
+                CTX_MARK_AS_UNPLAYED -> coroutineScope.launch(Dispatchers.IO) {
+                    (item as? MediaWrapper)?.let { mw ->
+                        mw.playCount = 0L
+                        mw.seen = 0L
+                        browserModel.refresh()
+                    }
+                }
+                else -> mainActivityViewModel.showSnackbar(SnackbarContent(activity!!.resources.getString(R.string.not_implemented)))
+
+            }
         }
         val displaySettingsChange by mainActivityViewModel.currentDisplaySettingsChange.collectAsState()
         InvalidationComposable(displaySettingsChange) { invalidate ->
