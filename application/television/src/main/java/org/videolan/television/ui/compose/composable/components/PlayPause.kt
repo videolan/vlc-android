2c4c80e61c45a24b19915e9674947f9a4e245666
Nicolas Pomepuy <nicolas@videolabs.io>
2026-04-13 09:52:40 +0200

    Compose TV: rework the audio player control buttons

---
+++ application/television/src/main/java/org/videolan/television/ui/compose/composable/components/PlayPause.kt
@@ -0,0 +1,52 @@
+/*
+ * ************************************************************************
+ *  AnimatedVectorDrawable.kt
+ * *************************************************************************
+ * Copyright © 2026 VLC authors and VideoLAN
+ * Author: Nicolas POMEPUY
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ * **************************************************************************
+ *
+ *
+ */
+
+package org.videolan.television.ui.compose.composable.components
+
+import androidx.compose.animation.graphics.res.animatedVectorResource
+import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
+import androidx.compose.animation.graphics.vector.AnimatedImageVector
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import org.videolan.vlc.R
+
+@Composable
+fun PlayPause(click: ()-> Unit, atEnd: Boolean = false) {
+    val image = AnimatedImageVector.animatedVectorResource(R.drawable.anim_play_pause_video)
+    Image(
+        painter = rememberAnimatedVectorPainter(image, atEnd),
+        contentDescription = "Timer",
+        modifier = Modifier.clickable {
+            click()
+        },
+        contentScale = ContentScale.Crop
+    )
+}
