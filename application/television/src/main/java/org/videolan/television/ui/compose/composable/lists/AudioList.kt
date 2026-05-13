06584d08b49bd5c70bfc1886c24ab98c740133cd
Nicolas Pomepuy <nicolas@videolabs.io>
2026-05-13 07:26:21 +0200

    Compose TV: use navigation 3

--- application/television/src/main/java/org/videolan/television/ui/compose/composable/lists/AudioList.kt
+++ application/television/src/main/java/org/videolan/television/ui/compose/composable/lists/AudioList.kt
@@ -29,28 +29,14 @@ import android.util.Log
 import androidx.activity.compose.LocalActivity
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.focusGroup
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -58,17 +44,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.focus.focusProperties
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.core.content.edit
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
-import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewmodel.MutableCreationExtras
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.paging.LoadState
@@ -88,24 +71,21 @@ import org.videolan.resources.PLAYLIST_TYPE_AUDIO
 import org.videolan.resources.PLAYLIST_TYPE_VIDEO
 import org.videolan.television.R
 import org.videolan.television.ui.TvUtil
+import org.videolan.television.ui.compose.AudioDestination
 import org.videolan.television.ui.compose.composable.components.InvalidationComposable
 import org.videolan.television.ui.compose.composable.components.MediaListSidePanel
 import org.videolan.television.ui.compose.composable.components.MediaListSidePanelContent
 import org.videolan.television.ui.compose.composable.components.MediaListSidePanelListenerKey
 import org.videolan.television.ui.compose.composable.components.PaginatedGrid
 import org.videolan.television.ui.compose.composable.components.PaginatedList
-import org.videolan.television.ui.compose.composable.components.VLCTabRow
 import org.videolan.television.ui.compose.composable.components.VlcEmptyViewLoader
 import org.videolan.television.ui.compose.composable.items.AudioItemCard
 import org.videolan.television.ui.compose.composable.items.AudioItemList
 import org.videolan.television.ui.compose.theme.Transparent
-import org.videolan.television.ui.compose.theme.White
-import org.videolan.television.ui.compose.theme.WhiteTransparent50
 import org.videolan.television.util.showParent
 import org.videolan.television.viewmodel.MainActivityViewModel
 import org.videolan.television.viewmodel.SnackbarContent
 import org.videolan.tools.KEY_AUDIO_CURRENT_TAB
-import org.videolan.tools.KEY_AUDIO_TAB
 import org.videolan.tools.Settings
 import org.videolan.tools.putSingle
 import org.videolan.vlc.BuildConfig
@@ -118,7 +98,6 @@ import org.videolan.vlc.gui.helpers.UiTools.addToPlaylist
 import org.videolan.vlc.gui.view.EmptyLoadingState
 import org.videolan.vlc.media.MediaUtils
 import org.videolan.vlc.providers.medialibrary.MedialibraryProvider
-import org.videolan.vlc.util.ContextOption
 import org.videolan.vlc.util.ContextOption.*
 import org.videolan.vlc.util.MediaListEntry
 import org.videolan.vlc.util.Permissions
@@ -129,111 +108,22 @@ import java.security.SecureRandom
 import kotlin.math.min
 
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AudioListScreen(onFocusExit: () -> Unit, onFocusEnter: () -> Unit, mainActivityViewModel: MainActivityViewModel = viewModel()) {
+fun AudioListScreen(subDestination: AudioDestination, onFocusExit: () -> Unit, onFocusEnter: () -> Unit) {
     val context = LocalContext.current
-    val settings = Settings.getInstance(context)
-    val pagerState = rememberPagerState(
-        initialPage = settings.getInt(KEY_AUDIO_TAB, 0),
-        pageCount = { mainActivityViewModel.audioTabs.size }
-    )
-    val coroutineScope = rememberCoroutineScope()
-    val medialistEntries = arrayOf(
-        MediaListEntry.ARTISTS,
-        MediaListEntry.ALBUMS,
-        MediaListEntry.TRACKS,
-        MediaListEntry.GENRES,
-        MediaListEntry.AUDIO_PLAYLISTS
-    )
-
-    Column(
-        modifier = Modifier
-    ) {
-        val displaySettingsChange by mainActivityViewModel.currentDisplaySettingsChange.collectAsState()
-        InvalidationComposable(displaySettingsChange) {
-            VLCTabRow(
-                selectedTabIndex = pagerState.currentPage,
-                modifier = Modifier
-                    .padding(vertical = 4.dp, horizontal = 8.dp)
-                    .focusProperties {
-                        onEnter = {
-                            onFocusEnter()
-                        }
-                        onExit = {
-                            if (requestedFocusDirection == FocusDirection.Up) {
-                                if (BuildConfig.DEBUG) Log.d("MainScreenLogs", "onFocusExit triggered for Column")
-                                onFocusExit()
-                            }
-                        }
-                    }
-                    .focusGroup(),
-
-                onSelected = { index ->
-                    if (index == pagerState.currentPage) return@VLCTabRow
-                    coroutineScope.launch {
-                        pagerState.animateScrollToPage(index)
-                    }
-                    settings.edit { putInt(KEY_AUDIO_TAB, index) }
-
-                },
-                tabNumber = mainActivityViewModel.audioTabs.size,
-                indicator = { hasFocus ->
-                    Box(
-                        Modifier
-                            .fillMaxSize()
-                            .background(if (hasFocus) White else WhiteTransparent50, RoundedCornerShape(50))
-                    )
-                },
-                key = "audio",
-                getTab = { index, focused ->
-                    val inFav = settings.getBoolean(medialistEntries[index].onlyFavsKey, false)
-                    val tab = mainActivityViewModel.audioTabs[index]
-                    Row(
-                        modifier = Modifier
-                            .padding(vertical = 0.dp, horizontal = 8.dp)
-                            .align(Alignment.CenterHorizontally),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        if (inFav)
-                            Icon(
-                                imageVector = Icons.Default.Favorite,
-                                stringResource(R.string.show_only_favs),
-                                tint = if (focused) MaterialTheme.colorScheme.background else MaterialTheme.colorScheme.onSurface.copy(0.4F),
-                                modifier = Modifier
-                                    .padding(end = 8.dp)
-                                    .height(16.dp)
-                            )
-                        Text(
-                            style = MaterialTheme.typography.labelLarge,
-                            text = stringResource(tab),
-                            color = if (focused) MaterialTheme.colorScheme.background else MaterialTheme.colorScheme.onSurface.copy(0.4F),
-
-                            )
-                    }
-                }
-            )
-        }
-        HorizontalPager(
-            pagerState,
-            userScrollEnabled = false,
-            verticalAlignment = Alignment.Top,
-            modifier = Modifier.fillMaxSize()
-        ) { page ->
-            Settings.getInstance(context).putSingle(KEY_AUDIO_CURRENT_TAB, pagerState.currentPage)
-            when (page) {
-                0 -> MediaList(MediaListEntry.ARTISTS, pagerState.currentPage)
-                1 -> MediaList(MediaListEntry.ALBUMS, pagerState.currentPage)
-                2 -> MediaList(MediaListEntry.TRACKS, pagerState.currentPage)
-                3 -> MediaList(MediaListEntry.GENRES, pagerState.currentPage)
-                4 -> MediaList(MediaListEntry.AUDIO_PLAYLISTS, pagerState.currentPage)
-            }
-        }
+    val index = subDestination.ordinal
+    Settings.getInstance(context).putSingle(KEY_AUDIO_CURRENT_TAB, index)
+    when (subDestination) {
+        AudioDestination.Artists -> MediaList(MediaListEntry.ARTISTS, index, onFocusExit = onFocusExit, onFocusEnter = onFocusEnter)
+        AudioDestination.Albums -> MediaList(MediaListEntry.ALBUMS, index, onFocusExit = onFocusExit, onFocusEnter = onFocusEnter)
+        AudioDestination.Tracks -> MediaList(MediaListEntry.TRACKS, index, onFocusExit = onFocusExit, onFocusEnter = onFocusEnter)
+        AudioDestination.Genres -> MediaList(MediaListEntry.GENRES, index, onFocusExit = onFocusExit, onFocusEnter = onFocusEnter)
+        AudioDestination.Playlists -> MediaList(MediaListEntry.AUDIO_PLAYLISTS, index, onFocusExit = onFocusExit, onFocusEnter = onFocusEnter)
     }
 }
 
 @Composable
-fun MediaList(entry: MediaListEntry, index: Int, mainActivityViewModel: MainActivityViewModel = viewModel()) {
+fun MediaList(entry: MediaListEntry, index: Int, onFocusExit: () -> Unit = {}, onFocusEnter: () -> Unit = {}, mainActivityViewModel: MainActivityViewModel = viewModel()) {
     val displaySettingsChange by mainActivityViewModel.currentDisplaySettingsChange.collectAsState()
     val invalidateEntry by mainActivityViewModel.invalidateMediaListEntry.collectAsState()
     val coroutineScope = rememberCoroutineScope()
@@ -420,7 +310,16 @@ fun MediaList(entry: MediaListEntry, index: Int, mainActivityViewModel: MainActi
             EmptyLoadingState.NONE
 
         VlcEmptyViewLoader(emptyState) {
-            Row(modifier = Modifier.fillMaxHeight()) {
+            Row(modifier = Modifier
+                .fillMaxSize()
+                .focusProperties {
+                    onExit = {
+                        if (requestedFocusDirection == FocusDirection.Up) onFocusExit()
+                    }
+                    onEnter = {
+                        onFocusEnter()
+                    }
+                }) {
                 if (emptyState != EmptyLoadingState.EMPTY_FAVORITES)
                     if (inCard) {
                         PaginatedGrid(
--- application/television/src/main/java/org/videolan/television/ui/compose/composable/lists/VideoList.kt
+++ application/television/src/main/java/org/videolan/television/ui/compose/composable/lists/VideoList.kt
@@ -28,27 +28,13 @@ import android.app.Application
 import android.util.Log
 import androidx.activity.compose.LocalActivity
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.background
-import androidx.compose.foundation.focusGroup
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -56,17 +42,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.focus.focusProperties
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.core.content.edit
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
-import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewmodel.MutableCreationExtras
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.paging.LoadState
@@ -80,18 +63,16 @@ import org.videolan.medialibrary.media.MediaLibraryItem
 import org.videolan.resources.PLAYLIST_TYPE_VIDEO
 import org.videolan.television.R
 import org.videolan.television.ui.TvUtil
+import org.videolan.television.ui.compose.VideoDestination
 import org.videolan.television.ui.compose.composable.components.InvalidationComposable
 import org.videolan.television.ui.compose.composable.components.MediaListSidePanel
 import org.videolan.television.ui.compose.composable.components.MediaListSidePanelContent
 import org.videolan.television.ui.compose.composable.components.MediaListSidePanelListenerKey
 import org.videolan.television.ui.compose.composable.components.PaginatedGrid
 import org.videolan.television.ui.compose.composable.components.PaginatedList
-import org.videolan.television.ui.compose.composable.components.VLCTabRow
 import org.videolan.television.ui.compose.composable.components.VlcEmptyViewLoader
 import org.videolan.television.ui.compose.composable.items.VideoItem
 import org.videolan.television.ui.compose.composable.items.VideoItemList
-import org.videolan.television.ui.compose.theme.White
-import org.videolan.television.ui.compose.theme.WhiteTransparent50
 import org.videolan.television.ui.openVideoGroupFolder
 import org.videolan.television.util.showParent
 import org.videolan.television.viewmodel.MainActivityViewModel
@@ -99,7 +80,6 @@ import org.videolan.television.viewmodel.SnackbarContent
 import org.videolan.tools.KEY_CASTING_AUDIO_ONLY
 import org.videolan.tools.KEY_GROUP_VIDEOS
 import org.videolan.tools.KEY_VIDEOS_CARDS
-import org.videolan.tools.KEY_VIDEO_TAB
 import org.videolan.tools.Settings
 import org.videolan.vlc.BuildConfig
 import org.videolan.vlc.PlaybackService
@@ -133,109 +113,20 @@ import org.videolan.vlc.util.ContextOption.CTX_SHARE
 import org.videolan.vlc.util.MediaListEntry
 import org.videolan.vlc.util.Permissions
 import org.videolan.vlc.util.share
-import org.videolan.vlc.util.showParentFolder
 import org.videolan.vlc.viewmodels.mobile.VideoGroupingType
 import org.videolan.vlc.viewmodels.mobile.VideosViewModel
 
 @Composable
-fun VideoListScreen(onFocusExit: () -> Unit, onFocusEnter: () -> Unit, mainActivityViewModel: MainActivityViewModel = viewModel()) {
-    val context = LocalContext.current
-    val settings = Settings.getInstance(context)
-    val pagerState = rememberPagerState(
-        initialPage = settings.getInt(KEY_VIDEO_TAB, 0),
-        pageCount = { mainActivityViewModel.videoTabs.size }
-    )
-    val coroutineScope = rememberCoroutineScope()
-
-    Column(
-        modifier = Modifier
-    ) {
-        val displaySettingsChange by mainActivityViewModel.currentDisplaySettingsChange.collectAsState()
-        InvalidationComposable(displaySettingsChange) {
-            VLCTabRow(
-                selectedTabIndex = pagerState.currentPage,
-                modifier = Modifier
-                    .padding(vertical = 4.dp, horizontal = 8.dp)
-                    .focusProperties {
-                        onEnter = {
-                            onFocusEnter()
-                        }
-                        onExit = {
-                            if (requestedFocusDirection == FocusDirection.Up) {
-                                if (BuildConfig.DEBUG) Log.d("MainScreenLogs", "onFocusExit triggered for Column")
-                                onFocusExit()
-                            }
-                        }
-                    }
-                    .focusGroup(),
-
-                onSelected = { index ->
-                    if (index == pagerState.currentPage) return@VLCTabRow
-                    coroutineScope.launch {
-                        pagerState.animateScrollToPage(index)
-                    }
-                    settings.edit { putInt(KEY_VIDEO_TAB, index) }
-                },
-                tabNumber = mainActivityViewModel.videoTabs.size,
-                indicator = { hasFocus ->
-                    Box(
-                        Modifier
-                            .fillMaxSize()
-                            .background(if (hasFocus) White else WhiteTransparent50, RoundedCornerShape(50))
-                    )
-                },
-                key = "video",
-                getTab = { index, focused ->
-                    val tab = mainActivityViewModel.videoTabs[index]
-                    val entry = if (index == 1) MediaListEntry.VIDEO_PLAYLISTS else {
-                        when (Settings.getInstance(context).getString(KEY_GROUP_VIDEOS, VideoGroupingType.NAME.settingsKey)) {
-                            VideoGroupingType.NAME.settingsKey -> MediaListEntry.VIDEO_GROUPS
-                            VideoGroupingType.FOLDER.settingsKey -> MediaListEntry.VIDEO_FOLDER
-                            else -> MediaListEntry.VIDEO
-                        }
-                    }
-                    val inFav = settings.getBoolean(entry.onlyFavsKey, false)
-                    Row(
-                        modifier = Modifier
-                            .padding(vertical = 0.dp, horizontal = 8.dp)
-                            .align(Alignment.CenterHorizontally),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        if (inFav)
-                            Icon(
-                                imageVector = Icons.Default.Favorite,
-                                stringResource(R.string.show_only_favs),
-                                tint = if (focused) MaterialTheme.colorScheme.background else MaterialTheme.colorScheme.onSurface.copy(0.4F),
-                                modifier = Modifier
-                                    .padding(end = 8.dp)
-                                    .height(16.dp)
-                            )
-                        Text(
-                            style = MaterialTheme.typography.labelLarge,
-                            text = stringResource(tab),
-                            color = if (focused) MaterialTheme.colorScheme.background else MaterialTheme.colorScheme.onSurface.copy(0.4F),
-                        )
-                    }
-                }
-            )
-        }
-        HorizontalPager(
-            pagerState,
-            userScrollEnabled = false,
-            verticalAlignment = Alignment.Top,
-            modifier = Modifier.fillMaxSize()
-        ) { page ->
-            when (page) {
-                0 -> VideoList()
-                1 -> MediaList(MediaListEntry.VIDEO_PLAYLISTS, -1)
-            }
-        }
+fun VideoListScreen(subDestination: VideoDestination, onFocusExit: () -> Unit, onFocusEnter: () -> Unit) {
+    when (subDestination) {
+        VideoDestination.Videos -> VideoList(onFocusExit = onFocusExit, onFocusEnter = onFocusEnter)
+        VideoDestination.Playlists -> MediaList(MediaListEntry.VIDEO_PLAYLISTS, -1, onFocusExit = onFocusExit, onFocusEnter = onFocusEnter)
     }
 }
 
 
 @Composable
-fun VideoList(modifier: Modifier = Modifier, folder: Folder? = null, group: VideoGroup? = null, mainActivityViewModel: MainActivityViewModel = viewModel()) {
+fun VideoList(modifier: Modifier = Modifier, folder: Folder? = null, group: VideoGroup? = null, onFocusExit: () -> Unit, onFocusEnter: () -> Unit, mainActivityViewModel: MainActivityViewModel = viewModel()) {
     val activity = LocalActivity.current
     val settings = Settings.getInstance(activity!!)
     val listState = rememberLazyListState()
@@ -335,13 +226,22 @@ fun VideoList(modifier: Modifier = Modifier, folder: Folder? = null, group: Vide
 
                 }
             }
-            val onLongClick:(MediaLibraryItem, Int) -> Unit = { video, position ->
+            val onLongClick:(MediaLibraryItem, Int) -> Unit = { video, _ ->
                 if (video is Folder || video is VideoGroup) {
-                    mainActivityViewModel.showSnackbar(SnackbarContent(activity!!.resources.getString(R.string.not_implemented)))
+                    mainActivityViewModel.showSnackbar(SnackbarContent(activity.resources.getString(R.string.not_implemented)))
                 } else
-                    TvUtil.showMediaDetail(activity!!, video as MediaWrapper, false)
+                    TvUtil.showMediaDetail(activity, video as MediaWrapper, false)
             }
-            Row(modifier = modifier.fillMaxHeight()) {
+            Row(modifier = modifier
+                .fillMaxSize()
+                .focusProperties {
+                    onExit = {
+                        if (requestedFocusDirection == FocusDirection.Up) onFocusExit()
+                    }
+                    onEnter = {
+                        onFocusEnter()
+                    }
+                }) {
                 if (emptyState != EmptyLoadingState.EMPTY_FAVORITES)
                     if (inCard) {
                         PaginatedGrid(
