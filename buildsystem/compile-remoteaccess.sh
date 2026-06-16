9dcdea25dc22400fe8d2c8f8f06b46871aed4b37
Nicolas Pomepuy <nicolas@videolabs.io>
2026-06-16 06:24:46 +0200

    Bump the remote access artifact to 0.18.0

--- buildsystem/compile-remoteaccess.sh
+++ buildsystem/compile-remoteaccess.sh
@@ -57,7 +57,7 @@ done
 ##############################
 diagnostic "Setting up the Remote Access project"
 
-REMOTE_ACCESS_TESTED_HASH=d618004a966acf81b9bc8878bcaf43578f69da07
+REMOTE_ACCESS_TESTED_HASH=2dcf214a1b215416767a057ab7bc3913ecc1b57b
 REMOTE_ACCESS_REPOSITORY=https://code.videolan.org/videolan/remoteaccess
 
 : ${VLC_REMOTE_ACCESS_PATH:="$(pwd -P)/application/remote-access-client/remoteaccess"}
