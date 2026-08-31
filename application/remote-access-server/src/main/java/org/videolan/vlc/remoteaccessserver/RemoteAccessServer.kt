225612d9899a5db524975cd9df6f2bce712eef1f
Nicolas Pomepuy <nicolas@videolabs.io>
2026-08-31 13:12:01 +0200

    Remote access: populate the track info in the now playing data

--- application/remote-access-server/src/main/java/org/videolan/vlc/remoteaccessserver/RemoteAccessServer.kt
+++ application/remote-access-server/src/main/java/org/videolan/vlc/remoteaccessserver/RemoteAccessServer.kt
@@ -775,9 +775,14 @@ class RemoteAccessServer(private val context: Context) : PlaybackService.Callbac
                 val isVideoPlaying = service.playlistManager.player.isVideoPlaying()
                 val waitForMediaEnd = service.waitForMediaEnd
                 val resetOnInteraction = service.resetOnInteraction
+                val audioTracks = service.audioTracks?.map { WSTrack(it.getId(), it.getName()) } ?: listOf()
+                val currentAudioTrack = service.audioTrack
+                val subtitleTracks = service.spuTracks?.map { WSTrack(it.getId(), it.getName()) } ?: listOf()
+                val currentSubtitleTrack = service.spuTrack
                 val nowPlaying = NowPlaying(media.title ?: "", media.artistName
                         ?: "", service.isPlaying, isVideoPlaying, service.getTime(), service.length, media.id, media.artworkURL
-                        ?: "", media.uri.toString(), getVolume(), speed, sleepTimer, waitForMediaEnd, resetOnInteraction, service.isShuffling, service.repeatType, bookmarks = bookmarks.map { WSBookmark(it.id, it.title, it.time) }, chapters = chapters.map { WSChapter(it.name, it.duration) })
+                        ?: "", media.uri.toString(), getVolume(), speed, sleepTimer, waitForMediaEnd, resetOnInteraction, service.isShuffling, service.repeatType, bookmarks = bookmarks.map { WSBookmark(it.id, it.title, it.time) }, chapters = chapters.map { WSChapter(it.name, it.duration) },
+                        audioTracks = audioTracks, currentAudioTrack = currentAudioTrack, subtitleTracks = subtitleTracks, currentSubtitleTrack = currentSubtitleTrack)
                 return nowPlaying
 
             }
