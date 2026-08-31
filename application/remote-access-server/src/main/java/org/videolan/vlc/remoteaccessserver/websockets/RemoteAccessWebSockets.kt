93efb9c3113e35fcd5362e8e1d5881bba542bc69
Nicolas Pomepuy <nicolas@videolabs.io>
2026-08-31 13:14:22 +0200

    Remote access: handle the video tracks websocket commands

--- application/remote-access-server/src/main/java/org/videolan/vlc/remoteaccessserver/websockets/RemoteAccessWebSockets.kt
+++ application/remote-access-server/src/main/java/org/videolan/vlc/remoteaccessserver/websockets/RemoteAccessWebSockets.kt
@@ -248,6 +248,8 @@ object RemoteAccessWebSockets {
             SET_BROWSER_AUDIO -> {
                 service?.playlistManager?.setBrowserAudio(incomingMessage.id == 1)
             }
+            SET_AUDIO_TRACK -> incomingMessage.stringValue?.let { trackId -> service?.setAudioTrack(trackId) }
+            SET_SUBTITLE_TRACK -> incomingMessage.stringValue?.let { trackId -> service?.setSpuTrack(trackId) }
         }
         return true
     }
